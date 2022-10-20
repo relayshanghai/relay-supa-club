6f74b722579ae3e2c1b6eb7ec9bd760c9f4b7d57
cristianbote <botecristian@yahoo.com>
2022-10-20 10:36:23 +0300

    Added the search

--- pages/api/kol/index.ts
+++ pages/api/kol/index.ts
@@ -1,9 +1,99 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from 'src/utils/supabase-client';
 
+const headers = {
+    'X-Api-Key': process.env.NEXT_PUBLIC_DATA_KEY!,
+    'Content-Type': 'application/json'
+};
+
+const search = {
+    unlocked: async (platform = 'youtube') => {
+        console.log(headers);
+        return await (
+            await fetch(`https://socapi.icu/v2.0/api/lists/search_shown/?platform=${platform}`, {
+                method: 'get',
+                headers
+            })
+        ).json();
+    },
+    term: async (platform: string = 'youtube', term: string) => {
+        console.log(headers);
+
+        return await (
+            await fetch(`https://socapi.icu/v2.0/api/search/newv1?platform=${platform}`, {
+                method: 'post',
+                headers,
+                body: JSON.stringify({
+                    paging: {
+                        skip: null,
+                        limit: 10
+                    },
+                    filter: {
+                        audience_geo: [],
+                        geo: [],
+                        lang: {
+                            code: 'en'
+                        },
+                        audience_gender: {
+                            code: '',
+                            weight: ''
+                        },
+                        gender: {
+                            code: '',
+                            weight: ''
+                        },
+                        audience_age: [],
+                        age: {
+                            left_number: '',
+                            right_number: ''
+                        },
+                        followers: {
+                            left_number: '',
+                            right_number: ''
+                        },
+                        engagements: {
+                            left_number: '',
+                            right_number: ''
+                        },
+                        engagement_rate: {
+                            value: null,
+                            operator: 'gte'
+                        },
+                        last_posted: '',
+                        audience_relevance: [],
+                        views: {
+                            left_number: '',
+                            right_number: ''
+                        },
+                        relevance: {
+                            value: term,
+                            weight: 0.5
+                        }
+                    },
+                    sort: {
+                        field: 'relevance',
+                        direction: 'desc'
+                    },
+                    audience_source: 'any'
+                })
+            })
+        ).json();
+    }
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        return res.status(200).json({});
+        const { term, platform } = JSON.parse(req.body);
+
+        if (term.length > 2) {
+            const results = await search.term(platform, term);
+
+            console.log(results);
+
+            return res.status(200).json(results);
+        }
+
+        return res.status(200).json([]);
     }
 
     return res.status(400).json(null);
--- src/modules/search.tsx
+++ src/modules/search.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Input } from 'src/components/input';
 
 export const Search = () => {
     const [search, setSearch] = useState('');
-    const [channel, setChannel] = useState<any>('YouTube');
+    const [channel, setChannel] = useState<any>('youtube');
     const channels = [
-        { icon: '/assets/svg/yt.svg', label: 'YouTube' },
-        { icon: '/assets/svg/instagram.svg', label: 'Instagram' },
-        { icon: '/assets/svg/tiktok.svg', label: 'TikTok' }
+        { icon: '/assets/svg/yt.svg', label: 'YouTube', id: 'youtube' },
+        { icon: '/assets/svg/instagram.svg', label: 'Instagram', id: 'instagram' },
+        { icon: '/assets/svg/tiktok.svg', label: 'TikTok', id: 'tiktok' }
     ];
+    const [results, setResults] = useState<any>();
+
+    useEffect(() => {
+        console.log('search');
+        if (search && search.length > 2) {
+            fetch('/api/kol', {
+                method: 'post',
+                body: JSON.stringify({
+                    platform: channel,
+                    term: search
+                })
+            })
+                .then((res) => {
+                    return res.json();
+                })
+                .then((res) => {
+                    setResults(res);
+                });
+        } else {
+            setResults(undefined);
+        }
+    }, [search, channel]);
 
     return (
         <div className="space-y-4">
@@ -16,11 +38,11 @@ export const Search = () => {
                 {channels.map((item, i) => (
                     <button
                         className={`transition px-2 rounded-lg hover:shadow-xl ${
-                            channel === item.label ? 'bg-white shadow-xl' : ''
+                            channel === item.id ? 'bg-white shadow-xl' : ''
                         }`}
                         key={item.label}
                         onClick={() => {
-                            setChannel(item.label);
+                            setChannel(item.id);
                         }}
                     >
                         <img src={item.icon} height={32} width={32} alt={item.label} />
@@ -28,12 +50,48 @@ export const Search = () => {
                 ))}
             </div>
             <Input
+                type="text"
                 placeholder="Search for KOLs"
                 value={search}
                 onChange={(e: any) => {
                     setSearch(e.target.value);
                 }}
             />
+            <div>
+                {results ? (
+                    <div className="font-bold text-sm">Total Results: {results.total}</div>
+                ) : null}
+            </div>
+            <div className="gap-2 grid grid-cols-3">
+                {Array.isArray(results?.accounts)
+                    ? results.accounts.map((item: any, i: any) => {
+                          return (
+                              <div
+                                  key={item.account?.user_profile?.user_id ?? i}
+                                  className="flex flex-row items-center space-x-2 shadow-xl rounded-xl p-2 bg-white"
+                              >
+                                  <div className="rounded-full overflow-hidden shadow-lg">
+                                      <img
+                                          src={item.account.user_profile.picture}
+                                          className="w-12 h-12"
+                                      />
+                                  </div>
+                                  <div className="flex flex-col">
+                                      <div className="font-bold">
+                                          {item.account.user_profile.custom_name}
+                                      </div>
+                                      <div className="text-sm">
+                                          {item.account.user_profile.fullname}
+                                      </div>
+                                      <div className="font-bold text-sm">
+                                          Avg. views: {item.account.user_profile.avg_views}
+                                      </div>
+                                  </div>
+                              </div>
+                          );
+                      })
+                    : null}
+            </div>
         </div>
     );
 };
