8ca614d1e91e9e854ba2281670d590c7c310ba25
cristianbote <botecristian@yahoo.com>
2022-10-21 11:31:20 +0300

    Add placeholder

--- src/modules/search.tsx
+++ src/modules/search.tsx
@@ -114,7 +114,7 @@ export const Search = () => {
                             return (
                                 <tr
                                     key={i}
-                                    className={`${placeholder ? 'bg-gray-100' : ''} relative`}
+                                    className={`${placeholder ? 'bg-gray-50' : ''} relative`}
                                 >
                                     <td className="py-2 px-4 flex flex-row items-center space-x-2">
                                         {!placeholder ? (
@@ -134,10 +134,10 @@ export const Search = () => {
                                             </>
                                         ) : (
                                             <>
-                                                <div className="w-12 h-12 rounded-full bg-gray-200" />
+                                                <div className="w-12 h-12 rounded-full bg-gray-100" />
                                                 <div className="space-y-2">
-                                                    <div className="font-bold text-gray-200 bg-gray-200 w-40 h-4" />
-                                                    <div className="text-primary-500 text-sm text-gray-200 bg-gray-200 w-20 h-4" />
+                                                    <div className="font-bold bg-gray-100 w-40 h-4" />
+                                                    <div className="text-primary-500 text-sm bg-gray-100 w-20 h-4" />
                                                 </div>
                                                 <div className="absolute top-0 left-0 translate-x-1/2 translate-y-1/2 p-2 text-sm">
                                                     <Link href="/account" passHref>
@@ -154,28 +154,28 @@ export const Search = () => {
                                         {!placeholder ? (
                                             formatter(item.account.user_profile.followers)
                                         ) : (
-                                            <div className="text-primary-500 text-sm text-gray-200 bg-gray-200 w-10 h-4" />
+                                            <div className="text-primary-500 text-sm bg-gray-100 w-10 h-4" />
                                         )}
                                     </td>
                                     <td className="text-sm">
                                         {!placeholder ? (
                                             formatter(item.account.user_profile.engagements)
                                         ) : (
-                                            <div className="text-primary-500 text-sm text-gray-200 bg-gray-200 w-10 h-4" />
+                                            <div className="text-primary-500 text-sm bg-gray-100 w-10 h-4" />
                                         )}
                                     </td>
                                     <td className="text-sm">
                                         {!placeholder ? (
                                             formatter(item.account.user_profile.engagement_rate)
                                         ) : (
-                                            <div className="text-primary-500 text-sm text-gray-200 bg-gray-200 w-10 h-4" />
+                                            <div className="text-primary-500 text-sm bg-gray-100 w-10 h-4" />
                                         )}
                                     </td>
                                     <td className="text-sm">
                                         {!placeholder ? (
                                             formatter(item.account.user_profile.avg_views)
                                         ) : (
-                                            <div className="text-primary-500 text-sm text-gray-200 bg-gray-200 w-10 h-4" />
+                                            <div className="text-primary-500 text-sm bg-gray-100 w-10 h-4" />
                                         )}
                                     </td>
                                 </tr>
