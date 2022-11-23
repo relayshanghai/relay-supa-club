afb1c43230074620d8c6227ea8e9408afc035f6e
Ellie Wang <yuetingw@outlook.com>
2022-11-23 17:31:46 +0800

    add: init campaigns index handler to get data

---
+++ pages/api/campaigns/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from 'src/utils/supabase-client';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        const companyId = req.query.id;
+        const { data, error } = await supabase
+            .from('campaigns')
+            .select('*')
+            .eq('company_id', companyId);
+        if (error) {
+            return res.status(500).json(error);
+        }
+        return res.status(200).json(data);
+    }
+    return res.status(400).end();
+}
