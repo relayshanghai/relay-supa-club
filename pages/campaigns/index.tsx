fbb59ffb9278e7fb67d821581b48e3ad8394d48a
Ellie Wang <yuetingw@outlook.com>
2022-11-24 14:12:32 +0800

    fix: ActiveLink ref to match values passed

---
+++ pages/campaigns/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from 'src/modules/layout';
+
+const Page = () => {
+    return (
+        <Layout>
+            <div className="flex flex-col p-6">
+                Campaigns
+                {/* <Search /> */}
+            </div>
+        </Layout>
+    );
+};
+
+export default Page;
