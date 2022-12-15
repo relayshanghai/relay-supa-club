b175f0ad62f845814738d4a7f1f0b4fce9677332
jacobcoro <xiaokourou@gmail.com>
2022-12-15 17:39:45 +0800

    shared translations

---
+++ i18n/shared.js
@@ -0,0 +1,14 @@
+export const statuses = {
+    en: {
+        inProgress: 'In Progress',
+        notStarted: 'Not Started',
+        completed: 'Completed',
+        all: 'All'
+    },
+    zh: {
+        inProgress: '进行中',
+        notStarted: '未开始',
+        completed: '已完成',
+        all: '全部'
+    }
+};
