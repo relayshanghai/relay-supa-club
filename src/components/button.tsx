182541353debf932f1606f2ecfd22a1800af2f8a
jacobcoro <xiaokourou@gmail.com>
2022-12-20 14:47:01 +0800

    cleanup button css conditionals

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,21 @@
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import { ButtonHTMLAttributes } from 'react';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary';
     className?: string;
 }
 
+const defaultClasses =
+    'text-sm px-4 py-2 rounded-md cursor-pointer flex-shrink-0 duration-300 font-bold disabled:bg-gray-300 disabled:cursor-default ';
+const primaryClasses = 'text-white bg-primary-500 hover:bg-primary-700';
+const secondaryClasses =
+    'text-primary-500 bg-white border-primary-500 border hover:bg-primary-700/10';
+
 export const Button = ({ children, variant, className, ...rest }: ButtonProps) => {
     return (
         <button
-            className={`text-sm px-4 py-2 rounded-md cursor-pointer flex-shrink-0 duration-300 font-bold disabled:bg-gray-300 disabled:cursor-default ${
-                variant === 'secondary'
-                    ? 'text-primary-500 bg-white border-primary-500 border hover:bg-primary-700/10'
-                    : 'text-white bg-primary-500 hover:bg-primary-700'
+            className={`${defaultClasses} ${
+                variant === 'secondary' ? secondaryClasses : primaryClasses
             } ${className}`}
             {...rest}
         >
