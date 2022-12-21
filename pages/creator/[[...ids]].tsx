2c2f3eca5232680f4fdb03d3f57825c62ac51415
jacobcoro <xiaokourou@gmail.com>
2022-12-21 17:12:05 +0800

    reports wip

--- pages/creator/[[...ids]].tsx
+++ pages/creator/[[...ids]].tsx
@@ -1,69 +1,28 @@
 import { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { Layout } from 'src/modules/layout';
 import { headers } from 'src/utils/api/constants';
 import { CreatorSearchResult, CreatorSearchAccount } from 'types';
 
-const Page = ({
-    platform,
-    user_id,
-    creator,
-    error
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Page = () => {
+    const { ids } = useRouter().query;
+    const [platform, user_id] = ids as string[];
+
+    useEffect(() => {
+        // check if report exists
+    });
+
     return (
         <Layout>
             <div className="flex flex-col p-6">
                 <p>{platform}</p>
                 <p>{user_id}</p>
-                <p>{creator?.user_profile.username}</p>
-                <p>{error}</p>
+                {/* <p>{creator?.user_profile.username}</p>
+                <p>{error}</p> */}
             </div>
         </Layout>
     );
-};
-
-export const getServerSideProps: GetServerSideProps<{
-    platform: string;
-    user_id: string;
-    creator: CreatorSearchAccount | null;
-    error: string;
-}> = async (context) => {
-    const { ids } = context.query;
-    const [platform, user_id] = ids as string[];
-    const query = {
-        paging: {},
-        filter: {
-            filter_ids: [user_id]
-        },
-        sort: { field: 'followers', direction: 'desc' },
-        audience_source: 'any'
-    };
-    let creator: CreatorSearchAccount | null = null;
-    let error = '';
-    try {
-        const searchRes = await fetch(
-            `https://socapi.icu/v2.0/api/search/newv1?platform=${platform}&auto_unhide=1`,
-            {
-                method: 'post',
-                headers,
-                body: JSON.stringify(query)
-            }
-        );
-        console.log({ searchRes });
-        const searchResJson = (await searchRes.json()) as CreatorSearchResult;
-        console.log({ searchResJson });
-        creator = searchResJson.accounts[0].account;
-        console.log({ creator });
-    } catch (error: any) {
-        error = error.message;
-    }
-    return {
-        props: {
-            platform,
-            user_id,
-            creator,
-            error
-        }
-    };
 };
 
 export default Page;
