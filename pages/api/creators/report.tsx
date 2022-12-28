a89f6c68276c287a5ed2e03199e5b99abd71611e
Jacob Cohen-Rosenthal <52448067+jacobcoro@users.noreply.github.com>
2022-12-28 11:48:29 +0800

    Update pages/api/creators/report.tsx
    Co-authored-by: Cristian Bote <cristianbote@users.noreply.github.com>

--- pages/api/creators/report.tsx
+++ pages/api/creators/report.tsx
@@ -35,4 +35,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         return res.status(400).json(null);
     }
+    
+    return res.status(400).json(null)
 }
