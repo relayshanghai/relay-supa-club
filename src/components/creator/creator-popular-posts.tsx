24805c3648ae90cf9abe6caa11e473ef1e1405c6
jacobcoro <xiaokourou@gmail.com>
2022-12-28 12:29:25 +0800

    move component to own file

--- src/components/creator/creator-popular-posts.tsx
+++ src/components/creator/creator-popular-posts.tsx
@@ -1,23 +1,7 @@
 import { useTranslation } from 'react-i18next';
 
-import type { CreatorReport, Post } from 'types';
-import { CreatorPost } from './creator-post';
-
-const Posts = ({ posts, title }: { posts: Post[]; title: string }) => {
-    const postsList = posts.length > 4 ? posts.slice(0, 4) : posts;
-
-    if (postsList.length === 0) return null;
-    return (
-        <div className="p-6">
-            <h2 className="text-gray-600 font-semibold mb-2">{title}</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                {postsList.map((post, index) => (
-                    <CreatorPost key={index} post={post} />
-                ))}
-            </div>
-        </div>
-    );
-};
+import type { CreatorReport } from 'types';
+import { Posts } from './creator-posts';
 
 export const PopularPostsSection = ({ report }: { report: CreatorReport }) => {
     const { t } = useTranslation();
