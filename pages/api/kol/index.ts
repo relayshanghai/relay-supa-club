9cb344fbc1f4ba26b3635c69a6819b4413f52224
jacobcoro <xiaokourou@gmail.com>
2023-01-04 16:05:03 +0800

    remove search usage restriction

--- pages/api/kol/index.ts
+++ pages/api/kol/index.ts
@@ -1,44 +1,41 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { fetchCreatorsFiltered } from 'src/utils/api/iqdata';
-import { supabase } from 'src/utils/supabase-client';
-import { CompanyDB, UsagesDB } from 'types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        const { company_id, user_id, ...searchParams } = JSON.parse(req.body);
+        const {
+            // company_id, user_id,
+            ...searchParams
+        } = JSON.parse(req.body);
 
-        const { data: company, error: companyError } = await supabase
-            .from<CompanyDB>('companies')
-            .select('usage_limit')
-            .eq('id', company_id)
-            .single();
-
-        if (!company || companyError) {
-            return res.status(400).json({ error: 'Company not found' });
-        }
-
-        const limit = Number(company.usage_limit);
-
-        const { data: usagesData, error: usagesError } = await supabase
-            .from<UsagesDB>('usages')
-            .select('id')
-            .eq('company_id', company_id)
-            .eq('type', 'search');
-
-        if (usagesError || (usagesData?.length && usagesData.length >= limit)) {
-            return res.status(400).json({ error: 'Usage limit exceeded' });
-        }
+        // We're not checking for search usages for now, but we might want to in the future
+        // const { data: company, error: companyError } = await supabase
+        //     .from<CompanyDB>('companies')
+        //     .select('usage_limit')
+        //     .eq('id', company_id)
+        //     .single();
+        // if (!company || companyError) {
+        //     return res.status(400).json({ error: 'Company not found' });
+        // }
+        // const limit = Number(company.usage_limit);
+        // const { data: usagesData, error: usagesError } = await supabase
+        //     .from<UsagesDB>('usages')
+        //     .select('id')
+        //     .eq('company_id', company_id)
+        //     .eq('type', 'search');
+        // if (usagesError || (usagesData?.length && usagesData.length >= limit)) {
+        //     return res.status(400).json({ error: 'Usage limit exceeded' });
+        // }
 
         const results = await fetchCreatorsFiltered(searchParams);
-        const usage = {
-            company_id,
-            user_id,
-            type: 'search'
-        };
 
-        const { error } = await supabase.from<UsagesDB>('usages').insert([usage]);
-
-        if (error) return res.status(400).json(error);
+        // const usage = {
+        //     company_id,
+        //     user_id,
+        //     type: 'search'
+        // };
+        // const { error } = await supabase.from<UsagesDB>('usages').insert([usage]);
+        // if (error) return res.status(400).json(error);
 
         return res.status(200).json(results);
     }
