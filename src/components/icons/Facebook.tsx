d9a4c1f7ea994373535ea309a68ee7210d3d9583
jacobcoro <xiaokourou@gmail.com>
2023-02-01 17:05:15 +0800

    fix icon jsx attributes console errors

--- src/components/icons/Facebook.tsx
+++ src/components/icons/Facebook.tsx
@@ -4,13 +4,10 @@ export default function Facebook(props: SVGProps<SVGSVGElement>) {
     return (
         <svg
             version="1.1"
-            id="Layer_1"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
             viewBox="0 0 291.319 291.319"
-            enable-background="new 0 0 291.319 291.319"
+            enableBackground="new 0 0 291.319 291.319"
             xmlSpace="preserve"
             {...props}
         >
