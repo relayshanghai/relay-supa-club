8d18fa9001a3314c7171453d7160b8d11d9d3892
jacobcoro <xiaokourou@gmail.com>
2023-02-08 12:34:39 +0800

    refactor

--- pages/api/company/create-employee.ts
+++ pages/api/company/create-employee.ts
@@ -26,65 +26,64 @@ const relayCompanyConfig = {
     website: 'https://relay.club',
     email: 'tech@relay.club',
 };
+const getOrCreateCompany = async () => {
+    const { data: relayCompany } = await getCompanyByName(relayCompanyConfig.name);
+
+    if (relayCompany) {
+        return { data: relayCompany, error: null };
+    }
+    const { data: companyCreated, error: createRelayCompanyError } = await createCompany({
+        name: relayCompanyConfig.name,
+        website: relayCompanyConfig.website,
+    });
+    if (createRelayCompanyError || !companyCreated?.id) {
+        return { data: null, error: createRelayCompanyError ?? 'Missing company id.' };
+    }
+    const customer = await stripeClient.customers.create({
+        name: relayCompanyConfig.name,
+        email: relayCompanyConfig.email,
+        metadata: {
+            company_id: companyCreated.id,
+        },
+    });
+    await updateCompany({ id: companyCreated.id, cus_id: customer.id });
+    await updateCompanySubscriptionStatus({
+        subscription_status: 'active',
+        id: companyCreated.id,
+        subscription_current_period_end: new Date('2050-1-1').toISOString(),
+        subscription_current_period_start: new Date().toISOString(),
+        subscription_end_date: new Date('2050-1-1').toISOString(),
+        subscription_start_date: new Date().toISOString(),
+    });
+    await updateCompanyUsageLimits({
+        id: companyCreated.id,
+        profiles_limit: '100000000',
+        searches_limit: '1000000000',
+    });
+
+    return { data: companyCreated, error: null };
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
             const { email } = JSON.parse(req.body) as CreateEmployeePostBody;
-
             if (!EMPLOYEE_EMAILS.includes(email)) {
                 return res
                     .status(httpCodes.BAD_REQUEST)
                     .json({ error: createEmployeeError.isNotEmployee });
             }
-            let company: CompanyDB | null = null;
-            // if relay company doesn't exist, create it.
-            const { data: relayCompany } = await getCompanyByName(relayCompanyConfig.name);
-            if (!relayCompany) {
-                const { data: companyCreated, error: createRelayCompanyError } =
-                    await createCompany({
-                        name: relayCompanyConfig.name,
-                        website: relayCompanyConfig.website,
-                    });
-                if (createRelayCompanyError || !companyCreated?.id) {
-                    serverLogger(createRelayCompanyError, 'error');
-                    return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
-                }
-
-                const customer = await stripeClient.customers.create({
-                    name: relayCompanyConfig.name,
-                    email: relayCompanyConfig.email,
-                    metadata: {
-                        company_id: companyCreated.id,
-                    },
-                });
-                await updateCompany({ id: companyCreated.id, cus_id: customer.id });
-                await updateCompanySubscriptionStatus({
-                    subscription_status: 'active',
-                    id: companyCreated.id,
-                    subscription_current_period_end: new Date('2050-1-1').toISOString(),
-                    subscription_current_period_start: new Date().toISOString(),
-                    subscription_end_date: new Date('2050-1-1').toISOString(),
-                    subscription_start_date: new Date().toISOString(),
-                });
-                await updateCompanyUsageLimits({
-                    id: companyCreated.id,
-                    profiles_limit: '100000000',
-                    searches_limit: '1000000000',
-                });
-                company = companyCreated;
-            } else {
-                company = relayCompany;
-            }
+            const { data: company, error: getOrCreateCompanyError } = await getOrCreateCompany();
             if (!company) {
+                serverLogger(getOrCreateCompanyError, 'error');
                 return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
             }
+
             const { data: profile, error: getProfileError } = await getProfileByEmail(email);
             if (getProfileError) {
                 serverLogger(getProfileError, 'error');
                 return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
             }
-
             const { error: makeAdminError } = await updateUserRole(profile.id, 'relay_employee');
             if (makeAdminError) {
                 serverLogger(makeAdminError, 'error');
@@ -99,6 +98,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
         }
     }
-
     return res.status(httpCodes.METHOD_NOT_ALLOWED).json({});
 }
