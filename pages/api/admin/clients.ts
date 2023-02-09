e43fb7043cb40cb1ab3472bd14964ab7926d0e37
jacobcoro <xiaokourou@gmail.com>
2023-02-09 15:39:45 +0800

    remove unused conditional

--- pages/api/admin/clients.ts
+++ pages/api/admin/clients.ts
@@ -14,32 +14,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method !== 'GET') {
         return res.status(httpCodes.METHOD_NOT_ALLOWED).json({});
     }
-    if (req.method === 'GET') {
-        const { data: companies, error: companyError } = await supabase.from('companies').select();
-        const { data: campaigns, error: campaignError } = await supabase.from('campaigns').select();
-        const { data: profiles, error: profileError } = await supabase.from('profiles').select();
-        if (
-            !campaigns ||
-            !companies ||
-            !profiles ||
-            companyError ||
-            profileError ||
-            campaignError
-        ) {
-            return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
-        }
-
-        const result: AdminClientsGetResponse = companies.map((company) => {
-            const companyCampaigns = campaigns.filter(
-                (campaign) => campaign.company_id === company.id,
-            );
-            const companyProfiles = profiles.filter((profile) => profile.company_id === company.id);
-            return {
-                ...company,
-                campaigns: companyCampaigns,
-                profiles: companyProfiles,
-            };
-        });
-        return res.status(httpCodes.OK).json(result);
+    const { data: companies, error: companyError } = await supabase.from('companies').select();
+    const { data: campaigns, error: campaignError } = await supabase.from('campaigns').select();
+    const { data: profiles, error: profileError } = await supabase.from('profiles').select();
+    if (!campaigns || !companies || !profiles || companyError || profileError || campaignError) {
+        return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({});
     }
+
+    const result: AdminClientsGetResponse = companies.map((company) => {
+        const companyCampaigns = campaigns.filter((campaign) => campaign.company_id === company.id);
+        const companyProfiles = profiles.filter((profile) => profile.company_id === company.id);
+        return {
+            ...company,
+            campaigns: companyCampaigns,
+            profiles: companyProfiles,
+        };
+    });
+    return res.status(httpCodes.OK).json(result);
 }
