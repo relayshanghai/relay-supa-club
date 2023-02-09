eb43a934b3e58a8776ca87e067f3c4f74a4947ed
Ellie Wang <yuetingw@outlook.com>
2023-02-09 12:42:05 +0800

    wip: delete notes

--- pages/api/notes/delete.ts
+++ pages/api/notes/delete.ts
@@ -1,22 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import httpCodes from 'src/constants/httpCodes';
 import type { CampaignNotesDB, CampaignNotesInsertDB } from 'src/utils/api/db';
-import { insertCampaignNote } from 'src/utils/api/db';
 import { serverLogger } from 'src/utils/logger';
+import { supabase } from 'src/utils/supabase-client';
 
 export type CampaignNotePostBody = CampaignNotesInsertDB;
 export type CampaignNotePostResponse = CampaignNotesDB;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const data = JSON.parse(req.body);
-        const { data: CampaignNote, error } = await insertCampaignNote(data);
+    if (req.method === 'DELETE') {
+        const { data } = JSON.parse(req.body);
+        const { data: campaignNotes, error } = await supabase
+            .from('campaign_notes')
+            .delete()
+            .eq('id', data.id);
+        // .eq('campaign_creator_notes', campaignCreatorId);
 
         if (error) {
             serverLogger(error, 'error');
         }
 
-        return res.status(httpCodes.OK).json(CampaignNote);
+        return res.status(httpCodes.OK).json(campaignNotes);
     }
 
     return res.status(httpCodes.METHOD_NOT_ALLOWED).json({});
--- src/hooks/use-notes.ts
+++ src/hooks/use-notes.ts
@@ -5,6 +5,7 @@ import { clientLogger } from 'src/utils/logger';
 import { nextFetch, nextFetchWithQueries } from 'src/utils/fetcher';
 import { CampaignNotePostBody, CampaignNotePostResponse } from 'pages/api/notes/create';
 import { CampaignNotesIndexGetQuery, CampaignNotesIndexGetResult } from 'pages/api/notes';
+import { CampaignNotesDB } from 'src/utils/api/db';
 
 export const useNotes = ({ campaignCreatorId }: { campaignCreatorId?: string }) => {
     const [loading, setLoading] = useState<boolean>(false);
@@ -40,9 +41,23 @@ export const useNotes = ({ campaignCreatorId }: { campaignCreatorId?: string }) 
         [profile],
     );
 
+    const deleteNote = useCallback(async (input: CampaignNotesDB) => {
+        setLoading(true);
+        try {
+            await nextFetch('notes/delete', {
+                method: 'delete',
+                body: { id: input.id },
+            });
+        } catch (error: any) {
+            clientLogger(error, 'error');
+            setLoading(false);
+        }
+    }, []);
+
     return {
         campaignNotes,
         loading,
         createNote,
+        deleteNote,
     };
 };
