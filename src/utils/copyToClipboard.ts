07116485b528c9330c890bc2677f4a55e04e119b
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-02-09 05:48:59 +0530

    Updated clipboard function

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,7 +1,11 @@
 import { toast } from 'react-hot-toast';
 
-// COPY TO CLIPBOARD FUNCTION
-export const copyToClipboard = (text: string) => {
+const isClipboardAPISupported = navigator.clipboard && !!navigator.clipboard.writeText;
+
+export const copyToClipboard = (text: string): void => {
+    if (isClipboardAPISupported) {
+        return;
+    }
     navigator.clipboard.writeText(text);
     toast.success('Copied to clipboard');
 };
