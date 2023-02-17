cd6ed134a2380db68ee3a6554715c4031f202597
jacobcoro <xiaokourou@gmail.com>
2023-02-17 15:39:26 +0800

    get rid of edge

--- pages/api/error.ts
+++ pages/api/error.ts
@@ -1,19 +1,16 @@
 import httpCodes from 'src/constants/httpCodes';
 
-export const config = {
-    runtime: 'experimental-edge',
-};
-
 export default async function handler() {
+    const options: ResponseInit = {
+        status: httpCodes.FORBIDDEN,
+        headers: {
+            'content-type': 'application/json',
+        },
+    };
     return new Response(
         JSON.stringify({
             error: 'API Error',
         }),
-        {
-            status: httpCodes.FORBIDDEN,
-            headers: {
-                'content-type': 'application/json',
-            },
-        },
+        options,
     );
 }
