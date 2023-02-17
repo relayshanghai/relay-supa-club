2e662c22ed082a3b4eab11bc6d94373da1223754
jacobcoro <xiaokourou@gmail.com>
2023-02-17 17:04:45 +0800

    turn off supabase in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import type { Session, SupabaseClient } from '@supabase/auth-helpers-nextjs';
+// import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
+// import type { Session, SupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import type { DatabaseWithCustomTypes } from 'types';
@@ -14,123 +14,123 @@ const stripeWebhookAllowlist = ['https://stripe.com/', 'https://hooks.stripe.com
  *
 TODO https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/78: performance improvement. These two database calls might add too much loading time to each request. Consider adding a cache, or adding something to the session object that shows the user has a company and the company has a payment method.
  */
-const getCompanySubscriptionStatus = async (
-    supabase: SupabaseClient<DatabaseWithCustomTypes>,
-    userId: string,
-) => {
-    try {
-        const { data: profile } = await supabase
-            .from('profiles')
-            .select('company_id')
-            .eq('id', userId)
-            .single();
-        if (!profile?.company_id) return { subscriptionStatus: false, subscriptionEndDate: null };
+// const getCompanySubscriptionStatus = async (
+//     supabase: SupabaseClient<DatabaseWithCustomTypes>,
+//     userId: string,
+// ) => {
+//     try {
+//         const { data: profile } = await supabase
+//             .from('profiles')
+//             .select('company_id')
+//             .eq('id', userId)
+//             .single();
+//         if (!profile?.company_id) return { subscriptionStatus: false, subscriptionEndDate: null };
 
-        const { data: company } = await supabase
-            .from('companies')
-            .select('subscription_status, subscription_end_date')
-            .eq('id', profile.company_id)
-            .single();
-        return {
-            subscriptionStatus: company?.subscription_status,
-            subscriptionEndDate: company?.subscription_end_date,
-        };
-    } catch (error) {
-        serverLogger(error, 'error');
-        return { subscriptionStatus: false, subscriptionEndDate: null };
-    }
-};
+//         const { data: company } = await supabase
+//             .from('companies')
+//             .select('subscription_status, subscription_end_date')
+//             .eq('id', profile.company_id)
+//             .single();
+//         return {
+//             subscriptionStatus: company?.subscription_status,
+//             subscriptionEndDate: company?.subscription_end_date,
+//         };
+//     } catch (error) {
+//         serverLogger(error, 'error');
+//         return { subscriptionStatus: false, subscriptionEndDate: null };
+//     }
+// };
 
-const checkOnboardingStatus = async (
-    req: NextRequest,
-    res: NextResponse,
-    session: Session,
-    supabase: SupabaseClient<DatabaseWithCustomTypes>,
-) => {
-    const redirectUrl = req.nextUrl.clone();
+// const checkOnboardingStatus = async (
+//     req: NextRequest,
+//     res: NextResponse,
+//     session: Session,
+//     supabase: SupabaseClient<DatabaseWithCustomTypes>,
+// ) => {
+//     const redirectUrl = req.nextUrl.clone();
 
-    // special case where we require a signed in user to create a company, but we don't want to redirect them to onboarding cause this happens before they are onboarded
-    if (req.nextUrl.pathname === '/api/company/create') {
-        const { user_id } = JSON.parse(await req.text());
-        if (!user_id || user_id !== session.user.id) {
-            return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
-        }
-        return res;
-    }
-    // special case where we require a signed in user to view their profile, but we don't want to redirect them to onboarding cause this happens before they are onboarded
-    if (req.nextUrl.pathname === '/api/profiles' && req.method === 'GET') {
-        // print req queries
-        const id = new URL(req.url).searchParams.get('id');
-        if (!id || id !== session.user.id) {
-            return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
-        }
-        return res;
-    }
-    const { subscriptionStatus, subscriptionEndDate } = await getCompanySubscriptionStatus(
-        supabase,
-        session.user.id,
-    );
-    // if signed up, but no company, redirect to onboarding
-    if (!subscriptionStatus) {
-        if (req.nextUrl.pathname.includes('api')) {
-            return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
-        }
-        if (req.nextUrl.pathname === '/signup/onboarding') return res;
-        redirectUrl.pathname = '/signup/onboarding';
-        return NextResponse.redirect(redirectUrl);
-    }
-    if (subscriptionStatus === 'active' || subscriptionStatus === 'trial') {
-        // if already signed in and has company, when navigating to index or login page, redirect to dashboard
-        if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/login') {
-            redirectUrl.pathname = '/dashboard';
-            return NextResponse.redirect(redirectUrl);
-        }
+//     // special case where we require a signed in user to create a company, but we don't want to redirect them to onboarding cause this happens before they are onboarded
+//     if (req.nextUrl.pathname === '/api/company/create') {
+//         const { user_id } = JSON.parse(await req.text());
+//         if (!user_id || user_id !== session.user.id) {
+//             return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
+//         }
+//         return res;
+//     }
+//     // special case where we require a signed in user to view their profile, but we don't want to redirect them to onboarding cause this happens before they are onboarded
+//     if (req.nextUrl.pathname === '/api/profiles' && req.method === 'GET') {
+//         // print req queries
+//         const id = new URL(req.url).searchParams.get('id');
+//         if (!id || id !== session.user.id) {
+//             return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
+//         }
+//         return res;
+//     }
+//     const { subscriptionStatus, subscriptionEndDate } = await getCompanySubscriptionStatus(
+//         supabase,
+//         session.user.id,
+//     );
+//     // if signed up, but no company, redirect to onboarding
+//     if (!subscriptionStatus) {
+//         if (req.nextUrl.pathname.includes('api')) {
+//             return NextResponse.rewrite(redirectUrl.origin, { status: httpCodes.FORBIDDEN });
+//         }
+//         if (req.nextUrl.pathname === '/signup/onboarding') return res;
+//         redirectUrl.pathname = '/signup/onboarding';
+//         return NextResponse.redirect(redirectUrl);
+//     }
+//     if (subscriptionStatus === 'active' || subscriptionStatus === 'trial') {
+//         // if already signed in and has company, when navigating to index or login page, redirect to dashboard
+//         if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/login') {
+//             redirectUrl.pathname = '/dashboard';
+//             return NextResponse.redirect(redirectUrl);
+//         }
 
-        // Authentication successful, forward request to protected route.
-        return res;
-    }
-    if (subscriptionStatus === 'canceled') {
-        // if subscription ended only allow access to account page, and subscription endpoints
-        const allowedPaths = [
-            '/signup',
-            '/login',
-            '/account',
-            '/api/subscriptions',
-            '/api/subscriptions/payment-method',
-            '/api/subscriptions/portal',
-            '/api/subscriptions/create',
-            '/api/company',
-            '/pricing',
-        ];
-        if (allowedPaths.some((path) => req.nextUrl.pathname === path)) return res;
-        else {
-            if (!subscriptionEndDate) {
-                redirectUrl.pathname = '/account';
-                return NextResponse.redirect(redirectUrl);
-            }
+//         // Authentication successful, forward request to protected route.
+//         return res;
+//     }
+//     if (subscriptionStatus === 'canceled') {
+//         // if subscription ended only allow access to account page, and subscription endpoints
+//         const allowedPaths = [
+//             '/signup',
+//             '/login',
+//             '/account',
+//             '/api/subscriptions',
+//             '/api/subscriptions/payment-method',
+//             '/api/subscriptions/portal',
+//             '/api/subscriptions/create',
+//             '/api/company',
+//             '/pricing',
+//         ];
+//         if (allowedPaths.some((path) => req.nextUrl.pathname === path)) return res;
+//         else {
+//             if (!subscriptionEndDate) {
+//                 redirectUrl.pathname = '/account';
+//                 return NextResponse.redirect(redirectUrl);
+//             }
 
-            const endDate = new Date(subscriptionEndDate);
-            if (endDate < new Date()) {
-                redirectUrl.pathname = '/account';
-                return NextResponse.redirect(redirectUrl);
-            } else return res;
-        }
-    }
+//             const endDate = new Date(subscriptionEndDate);
+//             if (endDate < new Date()) {
+//                 redirectUrl.pathname = '/account';
+//                 return NextResponse.redirect(redirectUrl);
+//             } else return res;
+//         }
+//     }
 
-    // if company registered, but no payment method, redirect to payment onboarding
-    if (subscriptionStatus === 'awaiting_payment_method') {
-        // allow the endpoints payment onboarding page requires
-        if (
-            req.nextUrl.pathname.includes('/api/company') ||
-            req.nextUrl.pathname.includes('/api/subscriptions')
-        )
-            return res;
+//     // if company registered, but no payment method, redirect to payment onboarding
+//     if (subscriptionStatus === 'awaiting_payment_method') {
+//         // allow the endpoints payment onboarding page requires
+//         if (
+//             req.nextUrl.pathname.includes('/api/company') ||
+//             req.nextUrl.pathname.includes('/api/subscriptions')
+//         )
+//             return res;
 
-        if (req.nextUrl.pathname.includes('/signup/payment-onboard')) return res;
-        redirectUrl.pathname = '/signup/payment-onboard';
-        return NextResponse.redirect(redirectUrl);
-    }
-};
+//         if (req.nextUrl.pathname.includes('/signup/payment-onboard')) return res;
+//         redirectUrl.pathname = '/signup/payment-onboard';
+//         return NextResponse.redirect(redirectUrl);
+//     }
+// };
 
 /** Special case: we need to be able to access this from the marketing page, so we need to allow CORS */
 const allowPricingCors = (req: NextRequest, res: NextResponse) => {
@@ -170,18 +170,20 @@ export async function middleware(req: NextRequest) {
     if (req.nextUrl.pathname === '/api/subscriptions/prices') return allowPricingCors(req, res);
     if (req.nextUrl.pathname === '/api/subscriptions/webhook') return allowStripeCors(req, res);
 
-    // Create authenticated Supabase Client.
-    const supabase = createMiddlewareSupabaseClient<DatabaseWithCustomTypes>({ req, res });
-    const { data: authData } = await supabase.auth.getSession();
-    if (req.nextUrl.pathname.includes('/admin')) {
-        if (!authData.session?.user?.email) {
-            return NextResponse.rewrite(req.nextUrl.origin, { status: httpCodes.FORBIDDEN });
-        }
-        return await checkIsRelayEmployee(res, authData.session.user.email);
-    }
+    return res;
 
-    if (authData.session?.user?.email)
-        return await checkOnboardingStatus(req, res, authData.session, supabase);
+    // Create authenticated Supabase Client.
+    // const supabase = createMiddlewareSupabaseClient<DatabaseWithCustomTypes>({ req, res });
+    // const { data: authData } = await supabase.auth.getSession();
+    // if (req.nextUrl.pathname.includes('/admin')) {
+    //     if (!authData.session?.user?.email) {
+    //         return NextResponse.rewrite(req.nextUrl.origin, { status: httpCodes.FORBIDDEN });
+    //     }
+    //     return await checkIsRelayEmployee(res, authData.session.user.email);
+    // }
+
+    // if (authData.session?.user?.email)
+    //     return await checkOnboardingStatus(req, res, authData.session, supabase);
 
     // not logged in -- api requests, just return an error
     if (req.nextUrl.pathname.includes('api'))
