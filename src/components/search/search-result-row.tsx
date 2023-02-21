f1539f1c323f63a80d60a2efddd733bf08e6c8bc
jacobcoro <xiaokourou@gmail.com>
2023-02-21 11:43:48 +0800

    use number formatter on numbers, decimal on decimals

--- src/components/search/search-result-row.tsx
+++ src/components/search/search-result-row.tsx
@@ -4,7 +4,7 @@ import { Button } from 'src/components/button';
 import { ShareLink } from 'src/components/icons';
 import { useSearch } from 'src/hooks/use-search';
 import { imgProxy } from 'src/utils/fetcher';
-import { formatter } from 'src/utils/formatter';
+import { decimalToPercent, numberFormatter } from 'src/utils/formatter';
 import { CreatorSearchAccountObject } from 'types';
 
 export const SearchResultRow = ({
@@ -19,12 +19,19 @@ export const SearchResultRow = ({
 }) => {
     const { t } = useTranslation();
     const { platform } = useSearch();
-    const handle =
-        creator.account.user_profile.username ||
-        creator.account.user_profile.custom_name ||
-        creator.account.user_profile.fullname ||
-        '';
-
+    const {
+        username,
+        custom_name,
+        fullname,
+        user_id,
+        url,
+        picture,
+        followers,
+        engagements,
+        engagement_rate,
+        avg_views,
+    } = creator.account.user_profile;
+    const handle = username || custom_name || fullname || '';
     const addToCampaign = () => {
         setShowCampaignListModal(true);
         if (creator) setSelectedCreator(creator);
@@ -38,21 +45,14 @@ export const SearchResultRow = ({
                         {t('creators.addToCampaign')}
                     </Button>
                     <Button>
-                        <Link
-                            href={`/influencer/${platform}/${creator.account.user_profile.user_id}`}
-                            target="_blank"
-                        >
+                        <Link href={`/influencer/${platform}/${user_id}`} target="_blank">
                             {t('creators.analyzeProfile')}
                         </Link>
                     </Button>
 
-                    {creator.account.user_profile.url && (
+                    {url && (
                         <Button>
-                            <Link
-                                href={creator.account.user_profile.url}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
+                            <Link href={url} target="_blank" rel="noopener noreferrer">
                                 <ShareLink className="w-3.5 h-3.5 fill-current text-white" />
                             </Link>
                         </Button>
@@ -61,24 +61,18 @@ export const SearchResultRow = ({
             </td>
 
             <td className="py-2 px-4 flex flex-row items-center space-x-2 min-w-min">
-                <img
-                    src={imgProxy(creator.account.user_profile.picture) as string}
-                    className="w-12 h-12"
-                    alt={handle}
-                />
+                <img src={imgProxy(picture) as string} className="w-12 h-12" alt={handle} />
                 <div>
-                    <div className="font-bold line-clamp-2">
-                        {creator.account.user_profile.fullname}
-                    </div>
+                    <div className="font-bold line-clamp-2">{fullname}</div>
                     <div className="text-primary-500 text-sm line-clamp-1">
                         {handle ? `@${handle}` : null}
                     </div>
                 </div>
             </td>
-            <td className="text-sm">{formatter(creator.account.user_profile.followers)}</td>
-            <td className="text-sm">{formatter(creator.account.user_profile.engagements)}</td>
-            <td className="text-sm">{formatter(creator.account.user_profile.engagement_rate)}</td>
-            <td className="text-sm">{formatter(creator.account.user_profile.avg_views)}</td>
+            <td className="text-sm">{numberFormatter(followers) ?? '-'}</td>
+            <td className="text-sm">{numberFormatter(engagements) ?? '-'}</td>
+            <td className="text-sm">{decimalToPercent(engagement_rate) ?? '-'}</td>
+            <td className="text-sm">{numberFormatter(avg_views) ?? '-'}</td>
         </tr>
     );
 };
