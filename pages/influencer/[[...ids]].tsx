bf73465519212f4bb455c1e1c452d548d194ef7a
Ellie Wang <yuetingw@outlook.com>
2023-02-21 10:02:13 +0800

    added isReady

--- pages/influencer/[[...ids]].tsx
+++ pages/influencer/[[...ids]].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ErrorPopover } from 'src/components/common/error-popover';
 import { CreatorPage } from 'src/components/creator/creator-page';
@@ -9,23 +10,28 @@ const Page = () => {
     const router = useRouter();
     const { ids } = router.query;
     const { t } = useTranslation();
+    const [routing, setRouting] = useState(true);
+
+    useEffect(() => {
+        if (router.isReady) {
+            setRouting(false);
+        }
+    }, [router.isReady]);
 
     return (
         <Layout>
-            {ids ? (
-                !ids || !Array.isArray(ids) || ids?.length < 2 ? (
-                    <div className="relative p-6">
-                        <ErrorPopover
-                            errorMessage={'Invalid creator id or platform'}
-                            buttonText={t('website.back') || ''}
-                            buttonAction={() => router.back()}
-                        />
-                    </div>
-                ) : (
-                    <CreatorPage platform={ids[0] as any} creator_id={ids[1]} />
-                )
-            ) : (
+            {routing ? (
                 <Spinner className="mx-auto mt-10 w-10 h-10 fill-primary-600 text-white" />
+            ) : !ids || !Array.isArray(ids) || ids?.length < 2 ? (
+                <div className="relative p-6">
+                    <ErrorPopover
+                        errorMessage={'Invalid creator id or platform'}
+                        buttonText={t('website.back') || ''}
+                        buttonAction={() => router.back()}
+                    />
+                </div>
+            ) : (
+                <CreatorPage platform={ids[0] as any} creator_id={ids[1]} />
             )}
         </Layout>
     );
