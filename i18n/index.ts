2d8752fa2714f8f387d10d8038f017c69462d3f4
Ellie Wang <yuetingw@outlook.com>
2023-02-22 12:26:21 +0800

    change default language to chinese

--- i18n/index.ts
+++ i18n/index.ts
@@ -16,8 +16,8 @@ i18n.use(LanguageDetector)
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         resources,
-        lng: 'en-US',
-        fallbackLng: 'en-US', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+        lng: 'zh-cn',
+        fallbackLng: { default: ['zh'] }, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
         // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
         // if you're using a language detector, do not define the lng option
 
