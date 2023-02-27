855b98fa11709c7700c026700b0cce7fd8f79b49
Ellie Wang <yuetingw@outlook.com>
2023-02-27 18:43:11 +0800

    pinned messages ui

--- src/components/campaigns/comment-cards.tsx
+++ src/components/campaigns/comment-cards.tsx
@@ -21,16 +21,33 @@ export default function CommentCards({
         commentsEndRef.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
+    const importantMessages = () => {
+        const pinnedMessages = campaignCreatorNotes?.filter((note) => note.important === true);
+        console.log(pinnedMessages);
+        if (pinnedMessages.length > 0)
+            return (
+                <div className="fixed z-20 w-full bg-gray-100 p-2 ">
+                    <div className=" relative w-fit rounded-lg border border-primary-200 bg-primary-100 py-1 px-4 text-primary-500 hover:cursor-pointer">
+                        {t('campaigns.show.importantMessages')}
+                        <div className="absolute -top-1 -right-2 z-40 h-4 w-4 rounded-full bg-red-500 text-center text-white">
+                            {pinnedMessages.length}
+                        </div>
+                    </div>
+                </div>
+            );
+    };
+
     useEffect(() => {
         scrollToBottom();
     }, [campaignCreatorNotes]);
 
     return (
-        <div className=" text-xs w-full h-96 flex flex-col overflow-y-auto">
+        <div className=" flex h-96 w-full flex-col overflow-y-auto text-xs">
             {isLoading ? (
                 <CommentCardsSkeleton />
             ) : (
                 <>
+                    {importantMessages()}
                     {campaignCreatorNotes?.length > 0 ? (
                         <>
                             <>
@@ -40,7 +57,7 @@ export default function CommentCards({
                             </>
                         </>
                     ) : (
-                        <div className="text-center text-gray-500 mt-4">
+                        <div className="mt-4 text-center text-gray-500">
                             {t('campaigns.creatorModal.commentsDescr')}
                         </div>
                     )}
