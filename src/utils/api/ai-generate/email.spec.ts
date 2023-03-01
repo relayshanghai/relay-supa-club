df47c67244ff6d3a9d65f4e7867daf9a974bcfa0
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-03-01 09:41:44 +0530

    Added test file

---
+++ src/utils/api/ai-generate/email.spec.ts
@@ -0,0 +1,84 @@
+import { describe } from 'node:test';
+import { generatePrompt, MAX_CHARACTER_LENGTH } from './email';
+
+describe('generatePrompt', () => {
+    test('returns error message when required fields are missing', () => {
+        const result = generatePrompt({
+            brandName: '',
+            company_id: '',
+            influencerName: '',
+            productDescription: '',
+            productName: '',
+            senderName: '',
+            user_id: '',
+        });
+        expect(result.status).toBe('error');
+        expect(result.message).toBe('Missing required fields!');
+    });
+
+    test('returns error message when wrong character length is provided', () => {
+        const data = {
+            brandName: 'BrandName',
+            company_id: 'company_id',
+            influencerName: 'InfluencerName',
+            productDescription: 'ProductDescription',
+            productName: 'ProductName',
+            senderName: 'SenderName',
+            user_id: 'user_id',
+            instructions: 'a'.repeat(MAX_CHARACTER_LENGTH + 1), // provide string greater than the MAX_CHARACTER_LENGTH
+        };
+        const result = generatePrompt(data);
+        expect(result.status).toBe('error');
+        expect(result.message).toBe('Wrong character length provided');
+    });
+
+    test('returns prompt when all required fields are provided', () => {
+        const data = {
+            brandName: 'BrandName',
+            company_id: 'company_id',
+            influencerName: 'InfluencerName',
+            productDescription: 'ProductDescription',
+            productName: 'ProductName',
+            senderName: 'SenderName',
+            user_id: 'user_id',
+        };
+        const result = generatePrompt(data);
+        expect(result.status).toBe('success');
+        expect(result.message).toBeDefined();
+    });
+
+    test('returns proper prompt message when all fields are provided', () => {
+        const data = {
+            brandName: 'BrandName',
+            company_id: 'company_id',
+            influencerName: 'InfluencerName',
+            productDescription: 'ProductDescription',
+            productName: 'ProductName',
+            senderName: 'SenderName',
+            user_id: 'user_id',
+            instructions: 'Follow these instructions',
+        };
+        const result = generatePrompt(data);
+        expect(result.status).toBe('success');
+        expect(result.message).toBe(
+            "Write an email (without subject) to InfluencerName with the following content:\n\t1) Express our brand BrandName's interest in participating with InfluencerName on a product marketing campaign.\n\t2) Express that I love their content and appreciate their creativity.\n\t3) Enthusiastically introduce our product: BrandName ProductName. ProductDescription.\n\t4) Ask InfluencerName to follow these instructions: Follow these instructions.\n\t5) Express gratitude for InfluencerName's time and consideration, and end with a call-to-action for them to respond if they are interested in the collaboration.\n\t6) Sign with the name: SenderName",
+        );
+    });
+
+    test('returns proper prompt message when all fields are provided without instructions', () => {
+        const data = {
+            brandName: 'BrandName',
+            company_id: 'company_id',
+            influencerName: 'InfluencerName',
+            productDescription: 'ProductDescription',
+            productName: 'ProductName',
+            senderName: 'SenderName',
+            user_id: 'user_id',
+        };
+        const result = generatePrompt(data);
+        expect(result.status).toBe('success');
+        expect(result.message).toBe(
+            "Write an email (without subject) to InfluencerName with the following content:\n\t1) Express our brand BrandName's interest in participating with InfluencerName on a product marketing campaign.\n\t2) Express that I love their content and appreciate their creativity.\n\t3) Enthusiastically introduce our product: BrandName ProductName. ProductDescription.\n\t4) Ask the influencer to post about the product on their social media.\n\t5) Express gratitude for InfluencerName's time and consideration, and end with a call-to-action for them to respond if they are interested in the collaboration.\n\t6) Sign with the name: SenderName",
+        );
+    });
+});
---
+++ src/utils/api/ai-generate/email.ts
@@ -0,0 +1,92 @@
+export interface AIEmailGeneratorPostBody {
+    brandName: string;
+    influencerName: string;
+    productName: string;
+    productDescription: string;
+    instructions?: string;
+    senderName: string;
+    company_id: string;
+    user_id: string;
+}
+
+export const MAX_CHARACTER_LENGTH = 600;
+
+export const generatePrompt = ({
+    brandName,
+    company_id,
+    influencerName,
+    productDescription,
+    productName,
+    senderName,
+    user_id,
+    instructions,
+}: AIEmailGeneratorPostBody) => {
+    if (
+        !brandName ||
+        !influencerName ||
+        !productDescription ||
+        !productName ||
+        !senderName ||
+        !company_id ||
+        !user_id
+    ) {
+        return {
+            status: 'error',
+            message: 'Missing required fields!',
+        };
+    }
+
+    if (
+        brandName.length === 0 ||
+        influencerName.length === 0 ||
+        productDescription.length === 0 ||
+        productName.length === 0 ||
+        senderName.length === 0
+    ) {
+        return {
+            status: 'error',
+            message: 'Missing required fields!',
+        };
+    }
+
+    if (
+        brandName.length > 100 ||
+        influencerName.length > 100 ||
+        productName.length > 100 ||
+        senderName.length > 100 ||
+        productDescription.length > MAX_CHARACTER_LENGTH ||
+        (instructions && instructions.length > MAX_CHARACTER_LENGTH)
+    ) {
+        return {
+            status: 'error',
+            message: 'Wrong character length provided',
+        };
+    }
+
+    const trimmedDescription = productDescription.trim();
+    const trimDescriptionPunctuation = trimmedDescription.endsWith('.')
+        ? trimmedDescription.slice(0, trimmedDescription.length - 1)
+        : trimmedDescription;
+
+    const trimmedInstructions = instructions?.trim();
+    const trimmedInstructionsPunctuation = trimmedInstructions?.endsWith('.')
+        ? trimmedInstructions?.slice(0, trimmedInstructions?.length - 1)
+        : trimmedInstructions;
+
+    const prompt = `Write an email (without subject) to ${influencerName} with the following content:
+	1) Express our brand ${brandName}'s interest in participating with ${influencerName} on a product marketing campaign.
+	2) Express that I love their content and appreciate their creativity.
+	3) Enthusiastically introduce our product: ${brandName} ${productName}. ${trimDescriptionPunctuation}.
+	${
+        instructions
+            ? `4) Ask ${influencerName} to follow these instructions: ${trimmedInstructionsPunctuation}.`
+            : '4) Ask the influencer to post about the product on their social media.'
+    }
+	5) Express gratitude for ${influencerName}'s time and consideration, and end with a call-to-action for them to respond if they are interested in the collaboration.
+	6) Sign with the name: ${senderName}`;
+
+    return {
+        status: 'success',
+        message: prompt,
+    };
+};
