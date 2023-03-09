b3984903cc50fc26e3360ee3e15eb553cdc659e3
jacobcoro <xiaokourou@gmail.com>
2023-03-09 16:55:10 +0800

    finish helpers

--- supabase/tests/database/01-relay_test_helpers.sql
+++ supabase/tests/database/01-relay_test_helpers.sql
@@ -1,4 +1,24 @@
--- we'll want to often create a company owner user, a non owner user in the same company, a user in a diffreent company, and a relay employee user. 
+CREATE
+OR REPLACE FUNCTION tests.create_profile (
+  email TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  user_role TEXT,
+  company_id UUID
+) RETURNS UUID AS $$
+DECLARE
+  user_id uuid;
+BEGIN
+  user_id := tests.create_supabase_user (email);
+  INSERT INTO profiles (id, email, last_name, first_name, user_role, company_id)
+  VALUES (user_id, email, last_name, first_name, user_role, company_id);
+  RETURN user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE
+OR REPLACE FUNCTION tests.create_company (NAME TEXT) RETURNS UUID AS $$ DECLARE company_id uuid; BEGIN company_id := uuid_generate_v4 (); INSERT INTO companies (id, NAME) VALUES (company_id, name); RETURN company_id; END; $$ LANGUAGE plpgsql;
+
 CREATE
 OR REPLACE FUNCTION tests.create_test_users () RETURNS void AS $$
 DECLARE
@@ -25,54 +45,133 @@ DECLARE
   relay_employee_email text;
   relay_employee_id uuid;
 BEGIN
-  -- create a company
-  company_name := 'test company';
-  company_id := uuid_generate_v4 ();
-  INSERT INTO companies (id, NAME)
-  VALUES (company_id, company_name);
+  company_owner_email := 'owner';
+  company_member_email := 'member';
+  company2_member_email := 'company2-member';
+  relay_employee_email := 'relay-employee';
 
-  -- create a company owner
-  company_owner_first_name := 'company owner first_name';
-  company_owner_last_name := 'company owner last_name';
-  company_owner_email := 'company@owner.email';
-  company_owner_id := tests.create_supabase_user (company_owner_email);
-    INSERT INTO profiles (id, email, last_name, first_name, user_role, company_id);
-  VALUES (company_owner_id, company_owner_email, company_owner_last_name, company_owner_first_name, 'company_owner', company_id);
-
-  -- create a company member
-  company_member_first_name := 'company member first_name';
-  company_member_last_name := 'company member last_name';
-  company_member_email := 'company@member.email';
-  company_member_id := tests.create_supabase_user (company_member_email);
-    INSERT INTO profiles (id, email, last_name, first_name, user_role, company_id) 
-  VALUES (company_member_id, company_member_email, company_member_last_name, company_member_first_name, 'company_member', company_id);
-
-  -- create a second company
-  company2_name := 'test company 2';
-  company2_id := uuid_generate_v4 ();
-  INSERT INTO companies (id, NAME)
-  VALUES (company2_id, company2_name);
-
-  -- create a company2 member
-  company2_member_first_name := 'company2 member first_name';
-  company2_member_last_name := 'company2 member last_name';
-  company2_member_email := 'company2@member.email';
-  company2_member_id := tests.create_supabase_user (company2_member_email);
-    INSERT INTO profiles (id, email, last_name, first_name, user_role, company_id)
-  VALUES (company2_member_id, company2_member_email, company2_member_last_name, company2_member_first_name, 'company_member', company2_id);
-
-  -- create a relay company
+  company_name := 'company1';
+  company2_name := 'company2';
   relay_company_name := 'relay.club';
-  relay_company_id := uuid_generate_v4 ();
-  INSERT INTO companies (id, NAME)
-  VALUES (relay_company_id, relay_company_name);
 
-  -- create a relay employee
-  relay_employee_first_name := 'relay employee first_name';
-  relay_employee_last_name := 'relay employee last_name';
-  relay_employee_email := 'relay@employee.email';
-  relay_employee_id := tests.create_supabase_user (relay_employee_email)
-    INSERT INTO profiles (id, email, last_name, first_name, user_role, company_id)
-  VALUES (relay_employee_id, relay_employee_email, relay_employee_last_name, relay_employee_first_name, 'relay_employee', relay_company_id);
+  company_owner_first_name := 'owner-first-name';
+  company_owner_last_name :=  'owner-last-name';
+  company_member_first_name :=  'member-first-name';
+  company_member_last_name :=  'member-last-name';
+  company2_member_first_name :=  'company2-member-first-name';
+  company2_member_last_name :=  'company2-member-last-name';
+  relay_employee_first_name :=  'relay-employee-first-name';
+  relay_employee_last_name :=  'relay-employee-last-name';
+
+  company_id := tests.create_company (company_name);
+  company2_id := tests.create_company (company2_name);
+  relay_company_id := tests.create_company (relay_company_name);
+
+  company_owner_id := tests.create_profile (
+    company_owner_email,
+    company_owner_first_name,
+    company_owner_last_name,
+    'company_owner',
+    company_id
+  );
+  company_member_id := tests.create_profile (
+    company_member_email,
+    company_member_first_name,
+    company_member_last_name,
+    'company_teammate',
+    company_id
+  );
+  company2_member_id := tests.create_profile (
+    company2_member_email,
+    company2_member_first_name,
+    company2_member_last_name,
+    'company_teammate',
+    company2_id
+  );
+  relay_employee_id := tests.create_profile (
+    relay_employee_email,
+    relay_employee_first_name,
+    relay_employee_last_name,
+    'relay_employee',
+    relay_company_id
+  );  
 END;
 $$ LANGUAGE plpgsql;
+
+SELECT
+  tests.create_test_users ();
+
+SELECT
+  plan (5);
+
+SELECT
+  IS (
+    (
+      SELECT
+        first_name
+      FROM
+        profiles
+      WHERE
+        id = tests.get_supabase_uid ('owner')
+    ),
+    'owner-first-name'
+  );
+
+SELECT
+  IS (
+    (
+      SELECT
+        last_name
+      FROM
+        profiles
+      WHERE
+        id = tests.get_supabase_uid ('owner')
+    ),
+    'owner-last-name'
+  );
+
+SELECT
+  IS (
+    (
+      SELECT
+        user_role
+      FROM
+        profiles
+      WHERE
+        id = tests.get_supabase_uid ('owner')
+    ),
+    'company_owner'
+  );
+
+SELECT
+  IS (
+    (
+      SELECT
+        user_role
+      FROM
+        profiles
+      WHERE
+        id = tests.get_supabase_uid ('member')
+    ),
+    'company_teammate'
+  );
+
+SELECT
+  IS (
+    (
+      SELECT
+        user_role
+      FROM
+        profiles
+      WHERE
+        id = tests.get_supabase_uid ('relay-employee')
+    ),
+    'relay_employee'
+  );
+
+SELECT
+  *
+FROM
+  finish ();
+
+ROLLBACK;
