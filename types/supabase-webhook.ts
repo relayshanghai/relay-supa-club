c02d7e7fc93461e31fda1744342beb7dcc8cfc37
Ellie Wang <yuetingw@outlook.com>
2023-03-13 16:22:32 +0800

    wip adding types

---
+++ types/supabase-webhook.ts
@@ -0,0 +1,21 @@
+// export type InsertPayload = {
+//     type: 'INSERT';
+//     table: string;
+//     schema: string;
+//     record: TableRecord<T>;
+//     old_record: null;
+// };
+// export type UpdatePayload = {
+//     type: 'UPDATE';
+//     table: string;
+//     schema: string;
+//     record: TableRecord<T>;
+//     old_record: TableRecord<T>;
+// };
+// export type DeletePayload = {
+//     type: 'DELETE';
+//     table: string;
+//     schema: string;
+//     record: null;
+//     old_record: TableRecord<T>;
+// };
