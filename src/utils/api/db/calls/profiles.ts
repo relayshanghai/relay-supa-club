66c0b8b9803744b805e7df35c66c1e7036f6d286
jacobcoro <xiaokourou@gmail.com>
2023-03-13 12:27:48 +0800

    superuser update/insert

--- src/utils/api/db/calls/profiles.ts
+++ src/utils/api/db/calls/profiles.ts
@@ -6,14 +6,19 @@ import type { ProfileDBUpdate, ProfileDBInsert } from '../types';
 export const insertProfile = (insert: ProfileDBInsert) => {
     const { role: _filter_out, ...insertData } = insert;
     insertData.updated_at = new Date().toISOString();
-    supabase.from('profiles').insert(insertData).select().single();
+    superuserSupabaseCLient.from('profiles').insert(insertData).select().single();
 };
 
 /** updates profile but does not allow changing of role status, automatically updates `updated_at` field */
 export const updateProfile = (update: ProfileDBUpdate) => {
     const { role: _filter_out, ...updateData } = update;
     updateData.updated_at = new Date().toISOString();
-    return supabase.from('profiles').update(updateData).eq('id', updateData.id).select().single();
+    return superuserSupabaseCLient
+        .from('profiles')
+        .update(updateData)
+        .eq('id', updateData.id)
+        .select()
+        .single();
 };
 
 /** called using the superuser client */
