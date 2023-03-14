9df9d7646cddbfda0bd7d4b1c1cb45a0b3ecc8e4
James <james@relay.club>
2023-03-14 10:23:26 +0800

    Apply some fixes in db script

--- supabase/tests/database/zzzzz-cleanup_helpers.sql
+++ supabase/tests/database/zzzzz-cleanup_helpers.sql
@@ -1,10 +1,11 @@
 -- clean up usebasejump test helpers
-DROP FUNCTION IF EXISTS tests.create_supabase_user();
-DROP FUNCTION IF EXISTS tests.get_supabase_uid();
-DROP FUNCTION IF EXISTS tests.get_supabase_user();
-DROP FUNCTION IF EXISTS tests.authenticate_as();
-DROP FUNCTION IF EXISTS tests.clear_authentication();
-DROP FUNCTION IF EXISTS tests.rls_enabled();
+DROP FUNCTION IF EXISTS tests.create_supabase_user(text, text, text);
+DROP FUNCTION IF EXISTS tests.get_supabase_uid(text);
+DROP FUNCTION IF EXISTS tests.get_supabase_user(text);
+DROP FUNCTION IF EXISTS tests.authenticate_as(text);
+DROP FUNCTION IF EXISTS tests.clear_authentication;
+DROP FUNCTION IF EXISTS tests.rls_enabled(text);
+DROP FUNCTION IF EXISTS tests.rls_enabled(text, text);
 
 -- for some reason, drop function does not seem to work. force it with cascade
 DROP SCHEMA tests CASCADE;
