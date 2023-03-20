a88a0937e85347a0afc1fdf0b7b5c52963e9459a
James <james@relay.club>
2023-03-20 12:17:36 +0800

    Fix db snapshot
    - Test anonymous user in is_employee() db function
    - Remove pgtap extension
    - Fix supabase types
    - Fix db snapshot (remote_commit file)
    - Run custom test command in gh workflow

--- supabase/tests/database/is_employee.test.sql
+++ supabase/tests/database/is_employee.test.sql
@@ -1,7 +1,7 @@
 -- Functions directory: /tmp/supabase/functions
 -- Policies directory: /tmp/supabase/policies
 BEGIN;
-SELECT plan(3);
+SELECT plan(4);
 
 -- start includes
 \include /tmp/supabase/functions/is_employee.sql
@@ -9,19 +9,31 @@ SELECT plan(3);
 
 SELECT has_function('relay_is_employee');
 
-SELECT tests.authenticate_as('jacob@relay.club');
-SELECT is(
-  relay_is_employee(),
-  true,
-  'jacob@relay.club IS a Relay employee'
-);
+-- test anonymous
+  SELECT tests.clear_authentication();
+  SELECT is(
+    relay_is_employee(),
+    null,
+    'Anonymous IS NOT a Relay employee'
+  );
 
-SELECT tests.authenticate_as('owner@email.com');
-SELECT is(
-  relay_is_employee(),
-  false,
-  'owner@email.com IS NOT a Relay employee'
-);
+-- test relay user
+  SELECT tests.authenticate_as('jacob@relay.club');
+  SELECT is(
+    relay_is_employee(),
+    true,
+    'jacob@relay.club IS a Relay employee'
+  );
+
+
+-- test basic user
+  SELECT tests.authenticate_as('owner@email.com');
+  SELECT is(
+    relay_is_employee(),
+    false,
+    'owner@email.com IS NOT a Relay employee'
+  );
+
 
 SELECT * FROM finish();
 ROLLBACK;
