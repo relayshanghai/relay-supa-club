8c02b4f3b42ec8bafef7cacaccf9b69bcc26f948
jacobcoro <xiaokourou@gmail.com>
2023-03-23 17:02:11 +0800

    add error components

---
+++ pages/api/make-error.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { serverLogger } from 'src/utils/logger';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    serverLogger('server logger error', 'error');
+    serverLogger('server logger info', 'info');
+    serverLogger('server logger warning', 'warn');
+    res.status(500).json({ message: 'Server error' });
+}
