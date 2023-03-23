0042d73ec921dded6f7b2403f56bd45ad69df3d6
jacobcoro <xiaokourou@gmail.com>
2023-03-23 13:15:40 +0800

    make error button

--- pages/make-error.tsx
+++ pages/make-error.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect } from 'react';
+import { Button } from 'src/components/button';
 import { nextFetch } from 'src/utils/fetcher';
 import { clientLogger } from 'src/utils/logger';
 
@@ -6,16 +8,20 @@ export default function MakeErrorPage() {
     // useEffect(() => {
     //     throw new Error('Error from MakeErrorPage');
     // }, []);
-    useEffect(() => {
-        clientLogger('clientLogger error', 'error');
-        clientLogger('clientLogger info', 'info');
-        clientLogger('clientLogger warning', 'warning');
-        nextFetch('make-error');
-    }, []);
+    // useEffect(() => {
+    //     clientLogger('clientLogger error', 'error');
+    //     clientLogger('clientLogger info', 'info');
+    //     clientLogger('clientLogger warning', 'warning');
+    //     nextFetch('make-error');
+    // }, []);
+    const makeError = () => {
+        throw new Error('Error from MakeErrorPage');
+    };
     return (
         <div>
             <h1>Make Error Page</h1>
             <p>See the console for the error.</p>
+            <Button onClick={makeError}>Make error</Button>
         </div>
     );
 }
