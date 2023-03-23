ecd235f485db96fc2ebfdd568eb06a2dfb0d8cca
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-03-23 11:24:41 +0530

    Updated chinese translations

--- i18n/zh/campaigns.ts
+++ i18n/zh/campaigns.ts
@@ -33,6 +33,9 @@ const campaigns = {
         addedSuccessfully: 'KOL已成功添加到项目',
         deletedSuccessfully: 'KOL已从项目中删除',
         deleteConfirmation: '您确定要从项目中删除此KOL吗？',
+        doNotAdd: '不能添加',
+        addAnyway: '总是添加',
+        influencerAlreadyAdded: '这个KOL已被添加到其他活动:',
     },
     creatorModal: {
         outreach: 'KOL沟通',
