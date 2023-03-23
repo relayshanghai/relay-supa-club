7cb37407932d57741e81a94f0f81144d900427cc
jacobcoro <xiaokourou@gmail.com>
2023-03-23 17:02:11 +0800

    add error components

---
+++ pages/make-error.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect } from 'react';
+import { Button } from 'src/components/button';
+import { nextFetch } from 'src/utils/fetcher';
+import { clientLogger } from 'src/utils/logger';
+
+export default function MakeErrorPage() {
+    // useEffect(() => {
+    //     throw new Error('Error from MakeErrorPage');
+    // }, []);
+    // useEffect(() => {
+    //     clientLogger('clientLogger error', 'error');
+    //     clientLogger('clientLogger info', 'info');
+    //     clientLogger('clientLogger warning', 'warning');
+    //     nextFetch('make-error');
+    // }, []);
+    const makeError = () => {
+        throw new Error('Error from MakeErrorPage');
+    };
+    return (
+        <div>
+            <h1>Make Error Page</h1>
+            <p>See the console for the error.</p>
+            <Button onClick={makeError}>Make error</Button>
+        </div>
+    );
+}
