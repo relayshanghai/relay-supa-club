6cd1ef22360fc28ee5c652ade8e7891cf8817cd5
jacobcoro <xiaokourou@gmail.com>
2023-03-23 12:00:14 +0800

    enable user report in client

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,11 +7,18 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://ec97fa3f666b4960b33d6b28b3918cb4@app.glitchtip.com/2814',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+    dsn: SENTRY_DSN || 'https://ec97fa3f666b4960b33d6b28b3918cb4@app.glitchtip.com/2814',
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+    beforeSend(event) {
+        // Check if it is an exception, and if so, show the report dialog
+        if (event.exception) {
+            Sentry.showReportDialog({ eventId: event.event_id });
+        }
+        return event;
+    },
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
 });
