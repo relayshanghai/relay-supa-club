a5ca075db8f8329f069a31d25f6fd2619d007f48
jacobcoro <xiaokourou@gmail.com>
2023-03-28 17:32:31 +0800

    wait till company is set

--- pages/make-error.tsx
+++ pages/make-error.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect } from 'react';
 import { Button } from 'src/components/button';
+import { Layout } from 'src/components/layout';
+import { useCompany } from 'src/hooks/use-company';
 import { nextFetch } from 'src/utils/fetcher';
 import { clientLogger } from 'src/utils/logger';
 
@@ -8,20 +10,24 @@ export default function MakeErrorPage() {
     // useEffect(() => {
     //     throw new Error('Error from MakeErrorPage');
     // }, []);
+    const { company } = useCompany();
     useEffect(() => {
+        if (!company) return;
         clientLogger('clientLogger error', 'error');
         clientLogger('clientLogger info', 'info');
         clientLogger('clientLogger warning', 'warn');
         nextFetch('make-error');
-    }, []);
+    }, [company]);
     const makeError = () => {
         throw new Error('Error from MakeErrorPage');
     };
     return (
-        <div>
-            <h1>Make Error Page</h1>
-            <p>See the console for the error.</p>
-            <Button onClick={makeError}>Make error</Button>
-        </div>
+        <Layout>
+            <div>
+                <h1>Make Error Page</h1>
+                <p>See the console for the error.</p>
+                <Button onClick={makeError}>Make error</Button>
+            </div>
+        </Layout>
     );
 }
