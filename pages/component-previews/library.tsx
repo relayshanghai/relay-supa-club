f2554af7b3885efecdac625ea8d6aa7a04231d2c
jacobcoro <xiaokourou@gmail.com>
2023-03-30 16:12:34 +0800

    title

--- pages/component-previews/library.tsx
+++ pages/component-previews/library.tsx
@@ -2,6 +2,7 @@ import * as library from 'src/components/library';
 
 const Badges = () => (
     <div className="m-5">
+        <h2 className="text-lg font-bold"> Badges</h2>
         <p>Primary, Solid, Rounded, 3 sizes</p>
         <div className="m-5 flex flex-wrap space-x-3 bg-slate-200 p-5">
             <library.Badge size="small">Small</library.Badge>
