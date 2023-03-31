fb5c076d54c2a23ed969a424dfb2604d7fef4ebd
jacobcoro <xiaokourou@gmail.com>
2023-03-31 15:02:32 +0800

    get tooltips set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,8 +18,13 @@ function MyApp({
 }>) {
     const [supabaseClient] = useState(() => createBrowserSupabaseClient());
     useEffect(() => {
+        //@ts-expect-error
+        import('preline');
+    }, []);
+    useEffect(() => {
         const storedLanguage = localStorage.getItem('language');
         storedLanguage !== null ? i18n.changeLanguage(storedLanguage) : i18n.changeLanguage(); // triggers the language detector
+        //@ts-ignore
     }, []);
     return (
         <>
