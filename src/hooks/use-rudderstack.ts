44a0cb4484b096f968fd821f4cdf42d82879c402
Ellie Wang <yuetingw@outlook.com>
2023-03-31 11:57:57 +0800

    wip init setup

---
+++ src/hooks/use-rudderstack.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect } from 'react';
+export default async function useRudderstack() {
+    //these keys are for RudderStack App-Frontend Source, if we need to add new source we need to add new keys
+    const WRITE_KEY = process.env.RUDDERSTACK_APP_FRONTEND_WRITE_KEY;
+    const DATA_PLANE_URL = process.env.RUDDERSTACK_APP_FRONTEND_DATA_PLANE_URL;
+
+    const rudderInitialized = useCallback(async () => {
+        if (!WRITE_KEY || !DATA_PLANE_URL) {
+            return;
+        }
+        console.log('im here!');
+        window.rudder = await import('rudder-sdk-js');
+
+        window.rudder?.load(WRITE_KEY, DATA_PLANE_URL, {
+            integrations: { All: true }, // load call options
+        });
+
+        console.log('loaded!');
+
+        window.rudder?.ready(() => {
+            //eslint-disable-next-line no-console
+            console.log('All set!');
+        });
+    }, [DATA_PLANE_URL, WRITE_KEY]);
+
+    useEffect(() => {
+        rudderInitialized();
+    }, [rudderInitialized]);
+}
