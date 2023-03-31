bf29b47c4a00fcee9feb4bf996e0ca33582a2fdc
Ellie Wang <yuetingw@outlook.com>
2023-03-31 17:05:49 +0800

    refactor get rid of usecallback fn

--- src/hooks/use-rudderstack.ts
+++ src/hooks/use-rudderstack.ts
@@ -1,26 +1,26 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 export default async function useRudderstack() {
     //these keys are for RudderStack App-Frontend Source, if we need to add new source we need to add new keys
     const WRITE_KEY = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_WRITE_KEY;
     const DATA_PLANE_URL = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_DATA_PLANE_URL;
 
-    const rudderInitialized = useCallback(async () => {
-        if (!WRITE_KEY || !DATA_PLANE_URL) {
-            return;
-        }
-        window.rudder = await import('rudder-sdk-js');
-
-        window.rudder?.load(WRITE_KEY, DATA_PLANE_URL, {
-            integrations: { All: true }, // load call options
-        });
-
-        window.rudder?.ready(() => {
-            //eslint-disable-next-line no-console
-            console.log('All set!');
-        });
-    }, [DATA_PLANE_URL, WRITE_KEY]);
-
     useEffect(() => {
+        const rudderInitialized = async () => {
+            if (!WRITE_KEY || !DATA_PLANE_URL) {
+                return;
+            }
+            window.rudder = await import('rudder-sdk-js');
+
+            window.rudder?.load(WRITE_KEY, DATA_PLANE_URL, {
+                integrations: { All: true }, // load call options
+            });
+
+            window.rudder?.ready(() => {
+                //eslint-disable-next-line no-console
+                console.log('All set!');
+            });
+        };
+
         rudderInitialized();
-    }, [rudderInitialized]);
+    }, [DATA_PLANE_URL, WRITE_KEY]);
 }
