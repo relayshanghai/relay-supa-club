18cc619cee7d77ce07c9068d6386ba5de2e6f93b
James <james@relay.club>
2023-03-31 14:29:12 +0800

    Fix invalid appending sql files

--- supabase/policies/index.sql
+++ supabase/policies/index.sql
@@ -1,4 +1,6 @@
+BEGIN;
 \include ./supabase/policies/profiles_select.policy.sql
 \include ./supabase/policies/profiles_insert.policy.sql
 \include ./supabase/policies/profiles_update.policy.sql
 \include ./supabase/policies/profiles_delete.policy.sql
+COMMIT;
