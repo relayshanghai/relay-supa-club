f8620ab5bd3557a2a0d71e53b964ddccbfe67a2d
Ellie Wang <yuetingw@outlook.com>
2023-03-31 12:10:50 +0800

    fix env variable naming

--- src/hooks/use-rudderstack.ts
+++ src/hooks/use-rudderstack.ts
@@ -1,21 +1,18 @@
 import { useCallback, useEffect } from 'react';
 export default async function useRudderstack() {
     //these keys are for RudderStack App-Frontend Source, if we need to add new source we need to add new keys
-    const WRITE_KEY = process.env.RUDDERSTACK_APP_FRONTEND_WRITE_KEY;
-    const DATA_PLANE_URL = process.env.RUDDERSTACK_APP_FRONTEND_DATA_PLANE_URL;
+    const WRITE_KEY = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_WRITE_KEY;
+    const DATA_PLANE_URL = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_DATA_PLANE_URL;
 
     const rudderInitialized = useCallback(async () => {
         if (!WRITE_KEY || !DATA_PLANE_URL) {
             return;
         }
-        console.log('im here!');
         window.rudder = await import('rudder-sdk-js');
 
         window.rudder?.load(WRITE_KEY, DATA_PLANE_URL, {
             integrations: { All: true }, // load call options
         });
-
-        console.log('loaded!');
 
         window.rudder?.ready(() => {
             //eslint-disable-next-line no-console
