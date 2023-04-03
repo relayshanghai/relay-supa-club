d8b3d6bee0c90f3471c8b4ba5b69b26d602fa1fb
jacobcoro <xiaokourou@gmail.com>
2023-04-03 15:21:26 +0800

    add switch component, move preview page

---
+++ src/components/library/library-preview-page.tsx
@@ -0,0 +1,54 @@
+import * as library from './index';
+
+const Badges = () => (
+    <div className="m-5">
+        <h2 className="text-lg font-bold"> Badges</h2>
+        <p>Primary, Solid, Rounded, 3 sizes</p>
+        <div className="m-5 flex flex-wrap space-x-3 bg-slate-200 p-5">
+            <library.Badge size="small">Small</library.Badge>
+
+            <library.Badge size="medium">Medium</library.Badge>
+
+            <library.Badge size="large">Large</library.Badge>
+        </div>
+    </div>
+);
+
+const Tooltips = () => (
+    <div className="m-5">
+        <h2 className="text-lg font-bold"> Tooltips</h2>
+        <p>default, with content</p>
+        <div className="m-5 flex flex-wrap space-x-3 bg-slate-200 p-5">
+            <library.Tooltip content="explaining this and that">
+                <library.Badge className="hs-tooltip-toggle"> Hover Me</library.Badge>
+            </library.Tooltip>
+            <library.Tooltip
+                content="explaining this"
+                detail="and also another thing. Perhaps a few more things. While we are at it one more thing. "
+            >
+                <library.Badge className="hs-tooltip-toggle"> With Details</library.Badge>
+            </library.Tooltip>
+        </div>
+    </div>
+);
+
+const Switches = () => (
+    <div className="m-5">
+        <h2 className="text-lg font-bold"> Switches</h2>
+        <p>default, checked, with descriptions</p>
+        <div className="m-5 flex flex-wrap space-x-3 bg-slate-200 p-5">
+            <library.Switch checked={false} />
+            <library.Switch checked={true} />
+        </div>
+    </div>
+);
+
+const LibraryPage = () => (
+    <>
+        <Badges />
+        <Tooltips />
+        <Switches />
+    </>
+);
+
+export default LibraryPage;
---
+++ src/components/library/switch.tsx
@@ -0,0 +1,30 @@
+import type { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+
+interface SwitchProps
+    extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    checked: boolean;
+    beforeLabel?: string;
+    afterLabel?: string;
+}
+
+export const Switch = ({ beforeLabel, afterLabel }: SwitchProps) => {
+    return (
+        <div className="flex items-center">
+            {beforeLabel && (
+                <label className="mr-3 text-sm text-gray-500 dark:text-gray-400">
+                    {beforeLabel}
+                </label>
+            )}
+            <input
+                type="checkbox"
+                id="hs-basic-with-description"
+                className="relative h-7 w-[3.25rem] shrink-0 cursor-pointer appearance-none rounded-full border-2 border-transparent bg-gray-100 ring-1 ring-transparent ring-offset-white transition-colors duration-200 ease-in-out before:inline-block before:h-6 before:w-6 before:translate-x-0 before:transform before:rounded-full before:bg-white before:shadow before:ring-0 before:transition before:duration-200 before:ease-in-out checked:bg-blue-600 checked:bg-none checked:before:translate-x-full checked:before:bg-blue-200 focus:border-blue-600 focus:outline-none focus:ring-blue-600 dark:bg-gray-700 dark:before:bg-gray-400 dark:checked:bg-blue-600 dark:checked:before:bg-blue-200 dark:focus:ring-offset-gray-800"
+            />
+            {afterLabel && (
+                <label className="ml-3 text-sm text-gray-500 dark:text-gray-400">
+                    {afterLabel}
+                </label>
+            )}{' '}
+        </div>
+    );
+};
