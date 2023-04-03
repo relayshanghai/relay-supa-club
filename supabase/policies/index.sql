1364186bd9b22ab30770f17409ab59f079c73a08
James <james@relay.club>
2023-04-03 09:47:47 +0800

    Do not include policies without migration

--- supabase/policies/index.sql
+++ supabase/policies/index.sql
@@ -1,6 +1,3 @@
 BEGIN;
-\include ./supabase/policies/profiles_select.policy.sql
-\include ./supabase/policies/profiles_insert.policy.sql
-\include ./supabase/policies/profiles_update.policy.sql
-\include ./supabase/policies/profiles_delete.policy.sql
+-- insert policy files here
 COMMIT;
