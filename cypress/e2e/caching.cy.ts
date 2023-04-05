cf0c517c2e5870a907c12e4e0a702fc17e64f029
jacobcoro <xiaokourou@gmail.com>
2023-04-05 13:32:25 +0800

    works

--- cypress/e2e/caching.cy.ts
+++ cypress/e2e/caching.cy.ts
@@ -1,4 +1,3 @@
-import { assertOffline, goOffline } from '../../src/utils/cypress-utils';
 describe('Main pages happy paths', () => {
     it('can log in and load search page and switch language', () => {
         cy.loginTestUser();
@@ -15,16 +14,16 @@ describe('Main pages happy paths', () => {
             .invoke('removeAttr', 'target') // remove target attribute so we can click it and stay on the same page
             .click({ force: true }); // force click because the button is hidden because of our weird hover UI
 
+        cy.contains("T-Series is India's largest Music Label", { timeout: 1000 }).should('not.exist'); // report is not loaded yet
+
         cy.contains('Generating influencer Report. Please wait', { timeout: 30000 }); // loading analyze page
         cy.contains("T-Series is India's largest Music Label", { timeout: 30000 }); // loads analyze page
 
-        goOffline();
-        assertOffline();
         cy.reload();
-        assertOffline();
+
         cy.contains('Campaigns', { timeout: 10000 }); // sidebar has loaded
 
-        cy.contains("T-Series is India's largest Music Label", { timeout: 300 }); // loads report super fast
+        cy.contains("T-Series is India's largest Music Label", { timeout: 1000 }); // loads report super fast
     });
 });
 
