b7ca7c75d6d06c665480c72cf9e90bce972c46c1
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-04-06 07:18:10 +0530

    Added empty term check

--- src/components/search/search-creators.tsx
+++ src/components/search/search-creators.tsx
@@ -1,11 +1,10 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useSearch } from 'src/hooks/use-search';
 import { useTranslation } from 'react-i18next';
 import type { CreatorPlatform } from 'types';
 import type { ChangeEvent } from 'react';
 
 export const SearchCreators = ({ platform }: { platform: CreatorPlatform }) => {
-    const searchRef = useRef<any>();
     const [searchTerm, setSearchTerm] = useState<string | ''>();
     const { t } = useTranslation();
 
@@ -35,15 +34,16 @@ export const SearchCreators = ({ platform }: { platform: CreatorPlatform }) => {
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value.trim());
+
+        if (e.target.value.trim() === '') {
+            setUsername('');
+        }
+
         searchInfluencer(e.target.value.trim());
     };
 
-    useEffect(() => {
-        setSearchTerm('');
-    }, [platform]);
-
     return (
-        <div className="group relative flex w-full flex-col font-medium" ref={searchRef}>
+        <div className="group relative flex w-full flex-col font-medium">
             <input
                 className="block w-full appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 ring-1 ring-gray-900 ring-opacity-5 placeholder:text-sm focus:outline-none"
                 placeholder={t('creators.show.searchInfluencerPlaceholder') as string}
