5525809a893137c3cb75c82c68af835395f771f7
jacobcoro <xiaokourou@gmail.com>
2023-04-07 17:32:23 +0800

    use cy selector? remove empty parent div

--- src/components/campaigns/creator-contacts-skeleton.tsx
+++ src/components/campaigns/creator-contacts-skeleton.tsx
@@ -2,12 +2,10 @@ import Skeleton from '../common/skeleton';
 
 export default function ContactsSkeleton() {
     return (
-        <div className="" data-testid="contacts-skeleton">
-            <div className="flex space-x-1">
-                <Skeleton className="h-4 w-4 rounded-full bg-gray-300" />
-                <Skeleton className="h-4 w-4 rounded-full bg-gray-400" />
-                <Skeleton className="h-4 w-4 rounded-full" />
-            </div>
+        <div className="flex space-x-1" data-testid="contacts-skeleton">
+            <Skeleton className="h-4 w-4 rounded-full bg-gray-300" />
+            <Skeleton className="h-4 w-4 rounded-full bg-gray-400" />
+            <Skeleton className="h-4 w-4 rounded-full" />
         </div>
     );
 }
