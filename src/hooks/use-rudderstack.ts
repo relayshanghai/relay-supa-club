ddb19156219a9ec25875ec6e69a51a240c5be4a3
Ellie Wang <yuetingw@outlook.com>
2023-04-14 15:21:02 +0800

    delete the fix that was not working

--- src/hooks/use-rudderstack.ts
+++ src/hooks/use-rudderstack.ts
@@ -1,5 +1,4 @@
 import type { apiObject } from 'rudder-sdk-js';
-import { rudderInitialized } from 'src/utils/rudder-initialize';
 
 //There are more traits properties, but we only need these for now. Ref: https://www.rudderstack.com/docs/event-spec/standard-events/identify/#identify-traits
 export interface IdentityTraits extends apiObject {
@@ -21,7 +20,6 @@ export interface PageProperties extends apiObject {
 }
 
 export const useRudderstack = () => {
-    rudderInitialized();
     const IdentifyUser = (userId: string, traits: IdentityTraits) => {
         if (window !== undefined) {
             window.rudder.identify(userId, traits);
