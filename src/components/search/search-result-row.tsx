2860daf4a5df2235cd4245543430cbf6a9ee896e
Ellie Wang <yuetingw@outlook.com>
2023-04-17 13:28:28 +0800

    added uncaught exception in Cypress config for Rudderstck

--- src/components/search/search-result-row.tsx
+++ src/components/search/search-result-row.tsx
@@ -218,6 +218,9 @@ export const SearchResultRow = ({
                                                 className={`${
                                                     active ? 'bg-violet-500 text-white' : 'text-gray-900'
                                                 } group flex w-full items-center justify-center rounded-md px-2 py-2 text-sm`}
+                                                onClick={() =>
+                                                    trackEvent('Opened a report from Search', { platform, user_id })
+                                                }
                                             >
                                                 {t('creators.analyzeProfile')}
                                             </button>
