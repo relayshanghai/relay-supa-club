1f69b9de04f214d2a0184c6863d5b927eeb76574
jacobcoro <xiaokourou@gmail.com>
2023-04-18 15:59:57 +0800

    use all policy

---
+++ supabase/policies/campaigns_all.policy.sql
@@ -0,0 +1,10 @@
+-- Allow all/any updates on campaigns table if user belongs to company 
+-- or is relay employee
+ALTER TABLE campaigns ENABLE ROW LEVEL SECURITY;
+
+DROP POLICY IF EXISTS campaigns_all ON campaigns;
+
+CREATE POLICY campaigns_all ON campaigns FOR ALL
+USING (
+    is_company_member(company_id) OR is_relay_employee()
+);
