0e1cade587e6325310b3113bec330c9dc3b8cf35
Ellie Wang <yuetingw@outlook.com>
2023-04-18 12:33:28 +0800

    add logger to replace console log

--- src/utils/rudder-initialize.ts
+++ src/utils/rudder-initialize.ts
@@ -1,11 +1,12 @@
+import { clientLogger } from './logger-client';
+
 export async function rudderInitialized() {
     //these keys are for RudderStack App-Frontend Source, if we need to add new source we need to add new keys
     const WRITE_KEY = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_WRITE_KEY;
     const DATA_PLANE_URL = process.env.NEXT_PUBLIC_RUDDERSTACK_APP_DATA_PLANE_URL;
 
     if (!WRITE_KEY || !DATA_PLANE_URL) {
-        // eslint-disable-next-line no-console
-        console.log('RudderStack keys not set');
+        clientLogger('RudderStack keys not set');
         return;
     }
     window.rudder = await import('rudder-sdk-js');
