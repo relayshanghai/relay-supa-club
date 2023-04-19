14ccf925414e0aa094ea87aff2d506bd0914568d
jacobcoro <xiaokourou@gmail.com>
2023-04-19 13:04:52 +0800

    Revert "rewrite modal without headless ui"
    This reverts commit e0cf115f8a1fcb3b6e07b14191d1ef0da56aac53.

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,40 +1,54 @@
-import type { ReactNode } from 'react';
-import { useEffect, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
 
-type ModalProps = {
+export interface ModalProps {
     visible: boolean;
-    onClose: () => void;
-    title: string | ReactNode;
-    children: ReactNode;
-};
+    title?: string | JSX.Element;
+    onClose: (value: boolean) => void;
+    children: JSX.Element | JSX.Element[];
+}
 
-export const Modal = ({ visible, onClose, title, children }: ModalProps) => {
-    const [show, setShow] = useState(false);
-
-    useEffect(() => {
-        setShow(visible);
-    }, [visible]);
-
-    const handleClose = () => {
-        setShow(false);
-        onClose();
-    };
-
+export const Modal: React.FC<ModalProps> = ({ children, visible, onClose, title }) => {
     return (
-        <>
-            <div
-                className={`fixed left-0 top-0 z-50 h-full w-full bg-black bg-opacity-50 ${show ? 'block' : 'hidden'}`}
-                onClick={handleClose}
-            />
-            <div
-                className={`fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white p-6 shadow-lg ${
-                    show ? 'block' : 'hidden'
-                }`}
-            >
-                <div className="text-xl font-semibold">{title}</div>
+        <Transition appear show={visible} as={Fragment}>
+            <Dialog open={visible} as="div" className="relative z-10" onClose={onClose}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
 
-                <div>{children}</div>
-            </div>
-        </>
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-tertiary-100 p-6 text-left align-middle shadow-xl transition-all">
+                                <Dialog.Title
+                                    as="h3"
+                                    className="mb-4 text-xl font-semibold leading-6 text-tertiary-600"
+                                >
+                                    {title}
+                                </Dialog.Title>
+
+                                <>{children}</>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
     );
 };
