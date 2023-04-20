ea332f7579a14013a8837cac9ef6deb90259291a
jacobcoro <xiaokourou@gmail.com>
2023-04-20 16:30:25 +0800

    company select

--- supabase/policies/index.sql
+++ supabase/policies/index.sql
@@ -5,4 +5,5 @@ BEGIN;
 \include ./supabase/policies/profiles_select.policy.sql
 \include ./supabase/policies/profiles_update.policy.sql
 \include ./supabase/policies/campaigns_all.policy.sql
+\include ./supabase/policies/companies_select.policy.sql
 COMMIT;
