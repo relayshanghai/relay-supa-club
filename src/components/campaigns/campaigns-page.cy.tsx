a2a0296f6601163853a9bae5c8c1f2a35022316a
jacobcoro <xiaokourou@gmail.com>
2023-04-25 14:33:06 +0800

    only use contains not get

--- src/components/campaigns/campaigns-page.cy.tsx
+++ src/components/campaigns/campaigns-page.cy.tsx
@@ -3,30 +3,20 @@ import { testMount } from 'src/utils/cypress-app-wrapper';
 import CampaignsPage from './campaigns-page';
 
 describe('CampaignsPage', () => {
-    before(async () => {
+    before(() => {
         worker.start();
     });
 
     it('Should render a list of campaign cards', () => {
-        cy.wait(3000);
-
         testMount(<CampaignsPage companyId="8e6e65ca-dd79-4e68-90e4-9c5462991ae4" />);
-        cy.get('#campaign-card-second').should('exist');
-        cy.get('#campaign-card-【test】amy-test-campaign-for-fragrance-kols').should('exist');
-
         cy.contains('Empty Campaign');
         cy.contains('【test】Amy test campaign for fragrance KOLs');
     });
 
     it('Should render a list of archived campaign cards', () => {
-        cy.wait(3000);
-
         testMount(<CampaignsPage companyId="8e6e65ca-dd79-4e68-90e4-9c5462991ae4" />);
-
+        cy.contains('Test Archive Campaign').should('not.exist');
         cy.contains('Archived Campaigns').click();
-
-        cy.get('#campaign-card-testcampaign').should('exist');
-
         cy.contains('Test Archive Campaign');
     });
 });
