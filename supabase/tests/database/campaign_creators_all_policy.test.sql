78c27de0ec24646a6160b4cf5f97cf5963c5d1a7
jacobcoro <xiaokourou@gmail.com>
2023-04-26 11:25:13 +0800

    test

--- supabase/tests/database/campaign_creators_all_policy.test.sql
+++ supabase/tests/database/campaign_creators_all_policy.test.sql
@@ -2,7 +2,7 @@
 -- Policies directory: /tmp/supabase/policies
 
 BEGIN;
-SELECT plan(6);
+SELECT plan(8);
 
 -- start includes
 \include /tmp/supabase/functions/is_company_member.sql
@@ -15,27 +15,37 @@ SELECT tests.rls_enabled('public', 'campaigns');
 SELECT policy_cmd_is('campaign_creators', 'campaign_creators_all', 'all');
 -- Because we confirm here that the rule applies to all, we only need to test SELECT.
 
-PREPARE user_select AS
-SELECT id FROM campaigns WHERE product_name = 'Shade Range Makeup';
 
 -- Test anonymous
 SELECT tests.clear_authentication();
-SELECT is_empty('user_select', 'anon CANNOT select campaign creators');
+SELECT is((SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'Shade Range Makeup')), null, 'anon CANNOT select campaign creators');
 
 -- test user
 SELECT tests.authenticate_as('christopher.david.thompson@blue-moonlight-stream.com');
 SELECT is(
-  $$ SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'NeonX Gaming Console'); $$,
-  'Beauty for Aafsdin Tones',
+  (SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'Shade Range Makeup')),
+  'Greg Renko',
   'Employee CAN select own company campaigns'
+);
+
+SELECT is(
+  (SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'NeonX Gaming Console')),
+  null,
+  'Employee CANNOT select other company campaigns'
 );
 
 -- test staff user
 SELECT tests.authenticate_as('jacob@relay.club');
 SELECT is(
-  $$ SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'NeonX Gaming Console'); $$,
-  'Beauty for All Skin Tones',
-  'Employee CAN select own company campaigns'
+  (SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'NeonX Gaming Console')),
+  'Yousef Gaming',
+  'Relay employee CAN select own company campaigns'
+);
+
+SELECT is(
+  (SELECT fullname FROM campaign_creators WHERE campaign_id = (SELECT id FROM campaigns WHERE product_name = 'Shade Range Makeup')),
+  'Greg Renko',
+  'Relay employee CAN select other company campaigns'
 );
 
 SELECT * FROM finish(); -- end test
