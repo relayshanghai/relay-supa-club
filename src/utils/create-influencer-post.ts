2b887547bcf35189ec3b5e27d8e80aa35724c7c2
James <james@relay.club>
2023-05-05 14:06:00 +0800

    Remove createInfluencerPost

--- src/utils/create-influencer-post.ts
+++
@@ -1,5 +0,0 @@
-import { createInfluencerPost as _createInfluencerPost } from './api/db/calls/posts';
-
-export const createInfluencerPost = async (data: any) => {
-    return _createInfluencerPost(data);
-};
