d715f7255f59a5c9be420e8f80ac1b46ce974c05
James <james@relay.club>
2023-05-12 16:21:21 +0800

    Username will use empty string as fallback

--- types/iqdata/creator-report.ts
+++ types/iqdata/creator-report.ts
@@ -168,8 +168,8 @@ interface UserProfile {
     type: CreatorPlatform;
     user_id: string;
     username: string;
-    handle: string; // youtube only
-    custom_name: string; // youtube only
+    handle?: string; // youtube only
+    custom_name?: string; // youtube only
     url: string;
     picture: string;
     fullname: string;
