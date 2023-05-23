ef9b5ef0a93a3109bf7b335ac0a33100a4be4a2b
jacobcoro <xiaokourou@gmail.com>
2023-05-23 10:59:00 +0800

    don't record default searches

--- pages/api/influencer-search/index.ts
+++ pages/api/influencer-search/index.ts
@@ -4,6 +4,7 @@ import { recordSearchUsage } from 'src/utils/api/db/calls/usages';
 import { fetchCreatorsFiltered } from 'src/utils/api/iqdata';
 import type { FetchCreatorsFilteredParams } from 'src/utils/api/iqdata/transforms';
 import { serverLogger } from 'src/utils/logger-server';
+import { hasCustomSearchParams } from 'src/utils/usagesHelpers';
 import type { CreatorSearchResult } from 'types';
 
 export type InfluencerPostRequest = FetchCreatorsFilteredParams & {
@@ -16,10 +17,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         try {
             const { company_id, user_id, ...searchParams } = req.body as InfluencerPostRequest;
+            if (!company_id || !user_id) {
+                return res.status(httpCodes.BAD_REQUEST).json({});
+            }
 
-            const { error: recordError } = await recordSearchUsage(company_id, user_id);
-            if (recordError) {
-                res.status(httpCodes.BAD_REQUEST).json({ error: recordError });
+            if (hasCustomSearchParams(searchParams)) {
+                const { error: recordError } = await recordSearchUsage(company_id, user_id);
+                if (recordError) {
+                    res.status(httpCodes.BAD_REQUEST).json({ error: recordError });
+                }
             }
 
             const results = await fetchCreatorsFiltered(searchParams);
