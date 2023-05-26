7676b0e097a9bb779139894aa2cb5c0fd60de7e1
James <james@relay.club>
2023-05-26 12:32:24 +0800

    Add scraping in POST api/influencer/posts

--- pages/api/influencer/posts.ts
+++ pages/api/influencer/posts.ts
@@ -1,7 +1,9 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import httpCodes from 'src/constants/httpCodes';
 import { ApiHandler } from 'src/utils/api-handler';
-import { saveInfluencerPost as saveInfluencerPostQuery } from 'src/utils/save-influencer-post';
+import { saveInfluencerPost } from 'src/utils/save-influencer-post';
+import { savePostPerformance } from 'src/utils/save-post-performance';
+import { scrapeInfluencerPost } from 'src/utils/scrape-influencer-post';
 import { db } from 'src/utils/supabase-client';
 
 type PostRequest = {
@@ -15,6 +17,7 @@ type PostInfo = {
     postedDate: string;
     id: string;
     url: string;
+    performance: any;
 };
 
 type PostResponse =
@@ -26,6 +29,31 @@ type PostResponse =
           error: string;
       };
 
+const processURL = async (url: string, campaign_id: string, influencer_social_profile_id: string) => {
+    const scrape = await scrapeInfluencerPost(url);
+
+    const _savePostPerformance = db<typeof savePostPerformance>(savePostPerformance);
+    const _saveInfluencerPost = db<typeof saveInfluencerPost>(saveInfluencerPost);
+
+    const post = await _saveInfluencerPost({
+        type: '',
+        campaign_id: campaign_id,
+        influencer_social_profile_id: influencer_social_profile_id,
+        url: url,
+    });
+
+    const performance = await _savePostPerformance({
+        campaign_id: campaign_id,
+        influencer_social_profile_id: influencer_social_profile_id,
+        post_id: post.id,
+        likes_total: scrape.likeCount,
+        comments_total: scrape.commentCount,
+        views_total: scrape.viewCount,
+    });
+
+    return { post, performance };
+};
+
 const postHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<PostResponse>) => {
     const data: PostResponse = {
         successful: [],
@@ -34,22 +62,20 @@ const postHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResp
 
     const body = req.body as PostRequest;
 
-    const saveInfluencerPost = db<typeof saveInfluencerPostQuery>(saveInfluencerPostQuery);
+    for (const url of body.urls) {
+        try {
+            const result = await processURL(url, body.campaign_id, body.influencer_social_profile_id);
 
-    for (const url in body.urls) {
-        const post = await saveInfluencerPost({
-            type: '',
-            campaign_id: body.campaign_id,
-            influencer_social_profile_id: body.influencer_social_profile_id,
-            url: url,
-        });
-
-        data.successful.push({
-            title: post.title || '',
-            postedDate: post.posted_date || '',
-            id: post.id,
-            url: post.url,
-        });
+            data.successful.push({
+                title: result.post.title || '',
+                postedDate: result.post.posted_date || '',
+                id: result.post.id,
+                url: result.post.url,
+                performance: result.performance,
+            });
+        } catch (error) {
+            data.failed.push(url);
+        }
     }
 
     res.status(httpCodes.OK).json(data);
