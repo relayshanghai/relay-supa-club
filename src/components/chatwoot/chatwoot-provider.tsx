1c7a0cc9f6311d87f42ff38250037a39fba0d4cd
James <james@relay.club>
2023-06-06 13:54:11 +0800

    Add chatwoot webhook handler

--- src/components/chatwoot/chatwoot-provider.tsx
+++ src/components/chatwoot/chatwoot-provider.tsx
@@ -1,65 +1,8 @@
 import type { PropsWithChildren } from 'react';
 import { useEffect, useState, useContext } from 'react';
 import React, { createContext } from 'react';
-
-export type ChatwootSDKParams = { websiteToken: string; baseUrl?: string };
-
-export type ChatwootSettings = {
-    hideMessageBubble?: boolean;
-    position?: 'left' | 'right';
-    locale?: string;
-    useBrowserLanguage?: boolean;
-    type?: 'standard' | 'expanded_bubble';
-    darkMode?: 'light' | 'auto';
-    launcherTitle?: string;
-    showPopoutButton?: boolean;
-};
-
-export type ChatwootConfig = ChatwootSettings & ChatwootSDKParams;
-
-export type Chatwoot = {
-    baseUrl: string;
-    hasLoaded: boolean;
-    hideMessageBubble: boolean;
-    isOpen: boolean;
-    position: string;
-    websiteToken: string;
-    locale: string;
-    useBrowserLanguage: boolean;
-    type: string;
-    launcherTitle: string;
-    showPopoutButton: boolean;
-    widgetStyle: string;
-    resetTriggered: boolean;
-    darkMode: string;
-    toggle: (state?: 'open' | 'close') => void;
-    toggleBubbleVisibility: (state?: 'show' | 'hide') => void;
-    popoutChatWindow: () => void;
-    setUser: (
-        id: string,
-        additional_params: {
-            identifier_hash?: string;
-            [key: string]: any;
-        },
-    ) => void;
-    setCustomAttributes: (attributes: { [key: string]: any }) => void;
-    deleteCustomAttribute: (attributeKey: string) => void;
-    setConversationCustomAttributes: (attributes: { [key: string]: any }) => void;
-    deleteConversationCustomAttribute: (attributeKey: string) => void;
-    setLabel: (label: string) => void;
-    removeLabel: (label: string) => void;
-    setLocale: (locale: string) => void;
-    setColorScheme: (scheme: string) => void;
-    reset: () => void;
-};
-
-export type WindowChatwoot = {
-    chatwootSettings: ChatwootSettings;
-    chatwootSDK: {
-        run: (params: ChatwootSDKParams) => void;
-    };
-    $chatwoot: Chatwoot;
-} & typeof window;
+import type { Chatwoot, ChatwootSettings, ChatwootSDKParams } from 'src/utils/chatwoot/types';
+import { clientLogger } from 'src/utils/logger-client';
 
 const ChatWootContext = createContext<Chatwoot | null>(null);
 
