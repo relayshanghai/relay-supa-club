3b7349faf611e32c1653652d0b9873c1429e4667
jacobcoro <xiaokourou@gmail.com>
2023-06-08 14:17:07 +0800

    increase wait

--- cypress/e2e/happy_path.cy.ts
+++ cypress/e2e/happy_path.cy.ts
@@ -310,7 +310,7 @@ describe('Main pages happy paths', () => {
         cy.contains('https://relay.club');
 
         // searches should have increased by 2
-        cy.contains('td', '2', { timeout: 20000 }); // wait for count to update
+        cy.contains('td', '2', { timeout: 30000 }); // wait for count to update
         cy.contains('tr', 'Searches').within(() => {
             cy.contains('td', '2');
         });
@@ -357,7 +357,7 @@ describe('Main pages happy paths', () => {
         // Check that search total increased
         cy.contains('Account').click();
         cy.contains('https://blue-moonlight-stream.com');
-        cy.contains('td', '1'); // wait for count to update
+        cy.contains('td', '1', { timeout: 30000 }); // wait for count to update
         cy.contains('tr', 'Searches').within(() => {
             cy.contains('td', '1');
         });
