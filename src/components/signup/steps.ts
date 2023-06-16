3465d008b1fab005ad8e55b5b79b0a63f4676f6c
Ellie Wang <yuetingw@outlook.com>
2023-06-16 18:07:33 +0800

    simplify imports

---
+++ src/components/signup/steps.ts
@@ -0,0 +1,6 @@
+import { StepOne } from './step-one';
+import { StepTwo } from './step-two';
+import { StepThree } from './step-three';
+import { StepFour } from './step-four';
+
+export { StepOne, StepTwo, StepThree, StepFour };
