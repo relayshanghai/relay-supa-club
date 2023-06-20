cad666b8b058336068acf9fa1a0b4a1a1ad9a01b
Ellie Wang <yuetingw@outlook.com>
2023-06-20 13:53:35 +0800

    fix signup input types

--- src/utils/validation/signup.ts
+++ src/utils/validation/signup.ts
@@ -27,6 +27,42 @@ export const validatePassword = (password: string) => {
 
     return null;
 };
+export type LegacySignupInputTypes =
+    | 'firstName'
+    | 'lastName'
+    | 'email'
+    | 'password'
+    | 'confirmPassword'
+    | 'phoneNumber';
+
+export const validateLegacySignupInput = (type: SignupInputTypes, value: string, password: string) => {
+    switch (type) {
+        case 'firstName':
+            return !value ? loginValidationErrors.firstNameRequired : null;
+        case 'lastName':
+            return !value ? loginValidationErrors.lastNameRequired : null;
+        case 'email':
+            // TODO: use library https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/176
+            if (value.includes(' ')) {
+                return loginValidationErrors.noSpacesAllowed;
+            }
+            return !emailRegex.test(value) ? loginValidationErrors.emailInvalid : null;
+        case 'password':
+            return validatePassword(value);
+        case 'confirmPassword':
+            return value !== password ? loginValidationErrors.passwordsDoNotMatch : null;
+        case 'phoneNumber':
+            if (value.includes(' ')) {
+                return loginValidationErrors.noSpacesAllowed;
+            }
+            // TODO: use library https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/176
+            const onlyNumbersDashesPLusSignParensRegex = /^[\d\-\+\(\)]+$/;
+            return !onlyNumbersDashesPLusSignParensRegex.test(value) ? loginValidationErrors.phoneNumberInvalid : null;
+        default:
+            return null;
+    }
+};
+
 export type SignupInputTypes =
     | 'firstName'
     | 'lastName'
