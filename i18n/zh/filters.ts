62ee893ce87f6eb315fb1732b103bc321a28482e
TGS963 <ghoshsuvojit2012@gmail.com>
2023-06-27 11:00:20 +0530

    added translations

--- i18n/zh/filters.ts
+++ i18n/zh/filters.ts
@@ -1,7 +1,7 @@
 const filters = {
-    title: 'Filters',
+    title: '筛选条件',
     location: {
-        label: 'Location',
+        label: '地区',
         influencerPlaceholder: '请输入国家（必填英文)',
         audiencePlaceholder: '粉丝国家',
     },
@@ -11,15 +11,15 @@ const filters = {
         femaleOption: '女性',
     },
     maxOption: '最大',
-    minOption: 'Min',
+    minOption: '最低（或最少）',
     anyOption: '任何',
     audience: {
-        title: 'Audience Filters',
-        ageLabel: 'Age',
+        title: '受众的筛选条件',
+        ageLabel: '年龄',
     },
     influencers: {
-        title: 'Influencer Filters',
-        hasEmail: 'Has Email',
+        title: '网红达人的筛选条件',
+        hasEmail: '具备邮箱联系方式',
         engagamentLabel: '互动率',
         subscribersLabel: '粉丝数',
         lastPostLabel: '上次更新时间',
@@ -27,7 +27,7 @@ const filters = {
     },
     days: '天',
     months: '月',
-    clearButton: 'Clear Filters',
+    clearButton: '清除目前筛选条件',
 };
 
 export default filters;
