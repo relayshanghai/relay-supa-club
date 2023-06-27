6e11fc2916e78636477ec0c6da44aa938dc4cd2f
James <james@relay.club>
2023-06-27 06:23:28 +0000

    Update seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -20,7 +20,6 @@ OR REPLACE FUNCTION create_supabase_user(email TEXT, _first_name TEXT, _last_nam
       "confirmation_sent_at",
       "recovery_token",
       "recovery_sent_at",
-      "email_change_token_new",
       "email_change",
       "email_change_sent_at",
       "last_sign_in_at",
@@ -28,18 +27,7 @@ OR REPLACE FUNCTION create_supabase_user(email TEXT, _first_name TEXT, _last_nam
       "raw_user_meta_data",
       "is_super_admin",
       "created_at",
-      "updated_at",
-      "phone",
-      "phone_confirmed_at",
-      "phone_change",
-      "phone_change_token",
-      "phone_change_sent_at",
-      "email_change_token_current",
-      "email_change_confirm_status",
-      "banned_until",
-      "reauthentication_token",
-      "reauthentication_sent_at",
-      "is_sso_user"
+      "updated_at"
     ) VALUES (
       '00000000-0000-0000-0000-000000000000',
       user_id,
@@ -54,7 +42,6 @@ OR REPLACE FUNCTION create_supabase_user(email TEXT, _first_name TEXT, _last_nam
       '',
       NULL,
       '',
-      '',
       NULL,
       '2023-02-18 23:31:13.01781+00',
       '{"provider": "email",
@@ -67,18 +54,7 @@ OR REPLACE FUNCTION create_supabase_user(email TEXT, _first_name TEXT, _last_nam
       _last_name),
       NULL,
       now(),
-      now(),
-      NULL,
-      NULL,
-      '',
-      '',
-      NULL,
-      '',
-      0,
-      NULL,
-      '',
-      NULL,
-      'f'
+      now()
     );
 
     RETURN user_id;
