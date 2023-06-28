0fa4bff8f3b0fc2527ecb2c04bee4ce9249a4c5d
Ellie Wang <yuetingw@outlook.com>
2023-06-28 15:26:55 +0800

    get rid of old login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,20 +1,7 @@
-import { LegacyLoginSignupLayout } from 'src/components/LegacySignupLayout';
 import LoginPage from 'src/components/login-page';
 import { ScreenshotsCarousel } from 'src/components/signup/screenshots-carousel';
 import LoginSignupLayout from 'src/components/SignupLayout';
 
 export default function Login() {
-    const loginV2 = true;
-
-    return (
-        <>
-            {loginV2 ? (
-                <LoginSignupLayout left={<ScreenshotsCarousel />} right={<LoginPage />} />
-            ) : (
-                <LegacyLoginSignupLayout>
-                    <LoginPage />
-                </LegacyLoginSignupLayout>
-            )}
-        </>
-    );
+    return <LoginSignupLayout left={<ScreenshotsCarousel />} right={<LoginPage />} />;
 }
