5959501649dbc25b387109566e96e85481ac2b3f
Ellie Wang <yuetingw@outlook.com>
2023-06-28 15:24:43 +0800

    match login with new form wizard

--- src/components/login-page.tsx
+++ src/components/login-page.tsx
@@ -8,6 +8,7 @@ import { Input } from 'src/components/input';
 import { APP_URL } from 'src/constants';
 import { useFields } from 'src/hooks/use-fields';
 import { useUser } from 'src/hooks/use-user';
+import { FormWizard } from './signup/form-wizard';
 
 const LoginPage = () => {
     const { t } = useTranslation();
@@ -70,48 +71,53 @@ const LoginPage = () => {
         }
     };
     return (
-        <form className="mx-auto flex w-full max-w-xs flex-grow flex-col items-center justify-center space-y-2">
-            <div className="w-full text-left">
-                <h1 className="mb-2 text-4xl font-bold">{t('login.logIn')}</h1>
-                <h3 className="mb-8 text-sm text-gray-600">{t('login.welcomeBack')}</h3>
+        <div>
+            <FormWizard title={t('login.welcomeBack')} showProgress={false}>
+                <Input
+                    label={t('login.email')}
+                    type="email"
+                    placeholder="hello@relay.club"
+                    value={email}
+                    onChange={(e) => {
+                        setFieldValue('email', e.target.value);
+                    }}
+                />
+                <Input
+                    label={t('login.password')}
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => {
+                        setFieldValue('password', e.target.value);
+                    }}
+                    onKeyDown={(e) => handleKeyDown(e)}
+                />
+                <Button
+                    disabled={!email || !password || loggingIn}
+                    type="button"
+                    onClick={handleSubmit}
+                    className="mt-12 w-full"
+                >
+                    {t('login.logIn')}
+                </Button>
+            </FormWizard>
+            <div className="mb-2 mt-6 text-center">
+                <p className="inline text-sm text-gray-500">
+                    {t('login.dontHaveAnAccount')}{' '}
+                    <Link href="/signup" className="inline cursor-pointer text-primary-500 hover:text-primary-700">
+                        <Button variant="secondary" className="ml-2 px-1 pb-1 pt-1 text-xs">
+                            {t('login.signUp')}
+                        </Button>
+                    </Link>
+                </p>
             </div>
-            <p className="inline text-sm text-gray-500">
-                {t('login.dontHaveAnAccount')}{' '}
-                <Link href="/signup" className="inline cursor-pointer text-primary-700 hover:text-primary-600">
-                    <Button variant="secondary" className="ml-2 px-1 pb-1 pt-1 text-xs">
-                        {t('login.signUp')}
-                    </Button>
-                </Link>
-            </p>
-            <Input
-                label={t('login.email')}
-                type="email"
-                placeholder="hello@relay.club"
-                value={email}
-                onChange={(e) => {
-                    setFieldValue('email', e.target.value);
-                }}
-            />
-            <Input
-                label={t('login.password')}
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => {
-                    setFieldValue('password', e.target.value);
-                }}
-                onKeyDown={(e) => handleKeyDown(e)}
-            />
-            <Button disabled={!email || !password || loggingIn} type="button" onClick={handleSubmit}>
-                {t('login.logIn')}
-            </Button>
-
-            <button type="button" onClick={handleResetPassword} disabled={generatingResetEmail}>
-                <p className="mt-4 inline pb-4 text-sm text-gray-400 hover:text-primary-500">
-                    {t('login.forgotPasswordReset')}
+            <button type="button" onClick={handleResetPassword} disabled={generatingResetEmail} className="w-full">
+                <p className="mt-4 inline pb-4 text-sm text-gray-500 ">
+                    {t('login.forgotPassword')}{' '}
+                    <span className="text-primary-500 hover:text-primary-700">{t('login.resetPassword')}</span>
                 </p>
             </button>
-        </form>
+        </div>
     );
 };
 
