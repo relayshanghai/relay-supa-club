f0a3e6cfcf1e101ac9f095c6bd824150a4616582
Ellie Wang <yuetingw@outlook.com>
2023-06-30 12:14:55 +0800

    add tracker on landing page

--- src/components/signup/landing-page.tsx
+++ src/components/signup/landing-page.tsx
@@ -2,16 +2,23 @@ import Link from 'next/link';
 import { LanguageToggle } from '../common/language-toggle';
 import { PricingPage } from '../pricing/pricing-page';
 import { useTranslation } from 'react-i18next';
+import { useRudderstack } from 'src/hooks/use-rudderstack';
 
 export const LandingPage = () => {
     const { t } = useTranslation();
+    const { trackEvent } = useRudderstack();
+
     return (
         <div className="flex flex-grow flex-col">
             <div className="flex self-end p-5 align-baseline">
                 <LanguageToggle />
                 <p className="ml-3 font-medium text-gray-500">
                     {t('signup.alreadySignedUp')}
-                    <Link href="/login" className="ml-2 text-primary-500">
+                    <Link
+                        href="/login"
+                        className="ml-2 text-primary-500"
+                        onClick={() => trackEvent('ladingPage, go to Login Page')}
+                    >
                         {t('login.logIn')}
                     </Link>
                 </p>
