784444b31602c486a2496c95957c0803733fa820
Ellie Wang <yuetingw@outlook.com>
2023-07-03 18:18:07 +0800

    V2-538 init commit, create eventnames file

---
+++ src/utils/rudderstack/event-names.ts
@@ -0,0 +1,11 @@
+export const LANDING_PAGE = (details: string): string => {
+    return `Landing Page, ${details}`;
+};
+
+export const SIGNUP_WIZARD = (details: string): string => {
+    return `Signup Wizard, ${details}`;
+};
+
+export const AI_EMAIL_GENERATOR = (details: string): string => {
+    return `AI Email Generator, ${details}`;
+};
