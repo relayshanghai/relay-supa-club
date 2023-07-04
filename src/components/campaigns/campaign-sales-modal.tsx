7442e9e8c4da2c2e323b5a1e38b30b3c5ff74942
TGS963 <ghoshsuvojit2012@gmail.com>
2023-07-04 19:16:02 +0530

    fixed modal stylin

--- src/components/campaigns/campaign-sales-modal.tsx
+++ src/components/campaigns/campaign-sales-modal.tsx
@@ -20,29 +20,32 @@ export const CampaignSalesModal = ({
     return (
         <Modal
             visible={show}
-            maxWidth={`max-w-md`}
+            maxWidth={`max-w-sm`}
             onClose={() => setShow(false)}
             title={t('campaigns.addSalesModal.title') || ''}
         >
-            <div className="flex flex-row items-center justify-between gap-4">
-                <p>{t('campaigns.addSalesModal.currency')}</p>
-                <div className="w-full rounded-full">
-                    <input
-                        className="input-field"
-                        type="number"
-                        autoComplete="off"
-                        data-testid="campaign-sales-input"
-                        onChange={(e) => {
-                            const input = parseFloat(e.target.value);
-                            if (isNaN(input)) {
-                                toast.error('Please enter a number!');
-                                return;
-                            }
-                            setAmount(parseFloat(e.target.value));
-                        }}
-                    />
+            <div className="mt-6 flex flex-col items-center justify-between gap-6">
+                <div className="flex w-full flex-row items-center gap-2">
+                    <p>{t('campaigns.addSalesModal.currency')}</p>
+                    <div className="w-full rounded-full">
+                        <input
+                            className="input-field"
+                            type="number"
+                            autoComplete="off"
+                            data-testid="campaign-sales-input"
+                            onChange={(e) => {
+                                const input = parseFloat(e.target.value);
+                                if (isNaN(input)) {
+                                    toast.error('Please enter a number!');
+                                    return;
+                                }
+                                setAmount(parseFloat(e.target.value));
+                            }}
+                        />
+                    </div>
                 </div>
                 <Button
+                    className="w-full"
                     onClick={() => {
                         onAddSales(amount);
                         setShow(false);
