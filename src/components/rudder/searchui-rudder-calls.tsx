2547f5f60d6f240a00a00c6ec6b00ed8e3433f82
TGS963 <ghoshsuvojit2012@gmail.com>
2023-07-05 15:29:28 +0530

    converted into a hook

--- src/components/rudder/searchui-rudder-calls.tsx
+++ src/components/rudder/searchui-rudder-calls.tsx
@@ -1,71 +1,56 @@
-import { rudderInitialized } from 'src/utils/rudder-initialize';
+import { useRudderstack } from 'src/hooks/use-rudderstack';
 
-export const wordCloudAddTag = async (item: { word: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('WordCloud Component, Added tag from wordcloud', item);
-};
+export const useSearchTrackers = () => {
+    const { trackEvent } = useRudderstack();
 
-export const wordCloudRemoveTag = async (item: { word: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('WordCloud Component, Removed tag from wordcloud', item);
-};
-
-export const audienceSetAgeFrom = async (age: { lower: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set audience lower age filter limit', age);
-};
-
-export const audienceSetAgeTo = async (age: { upper: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set audience upper age filter limit', age);
-};
-
-export const audienceSetAgeWeight = async (age: { weight: number }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set audience age filter weight', age);
-};
-
-export const audienceSetGender = async (gender: { code: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set audience gender filter', gender);
-};
-
-export const audienceSetGenderWeight = async (gender: { weight: number }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set audience gender filter weight', gender);
-};
-
-export const influencerSetGender = async (gender: { code: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set influencer gender filter', gender);
-};
-
-export const influencerSetGenderWeight = async (gender: { weight: number }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set influencer gender filter weight', gender);
-};
-
-export const influencerSetHasEmail = async (contactInfo: { mode: string }) => {
-    if (!window.rudder) {
-        await rudderInitialized();
-    }
-    window.rudder.track('Search Options, Set influencer gender filter parameters', contactInfo);
+    const trackWordCloudAddTag = async (item: { word: string }) => {
+        trackEvent('WordCloud Component, Added tag from wordcloud', item);
+    };
+    const trackWordCloudRemoveTag = async (item: { word: string }) => {
+        trackEvent('WordCloud Component, Removed tag from wordcloud', item);
+    };
+    const trackAudienceAgeFrom = async (age: { lower: string }) => {
+        trackEvent('Search Options, Set audience lower age filter limit', age);
+    };
+    const trackAudienceAgeTo = async (age: { upper: string }) => {
+        trackEvent('Search Options, Set audience upper age filter limit', age);
+    };
+    const trackAudienceAgeWeight = async (age: { weight: number }) => {
+        trackEvent('Search Options, Set audience age filter weight', age);
+    };
+    const trackAudienceGender = async (gender: { code: string }) => {
+        trackEvent('Search Options, Set audience gender filter', gender);
+    };
+    const trackAudienceGenderWeight = async (gender: { weight: number }) => {
+        trackEvent('Search Options, Set audience gender filter weight', gender);
+    };
+    const trackInfluencerGender = async (gender: { code: string }) => {
+        trackEvent('Search Options, Set influencer gender filter', gender);
+    };
+    const trackInfluencerGenderWeight = async (gender: { weight: number }) => {
+        trackEvent('Search Options, Set influencer gender filter weight', gender);
+    };
+    const trackInfluencerHasEmail = async (contactInfo: { mode: string }) => {
+        trackEvent('Search Options, Set influencer gender filter parameters', contactInfo);
+    };
+    const trackKeyword = async (search: { keyword: string }) => {
+        trackEvent('Search Options, Set keyword', { search });
+    };
+    const trackHashtags = async (search: { hashtags: string[] }) => {
+        trackEvent('Search Options, Set keyword', { search });
+    };
+    return {
+        trackWordCloudAddTag,
+        trackWordCloudRemoveTag,
+        trackAudienceAgeFrom,
+        trackAudienceAgeTo,
+        trackAudienceAgeWeight,
+        trackAudienceGender,
+        trackAudienceGenderWeight,
+        trackInfluencerGender,
+        trackInfluencerGenderWeight,
+        trackInfluencerHasEmail,
+        trackKeyword,
+        trackHashtags,
+    };
 };
