2ce3942deb92f0af13ea16b4bedc568b21ce8db3
jacobcoro <xiaokourou@gmail.com>
2023-07-14 14:40:04 +0800

    feature flag

--- src/constants/feature-flags.ts
+++ src/constants/feature-flags.ts
@@ -1,3 +1,4 @@
 // for some context about feature flags, see `docs/feature-flags.md`
 
 export const featRecommended = () => process.env.NEXT_PUBLIC_FEAT_RECOMMENDED === 'true';
+export const featEmail = () => process.env.NEXT_PUBLIC_FEAT_EMAIL === 'true';
