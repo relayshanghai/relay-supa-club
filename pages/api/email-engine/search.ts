b6c8eb1c802eae1bb4ddb47b0a4ed5fad21ddbf9
jacobcoro <xiaokourou@gmail.com>
2023-07-18 16:54:20 +0800

    fix naming

--- pages/api/email-engine/search.ts
+++ pages/api/email-engine/search.ts
@@ -6,19 +6,19 @@ import { searchMailbox } from 'src/utils/api/email-engine';
 import { GMAIL_INBOX } from 'src/utils/api/email-engine/prototype-mocks';
 import type { AccountAccountSearchPost, MailboxSearchOptions } from 'types/email-engine/account-account-search-post';
 
-export type GetEmailPostRequestBody = {
+export type EmailSearchPostRequestBody = {
     account: string;
     search: MailboxSearchOptions;
     // mailboxPath: string; // using mock for now
     page?: number;
     pageSize?: number;
 };
-export type GetEmailPostResponseBody = AccountAccountSearchPost;
+export type EmailSearchPostResponseBody = AccountAccountSearchPost;
 
 const postHandler: NextApiHandler = async (req, res) => {
-    const { account, search, page, pageSize } = req.body as GetEmailPostRequestBody;
+    const { account, search, page, pageSize } = req.body as EmailSearchPostRequestBody;
 
-    const result: GetEmailPostResponseBody = await searchMailbox(account, search, GMAIL_INBOX, page, pageSize);
+    const result: EmailSearchPostResponseBody = await searchMailbox(account, search, GMAIL_INBOX, page, pageSize);
     return res.status(httpCodes.OK).json(result);
 };
 
