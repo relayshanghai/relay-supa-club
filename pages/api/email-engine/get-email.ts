7cdc19b9ffbeac2dd6d3316f6cd022f3e1c81980
jacobcoro <xiaokourou@gmail.com>
2023-07-18 09:34:23 +0800

    wip

---
+++ pages/api/email-engine/get-email.ts
@@ -0,0 +1,29 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import httpCodes from 'src/constants/httpCodes';
+import { ApiHandler } from 'src/utils/api-handler';
+import type { SendEmailRequestBody, SendEmailResponseBody } from 'src/utils/api/email-engine';
+import { sendEmail } from 'src/utils/api/email-engine';
+import { serverLogger } from 'src/utils/logger-server';
+
+// text.id is the id to query for the text, not messageId
+
+export type SendEmailPostRequestBody = SendEmailRequestBody & {
+    account: string;
+};
+export type SendEmailPostResponseBody = SendEmailResponseBody;
+const postHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { account, ...body } = req.body as SendEmailPostRequestBody;
+    try {
+        const result: SendEmailPostResponseBody = await sendEmail(body, account);
+        return res.status(httpCodes.OK).json(result);
+    } catch (error: any) {
+        serverLogger(error, 'error');
+        return res.status(httpCodes.INTERNAL_SERVER_ERROR).json({
+            error: error?.message || 'Internal server error',
+        });
+    }
+};
+
+export default ApiHandler({
+    postHandler,
+});
--- src/components/inbox/inbox-page.tsx
+++ src/components/inbox/inbox-page.tsx
@@ -1,5 +1,7 @@
 import { Layout } from '../layout';
 
+// const account = 'gprtldm3xqb0424p'; // Test account - brendan.relay@gmail.com
+
 export const InboxPage = () => {
     return (
         <Layout>
