9a412f4a0d5814f2432416380a1be50278217894
jacobcoro <xiaokourou@gmail.com>
2023-07-20 16:12:50 +0800

    sequences table start

--- src/components/sequences/sequences-page.tsx
+++ src/components/sequences/sequences-page.tsx
@@ -1,9 +1,17 @@
+import { mockInfluencers } from 'src/utils/api/email-engine/prototype-mocks';
 import { Layout } from '../layout';
+import SequenceTable from './sequence-table';
+import { Button } from '../button';
 
 export const SequencesPage = () => {
+    const handleStartSequence = () => {
+        //
+    };
     return (
         <Layout>
             <h1>Sequences</h1>
+            <Button onClick={handleStartSequence}>Start</Button>
+            <SequenceTable influencers={mockInfluencers} />
         </Layout>
     );
 };
