df2bd2050f03490b307dc293e217dbd00d925fa6
Ellie Wang <yuetingw@outlook.com>
2023-07-21 13:41:22 +0800

    add inbox tool bar and search filter"

--- src/components/inbox/inbox-page.tsx
+++ src/components/inbox/inbox-page.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import Fuse from 'fuse.js';
 import { Layout } from '../layout';
 import { nextFetch } from 'src/utils/fetcher';
 import { GMAIL_INBOX, GMAIL_SENT, testAccount } from 'src/utils/api/email-engine/prototype-mocks';
@@ -8,15 +9,19 @@ import type { EmailSearchPostRequestBody, EmailSearchPostResponseBody } from 'pa
 import { clientLogger } from 'src/utils/logger-client';
 import { Email } from './Email';
 import { PreviewCard } from './preview-card';
+import { ToolBar } from './tool-bar';
 
 export const InboxPage = () => {
     const [messages, setMessages] = useState<MessagesGetMessage[]>([]);
+    const [searchResults, setSearchResults] = useState<MessagesGetMessage[]>([]);
     const [loadingMessages, setLoadingMessages] = useState(false);
     const [getMessagesError, setGetMessagesError] = useState('');
 
     const [selectedMessages, setSelectedMessages] = useState<EmailSearchPostResponseBody['messages'] | null>(null);
     const [loadingSelectedMessages, setLoadingSelectedMessages] = useState(false);
     const [getSelectedMessagesError, setGetSelectedMessagesError] = useState('');
+    const [selectedTab, setSelectedTab] = useState('new');
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
     const getMessages = useCallback(async () => {
         setLoadingMessages(true);
@@ -40,6 +45,37 @@ export const InboxPage = () => {
             getMessages();
         }
     }, [getMessages, loadingMessages, messages.length]);
+
+    const filteredMessages = useMemo(() => {
+        if (selectedTab === 'new') {
+            return messages.filter((message) => message.unseen);
+        }
+        return messages;
+    }, [messages, selectedTab]);
+
+    useEffect(() => {
+        if (searchTerm === '') {
+            setSearchResults([]);
+            return;
+        }
+        const fuse = new Fuse(filteredMessages, {
+            keys: ['from.name', 'from.address', 'to.name', 'to.address', 'date', 'subject'],
+        });
+        const results = fuse.search(searchTerm);
+        setSearchResults(results.map((result) => result.item));
+    }, [filteredMessages, searchTerm]);
+
+    const renderMessagesPreview = (messages: MessagesGetMessage[]) => {
+        return messages.map((message) => (
+            <div key={message.id}>
+                <PreviewCard
+                    message={message}
+                    handleGetThreadEmails={handleGetThreadEmails}
+                    loadingSelectedMessages={loadingSelectedMessages}
+                />
+            </div>
+        ));
+    };
 
     const handleGetThreadEmails = async (message: MessagesGetMessage) => {
         setSelectedMessages([]);
@@ -89,35 +125,41 @@ export const InboxPage = () => {
                 {loadingMessages && <p>Loading...</p>}
                 {getMessagesError && <p>Error: {getMessagesError}</p>}
                 {messages.length === 0 && !loadingMessages && !getMessagesError && <p>No messages</p>}
-                {messages.length > 0 && (
-                    <ul className="h-full w-1/2 overflow-y-auto">
-                        {messages.map((message) => (
-                            <div key={message.id}>
-                                <PreviewCard
-                                    message={message}
-                                    handleGetThreadEmails={handleGetThreadEmails}
-                                    loadingSelectedMessages={loadingSelectedMessages}
-                                />
-                            </div>
-                        ))}
-                    </ul>
-                )}
-                {getSelectedMessagesError && <p>Error: {getSelectedMessagesError}</p>}
-                {selectedMessages && (
-                    <ul className="w-1/2">
-                        {loadingSelectedMessages && <p>Loading...</p>}
-                        {selectedMessages.map((message) => (
-                            <li className="m-2 border border-black p-2" key={message.id}>
-                                <Email message={message} />
-                            </li>
-                        ))}
-                    </ul>
-                )}
-                {!selectedMessages && !loadingMessages && (
-                    <div className="font-sm m-auto flex h-full items-center justify-between overflow-y-auto text-gray-500">
-                        No message has been selected yet.
-                    </div>
-                )}
+                <div className="w-2/5">
+                    {messages.length > 0 && (
+                        <>
+                            <ToolBar
+                                selectedTab={selectedTab}
+                                setSelectedTab={setSelectedTab}
+                                searchTerm={searchTerm}
+                                setSearchTerm={setSearchTerm}
+                            />
+                            <ul className="h-full overflow-y-auto">
+                                {searchResults.length > 0
+                                    ? renderMessagesPreview(searchResults)
+                                    : renderMessagesPreview(filteredMessages)}
+                            </ul>
+                        </>
+                    )}
+                </div>
+                <div className="w-3/5">
+                    {getSelectedMessagesError && <p>Error: {getSelectedMessagesError}</p>}
+                    {selectedMessages && (
+                        <ul>
+                            {loadingSelectedMessages && <p>Loading...</p>}
+                            {selectedMessages.map((message) => (
+                                <li className="m-2 border border-black p-2" key={message.id}>
+                                    <Email message={message} />
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                    {!selectedMessages && !loadingMessages && (
+                        <div className="font-sm flex h-full items-center justify-center overflow-y-auto text-gray-500">
+                            No message has been selected yet.
+                        </div>
+                    )}
+                </div>
             </div>
         </Layout>
     );
---
+++ src/components/inbox/tool-bar.tsx
@@ -0,0 +1,62 @@
+import type { Dispatch, SetStateAction } from 'react';
+import { debounce } from 'src/utils/debounce';
+
+export const ToolBar = ({
+    selectedTab,
+    setSelectedTab,
+    setSearchTerm,
+}: {
+    selectedTab: string;
+    setSelectedTab: Dispatch<SetStateAction<string>>;
+    searchTerm: string;
+    setSearchTerm: Dispatch<SetStateAction<string>>;
+}) => {
+    const handleTabChange = (tab: { value: string; name: string }) => {
+        if (tab.value === selectedTab) return;
+        setSelectedTab(tab.value);
+    };
+
+    const handleInputChange = debounce((e: any) => {
+        setSearchTerm(e.target.value);
+    }, 1000);
+
+    const tabs = [
+        {
+            value: 'new',
+            name: 'NEW',
+        },
+        {
+            value: 'inbox',
+            name: 'INBOX',
+        },
+    ];
+
+    return (
+        <div className=" space-y-4 border-b-2 border-r-2 border-tertiary-200 p-3">
+            <div className="flex justify-around text-sm font-semibold text-gray-600">
+                {tabs.map((tab) => (
+                    <div
+                        key={tab.value}
+                        onClick={() => handleTabChange(tab)}
+                        className={`cursor-pointer hover:text-primary-500 ${
+                            selectedTab === tab.value &&
+                            'text-primary-500 underline decoration-current decoration-2 underline-offset-2'
+                        }`}
+                    >
+                        {tab.name}
+                    </div>
+                ))}
+            </div>
+            <div className="px-3">
+                <input
+                    className="block w-full appearance-none rounded-md border border-gray-200 bg-white px-3 py-2 text-gray-600 placeholder-gray-400 ring-1 ring-gray-900 ring-opacity-5 placeholder:text-xs focus:outline-none"
+                    placeholder="Search for name, email or content"
+                    id="influencer-search"
+                    onChange={(e) => {
+                        handleInputChange(e);
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
