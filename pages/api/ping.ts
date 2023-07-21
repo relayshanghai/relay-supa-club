aec4116731abb06f5056b6a599406b533952a565
jacobcoro <xiaokourou@gmail.com>
2023-07-21 10:12:58 +0800

    remove ping endpoint

--- pages/api/ping.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiHandler } from 'next';
-
-const handler: NextApiHandler = async (_req, res) => {
-    return res.status(200).json({ message: 'pong' });
-};
-
-export default handler;
