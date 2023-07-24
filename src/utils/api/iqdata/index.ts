b21e0bb0100fc94321ad2c9e790b4d76dcea5278
TGS963 <ghoshsuvojit2012@gmail.com>
2023-07-24 08:35:29 +0530

    switched back to API endpoints

--- src/utils/api/iqdata/index.ts
+++ src/utils/api/iqdata/index.ts
@@ -14,7 +14,7 @@ export const IQDATA_URL = 'https://socapi.icu/v2.0/api/';
  * @returns fetch .json() data
  * @description fetcher for IQData API. API docs: https://iqdata.social/docs/api
  */
-export const iqDataFetch = async <T = any>(path: string, action: string, options: RequestInit = {}) => {
+export const iqDataFetch = async <T = any>(path: string, options: RequestInit = {}) => {
     const res = await fetch(IQDATA_URL + path, {
         ...options,
         headers: {
@@ -22,7 +22,7 @@ export const iqDataFetch = async <T = any>(path: string, action: string, options
             ...options.headers,
         },
     });
-    await handleResError(res, action);
+    await handleResError(res, path);
     const json = await res.json();
     return json as T;
 };
@@ -34,29 +34,21 @@ export const iqDataFetch = async <T = any>(path: string, action: string, options
 // 2. Influencer lookalike: help you to find influencers that post similar content
 // These two option returns different data, so we need to use both in our search in order to give customer the best results.
 export const fetchIqDataLookalikeByAudience = async (term: string, platform: CreatorPlatform) =>
-    await iqDataFetch(
-        `dict/users/?q=${term}&type=lookalike&platform=${platform}&limit=5`,
-        fetchIqDataLookalikeByAudience.name,
-    ); //Audience lookalike
+    await iqDataFetch(`dict/users/?q=${term}&type=lookalike&platform=${platform}&limit=5`); //Audience lookalike
 
 export const fetchIqDataLookalikeByInfluencer = async (term: string, platform: CreatorPlatform) =>
-    await iqDataFetch(
-        `dict/users/?q=${term}&type=topic-tags&platform=${platform}&limit=5`,
-        fetchIqDataLookalikeByInfluencer.name,
-    ); //Influencer lookalike
+    await iqDataFetch(`dict/users/?q=${term}&type=topic-tags&platform=${platform}&limit=5`); //Influencer lookalike
 
 export const fetchIqDataTopics = async (term: string, platform: CreatorPlatform, limit = 10) =>
-    await iqDataFetch(`dict/topic-tags/?q=${term}&platform=${platform}&limit=${limit}`, fetchIqDataTopics.name);
+    await iqDataFetch(`dict/topic-tags/?q=${term}&platform=${platform}&limit=${limit}`);
 
-export const fetchIqDataGeos = async (term: string) =>
-    await iqDataFetch(`geos/?q=${term}&types=country&limit=5`, fetchIqDataGeos.name);
+export const fetchIqDataGeos = async (term: string) => await iqDataFetch(`geos/?q=${term}&types=country&limit=5`);
 
 export const fetchCreatorsFiltered = async (params: FetchCreatorsFilteredParams) => {
     const { platform, body } = prepareFetchCreatorsFiltered(params);
 
     return await iqDataFetch<CreatorSearchResult>(
         `search/newv1?${new URLSearchParams({ platform, auto_unhide: '1' })}`,
-        fetchCreatorsFiltered.name,
         {
             method: 'post',
             body: JSON.stringify(body),
@@ -67,27 +59,16 @@ export const fetchCreatorsFiltered = async (params: FetchCreatorsFilteredParams)
 export const requestNewReport = async (platform: CreatorPlatform, id: string, subscribe = false, dry_run = false) =>
     await iqDataFetch<CreatorReport>(
         `reports/new?platform=${platform}&url=${id}&subscribe=${subscribe ? 1 : 0}&dry_run=${dry_run}`,
-        requestNewReport.name,
     );
 
-export const fetchReport = async (reportId: string) =>
-    await iqDataFetch<CreatorReport>(`reports/${reportId}`, fetchReport.name);
+export const fetchReport = async (reportId: string) => await iqDataFetch<CreatorReport>(`reports/${reportId}`);
 
 //** omit id to get all previously generated reports of that platform */
 export const fetchReportsMetadata = async (platform: CreatorPlatform, creator_id?: string) =>
-    await iqDataFetch<CreatorReportsMetadata>(
-        `reports?platform=${platform}${creator_id ? `&url=${creator_id}` : ''}`,
-        fetchReportsMetadata.name,
-    );
+    await iqDataFetch<CreatorReportsMetadata>(`reports?platform=${platform}${creator_id ? `&url=${creator_id}` : ''}`);
 
 export const fetchYoutubeVideoInfo = async (videoUrl: string) =>
-    await iqDataFetch<YoutubeVideoDataRaw>(
-        `raw/yt/video?${new URLSearchParams({ url: videoUrl })}`,
-        fetchYoutubeVideoInfo.name,
-    );
+    await iqDataFetch<YoutubeVideoDataRaw>(`raw/yt/video?${new URLSearchParams({ url: videoUrl })}`);
 
 export const fetchTiktokVideoInfo = async (videoUrl: string) =>
-    await iqDataFetch<TikTokVideoDataRaw>(
-        `raw/tt/user/media?${new URLSearchParams({ url: videoUrl })}`,
-        fetchTiktokVideoInfo.name,
-    );
+    await iqDataFetch<TikTokVideoDataRaw>(`raw/tt/user/media?${new URLSearchParams({ url: videoUrl })}`);
