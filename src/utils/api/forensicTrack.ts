9a113f535ac12de8d8c1ee44701d36344ccc74ff
TGS963 <ghoshsuvojit2012@gmail.com>
2023-08-01 07:42:42 +0530

    added new library to get proper introspection

--- src/utils/api/forensicTrack.ts
+++ src/utils/api/forensicTrack.ts
@@ -1,10 +1,17 @@
 import { serverLogger } from '../logger-server';
+import callsites from 'callsites';
 import type { ServerContext } from './iqdata';
 
-export const forensicTrack = (context: ServerContext, caller?: string) => {
+export const forensicTrack = async (context: ServerContext, caller?: string) => {
+    const calls = await callsites();
+    const trace = calls.map((call) => {
+        return call.getFunctionName();
+    });
+
     const sentryPayload = {
         metadata: context.metadata,
         caller,
+        trace,
     };
     serverLogger(sentryPayload, 'error', true);
 };
