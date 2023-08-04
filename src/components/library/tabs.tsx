2fe3cd16cfd5f378585f2f41457dbaf23d0b0681
jacobcoro <xiaokourou@gmail.com>
2023-08-04 14:18:38 +0800

    style nits

--- src/components/library/tabs.tsx
+++ src/components/library/tabs.tsx
@@ -10,13 +10,15 @@ export const Tabs = ({ currentTab, setCurrentTab, tabs }: TabsProps) => {
 
     return (
         <div>
-            <div className="mb-6 flex items-center space-x-4">
+            <div className="mb-6 flex ">
                 {tabs.map((tab, index) => (
                     <button
                         key={index}
                         onClick={() => setCurrentTab(tab.value)}
-                        className={`flex space-x-2 border-b-2 px-4 py-2 text-sm font-medium hover:border-b-primary-700 ${
-                            currentTab === tab.value ? 'border-b-primary-500' : 'bg-gray-100 text-gray-400'
+                        className={`flex space-x-2 border-b-2 px-4 py-2 text-sm font-medium  ${
+                            currentTab === tab.value
+                                ? 'border-b-primary-600 text-primary-600'
+                                : 'border-b-gray-200 text-gray-500 hover:border-b-primary-400 hover:text-primary-400'
                         }`}
                     >
                         <p>{t(tab.label)}</p>
