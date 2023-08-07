38e098f858f83b62a869a67865e6e19b854befe0
Ellie Wang <yuetingw@outlook.com>
2023-08-07 09:53:50 +0800

    add Info icon on modal

--- src/components/modal-add-to-sequence.tsx
+++ src/components/modal-add-to-sequence.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { Modal } from './modal';
 import { Button } from './button';
+import { InfoIcon } from './icons';
 
 export const AddToSequenceModal = ({
     show,
@@ -29,8 +30,11 @@ export const AddToSequenceModal = ({
                         <div className="font-semibold text-gray-800">{t('creators.sequence')}</div>
                         <div>dropdown select</div>
                     </div>
-                    <div className="rounded-md bg-blue-50 p-4">
-                        {t('creators.addToSequenceNotes')} {new Date().toLocaleDateString(i18n.language)}{' '}
+                    <div className="flex items-start rounded-md bg-blue-50 p-4">
+                        <InfoIcon className="mr-4 mt-1 box-border h-4 w-4 flex-none text-blue-500" />
+                        <div className="text-gray-500">
+                            {t('creators.addToSequenceNotes')} {new Date().toLocaleDateString(i18n.language)}{' '}
+                        </div>
                     </div>
                 </div>
                 <div className="flex justify-end space-x-3 p-6">
