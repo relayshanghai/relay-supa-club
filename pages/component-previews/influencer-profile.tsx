1a1c079cda90fb930a55a202cfe0ac69dd15fc7f
James <james@relay.club>
2023-08-08 08:27:18 +0000

    Add events for overlay component

--- pages/component-previews/influencer-profile.tsx
+++ pages/component-previews/influencer-profile.tsx
@@ -1,10 +1,32 @@
-import { OverlayRight } from '../../src/components/influencer-profile/components/overlay-right';
-import { ProfileScreen } from '../../src/components/influencer-profile/screens/profile-screen';
+import { useCallback, useState } from 'react';
+import { OverlayRight } from 'src/components/influencer-profile/components/overlay-right';
+import { ProfileScreen } from 'src/components/influencer-profile/screens/profile-screen';
 
 export default function ComponentStage() {
+    const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
+    const toggleOverlay = useCallback(() => {
+        setIsOverlayOpen((s) => !s);
+    }, []);
+
     return (
         <>
-            <OverlayRight>
+            <button onClick={toggleOverlay} type="button">
+                Toggle Overlay
+            </button>
+
+            <OverlayRight
+                isOpen={isOverlayOpen}
+                onClose={() => {
+                    toggleOverlay();
+                    // eslint-disable-next-line no-console
+                    console.log('overlay closed!');
+                }}
+                onOpen={() => {
+                    // eslint-disable-next-line no-console
+                    console.log('overlay opened!');
+                }}
+            >
                 <ProfileScreen />
             </OverlayRight>
         </>
--- src/components/influencer-profile/components/overlay-right.tsx
+++ src/components/influencer-profile/components/overlay-right.tsx
@@ -1,48 +1,38 @@
 import type { PropsWithChildren } from 'react';
 import React from 'react';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
 import { cls } from 'src/utils/classnames';
 
 type Props = PropsWithChildren<{
+    isOpen?: boolean;
     onOpen?: () => void;
     onClose?: () => void;
 }>;
 
 export const OverlayRight = ({ children, ...props }: Props) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const { isOpen } = { isOpen: false, ...props };
 
-    const toggleOverlay = useCallback(() => {
-        setIsOpen((state) => !state);
-    }, [setIsOpen]);
+    const closeOverlay = useCallback(() => {
+        props.onClose && props.onClose();
+    }, [props]);
 
     useEffect(() => {
         isOpen && props.onOpen && props.onOpen();
-        !isOpen && props.onClose && props.onClose();
     }, [isOpen, props]);
 
-    const overlayCls = useMemo(() => cls({ '-translate-x-0': isOpen }), [isOpen]);
+    const overlayCls = useMemo(() => cls({ 'translate-x-full': !isOpen }), [isOpen]);
     const backdropCls = useMemo(() => cls({ hidden: !isOpen }), [isOpen]);
 
     return (
         <>
-            <nav className="flex space-x-2">
-                <button
-                    onClick={() => toggleOverlay()}
-                    type="button"
-                    className={`inline-flex grow basis-0 items-center justify-center gap-2 rounded-lg bg-transparent px-4 py-3 text-center text-sm font-medium text-gray-500 hover:text-blue-600 dark:hover:text-gray-300 dark:hover:text-gray-400`}
-                >
-                    Toggle Overlay
-                </button>
-            </nav>
-
             <div
-                className={`${overlayCls} fixed right-0 top-0 z-[60] h-full w-full max-w-md translate-x-full transform bg-white transition-all duration-300`}
+                className={`${overlayCls} fixed right-0 top-0 z-[60] h-full w-full max-w-md transform bg-white transition-all duration-300`}
                 tabIndex={-1}
             >
                 <div className="float-right flex px-3 py-3">
                     <button
-                        onClick={() => toggleOverlay()}
+                        onClick={() => closeOverlay()}
                         type="button"
                         className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-md text-gray-500 hover:text-gray-700 focus:outline-none"
                     >
@@ -53,7 +43,7 @@ export const OverlayRight = ({ children, ...props }: Props) => {
             </div>
 
             <div
-                onClick={() => toggleOverlay()}
+                onClick={() => closeOverlay()}
                 className={`${backdropCls} duration hs-overlay-backdrop fixed inset-0 z-50 bg-gray-400 bg-opacity-80 transition dark:bg-opacity-80`}
             />
         </>
