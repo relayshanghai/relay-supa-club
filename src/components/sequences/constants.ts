5c7ca20be4d255791299defa4d5b2ab9639bfc1b
jacobcoro <xiaokourou@gmail.com>
2023-08-08 15:40:50 +0800

    test all the right columns show

---
+++ src/components/sequences/constants.ts
@@ -0,0 +1,21 @@
+import type sequences from 'i18n/en/sequences';
+import type { SequenceInfluencer } from 'src/utils/api/db';
+
+export type SequenceColumn = keyof (typeof sequences)['columns'];
+export const columnsNeedsAttention: SequenceColumn[] = [
+    'name',
+    'email',
+    'influencerTopics',
+    'dateAdded',
+    'sequenceActions',
+];
+export const columnsInSequence: SequenceColumn[] = ['name', 'currentStep', 'status', 'sendTime', 'nextEmailPreview'];
+export const columnsIgnored: SequenceColumn[] = ['name', 'lastEmailSent', 'status', 'restartSequence'];
+export const sequenceColumns = (currentTab: SequenceInfluencer['funnel_status']) =>
+    currentTab === 'To Contact'
+        ? columnsNeedsAttention
+        : currentTab === 'In Sequence'
+        ? columnsInSequence
+        : currentTab === 'Ignored'
+        ? columnsIgnored
+        : [];
--- src/components/sequences/sequence-table.tsx
+++ src/components/sequences/sequence-table.tsx
@@ -1,26 +1,8 @@
 import type { SequenceInfluencer, SequenceEmail, SequenceStep } from 'src/utils/api/db';
 import SequenceRow from './sequence-row';
 import { useTranslation } from 'react-i18next';
-import type sequences from 'i18n/en/sequences';
+import { sequenceColumns } from './constants';
 
-export type SequenceColumn = keyof (typeof sequences)['columns'];
-export const columnsNeedsAttention: SequenceColumn[] = [
-    'name',
-    'email',
-    'influencerTopics',
-    'dateAdded',
-    'sequenceActions',
-];
-export const columnsInSequence: SequenceColumn[] = ['name', 'currentStep', 'status', 'sendTime', 'nextEmailPreview'];
-export const columnsIgnored: SequenceColumn[] = ['name', 'lastEmailSent', 'status', 'restartSequence'];
-export const sequenceColumns = (currentTab: SequenceInfluencer['funnel_status']) =>
-    currentTab === 'To Contact'
-        ? columnsNeedsAttention
-        : currentTab === 'In Sequence'
-        ? columnsInSequence
-        : currentTab === 'Ignored'
-        ? columnsIgnored
-        : [];
 interface SequenceTableProps {
     sequenceInfluencers: SequenceInfluencer[];
     allSequenceEmails?: SequenceEmail[];
