2bdafcf66c1d765f35a3319c1f52602a6c761ef7
James <james@relay.club>
2023-08-10 00:49:02 +0000

    Use date field in CollabScheduledPostDateInput

--- src/components/influencer-profile/components/collab-scheduled-post-date-input.tsx
+++ src/components/influencer-profile/components/collab-scheduled-post-date-input.tsx
@@ -1,14 +1,29 @@
+import type { InputHTMLAttributes } from 'react';
 import React from 'react';
-import { TextInputComponent } from 'src/components/library/forms/text-input';
+import dateFormat from 'src/utils/dateFormat';
 
 type Props = {
     //
-} & (typeof TextInputComponent)['defaultProps'];
+} & InputHTMLAttributes<HTMLInputElement>;
 
 export const CollabScheduledPostDateInput = (props: Props) => {
     return (
         <>
-            <TextInputComponent label="Scheduled Post Date" {...props} />
+            <label className="flex w-full flex-col text-sm text-gray-800">
+                <div className="font-semibold">Scheduled Post Date</div>
+                <div>
+                    <input
+                        value={dateFormat(String(props.value ?? ''), 'isoDate', true, true)}
+                        type="date"
+                        name="collab-scheduled-post-date"
+                        className="my-2 block w-full appearance-none rounded-md border border-transparent bg-white px-3 py-2 text-gray-900 placeholder-gray-400 shadow ring-1 ring-gray-900 ring-opacity-5 focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-xs"
+                        {...props}
+                    />
+                </div>
+                <span>
+                    <p className="text-xs">&nbsp;</p>
+                </span>
+            </label>
         </>
     );
 };
