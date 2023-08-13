88dbf59de9793392edd42f4acb485daed4e29bf3
jacobcoro <xiaokourou@gmail.com>
2023-08-13 14:31:52 +0800

    templates hook and endpoint

---
+++ pages/api/email-engine/templates.ts
@@ -0,0 +1,25 @@
+import type { NextApiHandler } from 'next';
+import httpCodes from 'src/constants/httpCodes';
+import { ApiHandler } from 'src/utils/api-handler';
+import { getTemplateInfo } from 'src/utils/api/email-engine';
+
+import type { TemplatesTemplateGetResponse } from 'types/email-engine/templates-template-get';
+
+export type TemplatesPostRequestBody = {
+    templateIds: string[];
+};
+export type TemplatesPostResponseBody = TemplatesTemplateGetResponse[];
+
+const postHandler: NextApiHandler = async (req, res) => {
+    const { templateIds } = req.body as TemplatesPostRequestBody;
+
+    const result: TemplatesPostResponseBody = await Promise.all(
+        templateIds.map(async (templateId) => {
+            const templateInfo = await getTemplateInfo(templateId);
+            return templateInfo;
+        }),
+    );
+    return res.status(httpCodes.OK).json(result);
+};
+
+export default ApiHandler({ postHandler });
