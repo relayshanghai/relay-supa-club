895ae705b5dd024b2b1bc7c3c67b38882c1976de
James <james@relay.club>
2023-08-14 10:25:19 +0000

    Add missing create-store

---
+++ src/utils/create-store.ts
@@ -0,0 +1,54 @@
+import { useSyncExternalStore } from 'react';
+
+const createInternalStore = <T>(initialState: T) => {
+    let state = initialState;
+    const listeners = new Set<() => void>();
+
+    function setStore(value: T | ((state: T) => T)) {
+        if (value instanceof Function) {
+            state = value(state);
+        }
+
+        if (!(value instanceof Function)) {
+            state = value;
+        }
+
+        listeners.forEach((listener) => listener());
+    }
+
+    function getSnapshot() {
+        return state;
+    }
+
+    function subscribe(listener: () => void) {
+        listeners.add(listener);
+
+        return () => {
+            listeners.delete(listener);
+        };
+    }
+
+    return {
+        setStore,
+        getSnapshot,
+        subscribe,
+    };
+};
+
+/**
+ * Helper function for creating storages of sharable state between components
+ *
+ * This can be easily be over-used so please use sparingly. Preferrably for sharing states that
+ * are "small" such as user interface data (e.g. isDarkTheme, isModalOpen).
+ */
+export const createStore = <T>(initialValue: T) => {
+    const store = createInternalStore<T>(initialValue);
+
+    const useStore = () => {
+        const externalStore = useSyncExternalStore(store.subscribe, store.getSnapshot);
+
+        return [externalStore, store.setStore] as [T, typeof store.setStore];
+    };
+
+    return useStore;
+};
