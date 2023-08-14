ebbefee52a334d25440f9dee8b0d20c7c607ee84
Ellie Wang <yuetingw@outlook.com>
2023-08-14 17:06:48 +0800

    update email view card

--- src/components/inbox/Threads.tsx
+++ src/components/inbox/Threads.tsx
@@ -6,11 +6,15 @@ import { cleanEmailBody } from 'src/utils/clean-html';
 import { testEmail } from 'src/utils/api/email-engine/prototype-mocks';
 import CommentCardsSkeleton from '../campaigns/comment-cards-skeleton';
 import { useTranslation } from 'react-i18next';
+import type { ReplyTo } from 'types/email-engine/account-account-message-get';
+import { ChevronDown } from '../icons';
 
 export interface ThreadMessage {
     subject: string;
     id: string;
     from: string;
+    to: ReplyTo[];
+    cc: ReplyTo[];
     date: string;
     text: string;
     isMe?: boolean;
@@ -41,6 +45,8 @@ export const Threads = ({ messages }: { messages: SearchResponseMessage[] }) => 
                     subject: message.subject,
                     id: message.id,
                     from: message.from.name || message.from.address,
+                    to: message.to,
+                    cc: message.cc,
                     date: message.date,
                     text: html,
                     isMe: message.from.address === testEmail,
@@ -73,30 +79,37 @@ export const Threads = ({ messages }: { messages: SearchResponseMessage[] }) => 
             ) : (
                 <div className="flex w-full flex-col space-y-6">
                     {threadMessages.map((message) => (
-                        <div
-                            key={message.id}
-                            className={`flex w-[31.25rem] flex-col ${message.isMe ? 'self-end' : 'self-start'}`}
-                        >
-                            <div
-                                className={`whitespace-normal rounded-md border border-primary-200 p-4 ${
-                                    message.isMe ? ' bg-primary-100 bg-opacity-70' : ''
-                                }`}
-                            >
+                        <div key={message.id} className="rounded-lg bg-white">
+                            <div className="border-b-2 border-gray-200 p-6">
+                                <div className="mb-3 text-lg font-semibold text-gray-400">
+                                    From: <span className="text-gray-600">{message.isMe ? 'Me' : message.from}</span>{' '}
+                                </div>
+                                <div className="mb-3 text-sm font-medium text-gray-400">
+                                    To: <span className="font-light">{message.to[0]?.address}</span>{' '}
+                                </div>
+                            </div>
+                            <details className="p-6">
+                                <summary className="flex list-none justify-end">
+                                    <div className="mr-3 font-medium text-gray-300">
+                                        {' '}
+                                        {new Date(message.date).toLocaleDateString(i18n.language, {
+                                            month: 'short',
+                                            day: 'numeric',
+                                        })}{' '}
+                                        {new Date(message.date).toLocaleTimeString(i18n.language, {
+                                            hour: 'numeric',
+                                            minute: 'numeric',
+                                        })}
+                                    </div>
+                                    <ChevronDown className="h-6 w-6 flex-none stroke-gray-400 stroke-2 text-white" />
+                                </summary>
                                 <div
                                     className="text-sm"
                                     dangerouslySetInnerHTML={{
                                         __html: cleanEmailBody(message.text),
                                     }}
                                 />
-                            </div>
-                            <div className={`${message.isMe ? 'self-end' : 'self-start'} mt-3 text-xs text-gray-400`}>
-                                {new Date(message.date).toLocaleDateString(i18n.language, {
-                                    year: 'numeric',
-                                    month: 'short',
-                                    day: 'numeric',
-                                })}{' '}
-                                {new Date(message.date).toLocaleTimeString(i18n.language)}
-                            </div>
+                            </details>
                         </div>
                     ))}
                     <div ref={endOfThread} />
