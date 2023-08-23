b5ac63227e664b8fe618c81a9d5374c9246b0474
TGS963 <ghoshsuvojit2012@gmail.com>
2023-08-23 16:32:45 +0530

    fixed deletion not showing

--- src/components/sequences/sequence-page.tsx
+++ src/components/sequences/sequence-page.tsx
@@ -23,7 +23,7 @@ export const SequencePage = ({ sequenceId }: { sequenceId: string }) => {
     const { t } = useTranslation();
 
     const { sequence, sendSequence, sequenceSteps, updateSequence } = useSequence(sequenceId);
-    const { sequenceInfluencers } = useSequenceInfluencers(sequence && [sequenceId]);
+    const { sequenceInfluencers, refreshSequenceInfluencers } = useSequenceInfluencers(sequence && [sequenceId]);
     const { sequenceEmails } = useSequenceEmails(sequenceId);
     const { templateVariables } = useTemplateVariables(sequenceId);
     const missingVariables = templateVariables
@@ -35,8 +35,10 @@ export const SequencePage = ({ sequenceId }: { sequenceId: string }) => {
     const [influencers, setInfluencers] = useState<SequenceInfluencerManagerPage[] | undefined>(sequenceInfluencers);
 
     useEffect(() => {
-        if (!influencers || influencers.length === 0) setInfluencers(sequenceInfluencers);
-    }, [sequenceInfluencers, influencers]);
+        if (!influencers || influencers.length === 0 || filterSteps.length === 0) {
+            setInfluencers(sequenceInfluencers);
+        }
+    }, [sequenceInfluencers, influencers, refreshSequenceInfluencers, filterSteps]);
 
     const handleStep = useCallback(
         (filters: CommonStatusType[]) => {
