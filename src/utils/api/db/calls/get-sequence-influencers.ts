bf4a94392bfd5a9e1873e78c1c13a0d7c2fb9101
jacobcoro <xiaokourou@gmail.com>
2023-08-24 12:14:27 +0800

    fix db calls

--- src/utils/api/db/calls/get-sequence-influencers.ts
+++ src/utils/api/db/calls/get-sequence-influencers.ts
@@ -1,7 +1,5 @@
 import { getInfluencerSocialProfileByIdCall as getInfluencerSocialProfileById } from '../../../../utils/api/db/calls/influencers';
 import { getSequenceInfluencersBySequenceIdCall as getSequenceInfluencersBySequenceId } from '../../../../utils/api/db/calls/sequence-influencers';
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import type { DatabaseWithCustomTypes } from 'types';
 import type { ServerContext } from '../../../../utils/api/iqdata';
 import { type SequenceInfluencerManagerPage } from 'pages/api/sequence/influencers';
 import { getSequenceByIdCall } from './sequences';
@@ -9,20 +7,21 @@ import { db } from '../../../../utils/supabase-client';
 import { serverLogger } from '../../../../utils/logger-server';
 
 export const getSequenceInfluencers = async (ctx: ServerContext, sequenceId: string) => {
-    const supabase = createServerSupabaseClient<DatabaseWithCustomTypes>(ctx);
-    const influencers = await getSequenceInfluencersBySequenceId(supabase)(sequenceId);
+    const influencers = await db<typeof getSequenceInfluencersBySequenceId>(getSequenceInfluencersBySequenceId)(
+        sequenceId,
+    );
 
     const results: SequenceInfluencerManagerPage[] = [];
 
     for (const influencer of influencers) {
         try {
-            const sequence = await db(getSequenceByIdCall)(supabase)(influencer.sequence_id);
-            const influencerInfo = await getInfluencerSocialProfileById(supabase)(
+            const sequence = await db<typeof getSequenceByIdCall>(getSequenceByIdCall)(influencer.sequence_id);
+            const influencerInfo = await db<typeof getInfluencerSocialProfileById>(getInfluencerSocialProfileById)(
                 influencer.influencer_social_profile_id,
             );
             results.push({
                 ...influencer,
-                manager_first_name: sequence.manager_first_name,
+                manager_first_name: sequence.manager_first_name || '',
                 name: influencerInfo.name || influencerInfo.username,
                 username: influencerInfo.username,
                 avatar_url: influencerInfo.avatar_url,
