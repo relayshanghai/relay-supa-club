dc80f658af710ce005d1dbc6b8772bbe2973ab4b
jacobcoro <xiaokourou@gmail.com>
2023-08-28 13:33:51 +0800

    delete in sequence influencers and keep others. add cascade delete

--- supabase/migrations/20230825070416_add-sequence-delete-flag.sql
+++ supabase/migrations/20230825070416_add-sequence-delete-flag.sql
@@ -1,1 +1,9 @@
 ALTER TABLE "public"."sequences" ADD COLUMN "deleted" boolean NOT NULL DEFAULT false;
+
+ALTER TABLE "public"."sequence_emails"
+DROP CONSTRAINT sequence_emails_sequence_influencer_id_fkey;
+
+ALTER TABLE "public"."sequence_emails"
+ADD CONSTRAINT sequence_emails_sequence_influencer_id_fkey
+FOREIGN KEY (sequence_influencer_id) REFERENCES sequence_influencers (id)
+ON DELETE CASCADE;
