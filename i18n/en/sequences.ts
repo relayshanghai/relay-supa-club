044963ef22034f114c4651baccd425cb8a9593c7
jacobcoro <xiaokourou@gmail.com>
2023-08-28 13:33:10 +0800

    translations, confirmation

--- i18n/en/sequences.ts
+++ i18n/en/sequences.ts
@@ -169,6 +169,9 @@ const sequences = {
         okay: 'Yes, delete them',
     },
     influencerAlreadyAdded_sequence: 'Influencer has already been added to sequence: {{sequence}} ',
+    deleteConfirm: 'Are you sure you want to delete?',
+    deleteSuccess: 'Successfully deleted.',
+    deleteFail: 'Failed to delete.',
 };
 
 export default sequences;
