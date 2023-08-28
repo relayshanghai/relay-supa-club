c2a26d32107aac4c033708634617e0c84fcbf1c9
James <james@relay.club>
2023-08-28 09:19:30 +0000

    Fix email being replaced with null if report is fetched

--- src/utils/api/iqdata/extract-influencer.ts
+++ src/utils/api/iqdata/extract-influencer.ts
@@ -23,7 +23,7 @@ export const mapIqdataProfileToInfluencerSocialProfile = (
     'url' | 'username' | 'platform' | 'reference_id' | 'name' | 'email' | 'avatar_url' | 'recent_video_title'
 > => {
     const contacts = userProfile.contacts || [];
-    const email = contacts.find((v: any) => v.type === 'email') || { value: null };
+    const email = contacts.find((v: any) => v.type === 'email') || { value: undefined };
     return {
         url: userProfile.url,
         username: userProfile.username || userProfile.handle || userProfile.custom_name || '',
