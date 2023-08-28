5a22dea8fcb46ed617ac8805faf67016d9e491cc
TGS963 <ghoshsuvojit2012@gmail.com>
2023-08-28 18:20:57 +0530

    finalized styling

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import type { ReactNode } from 'react';
+import type { MutableRefObject, ReactNode } from 'react';
 import { useEffect } from 'react';
 import useAboveScreenWidth from 'src/hooks/use-above-screen-width';
 import EmailOutline from './icons/EmailOutline';
@@ -9,6 +9,7 @@ import { Compass, FourSquare, Team, Guide, Send, Engagements, ProfilePlus, BarGr
 import { Title } from './title';
 import { useTranslation } from 'react-i18next';
 import { featEmail } from 'src/constants/feature-flags';
+import { Button } from './button';
 
 const links = {
     discover: '/dashboard',
@@ -37,31 +38,31 @@ const ActiveLink = ({ href, children }: { href: string; children: ReactNode }) =
         <Link
             href={href}
             className={`flex items-center overflow-hidden border-l-4 stroke-gray-400 py-2 pl-4 text-sm font-semibold text-gray-400 transition hover:stroke-primary-700 hover:text-primary-700 ${
-                isRouteActive ? 'border-l-4 border-primary-500 stroke-primary-500 text-primary-500' : ''
+                isRouteActive ? 'border-primary-500 stroke-primary-500 text-primary-500' : 'border-transparent'
             }`}
         >
             {(href === links.influencer || href === links.discover) && (
-                <Compass height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />
+                <Compass height={18} width={18} className="my-0.5 stroke-inherit" />
             )}
 
-            {href === links.boostbot && <Thunder height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.boostbot && <Thunder height={18} width={18} className="my-0.5 stroke-inherit" />}
 
-            {href === links.campaigns && <FourSquare height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.campaigns && <FourSquare height={18} width={18} className="my-0.5 stroke-inherit" />}
 
             {href === links.aiEmailGenerator && (
-                <EmailOutline height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />
+                <EmailOutline height={18} width={18} className="my-0.5 stroke-inherit" />
             )}
 
-            {href === links.admin && <Team height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.admin && <Team height={18} width={18} className="my-0.5 stroke-inherit" />}
 
-            {href === links.performance && <BarGraph height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.performance && <BarGraph height={18} width={18} className="my-0.5 stroke-inherit" />}
 
-            {href === links.guide && <Guide height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.guide && <Guide height={18} width={18} className="my-0.5 stroke-inherit" />}
 
-            {href === links.sequences && <Send height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
-            {href === links.inbox && <Engagements height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />}
+            {href === links.sequences && <Send height={18} width={18} className="my-0.5 stroke-inherit" />}
+            {href === links.inbox && <Engagements height={18} width={18} className="my-0.5 stroke-inherit" />}
             {href === links.influencerManager && (
-                <ProfilePlus height={18} width={18} className="my-0.5 mr-4 stroke-inherit" />
+                <ProfilePlus height={18} width={18} className="my-0.5 stroke-inherit" />
             )}
             {children}
         </Link>
@@ -70,12 +71,22 @@ const ActiveLink = ({ href, children }: { href: string; children: ReactNode }) =
 
 // eslint-disable-next-line complexity
 const NavBarInner = ({
+    accountMenuOpen,
+    setAccountMenuOpen,
+    accountMenuButtonRef,
+    accountMenuRef,
+    logout,
     loggedIn,
     profileFirstName,
     isRelayEmployee,
     open,
     desktop,
 }: {
+    accountMenuOpen: boolean;
+    accountMenuButtonRef: MutableRefObject<null>;
+    accountMenuRef: MutableRefObject<null>;
+    setAccountMenuOpen: (menu: boolean) => void;
+    logout: () => void;
     loggedIn: boolean | null;
     profileFirstName?: string;
     isRelayEmployee: boolean;
@@ -93,40 +104,42 @@ const NavBarInner = ({
             <div className="flex h-full flex-col justify-between gap-4 pt-8">
                 <section className="flex flex-col gap-4">
                     <ActiveLink href={links.discover}>
-                        <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.discover')}</p>
+                        <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.discover')}</p>
                     </ActiveLink>
                     <ActiveLink href={links.boostbot}>
-                        <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.boostbot')}</p>
+                        <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.boostbot')}</p>
                     </ActiveLink>
                     {featEmail() && (
                         <ActiveLink href={links.sequences}>
-                            <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.sequences')}</p>
+                            <p className={`whitespace-nowra ml-2 text-sm ${sidebarState}`}>{t('navbar.sequences')}</p>
                         </ActiveLink>
                     )}
                     {featEmail() && (
                         <ActiveLink href={links.inbox}>
-                            <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.inbox')}</p>
+                            <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.inbox')}</p>
                         </ActiveLink>
                     )}
                     {featEmail() && (
                         <ActiveLink href={links.influencerManager}>
-                            <p className={`whitespace-nowrap text-sm ${sidebarState}`}>
+                            <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>
                                 {t('navbar.influencerManager')}
                             </p>
                         </ActiveLink>
                     )}
                     {!featEmail() && (
                         <ActiveLink href={links.campaigns}>
-                            <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.campaigns')}</p>
+                            <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.campaigns')}</p>
                         </ActiveLink>
                     )}
                     {!featEmail() && (
                         <ActiveLink href={links.performance}>
-                            <p className={`whitespace-nowrap text-sm ${sidebarState}`}>{t('navbar.performance')}</p>
+                            <p className={`ml-2 whitespace-nowrap text-sm ${sidebarState}`}>
+                                {t('navbar.performance')}
+                            </p>
                         </ActiveLink>
                     )}
                     <ActiveLink href="/guide">
-                        <p className={`whitespace-nowrap text-sm ${open && desktop ? 'relative' : 'hidden'}`}>
+                        <p className={`ml-2 whitespace-nowrap text-sm ${open && desktop ? 'relative' : 'hidden'}`}>
                             {t('navbar.guide')}
                         </p>
                     </ActiveLink>
@@ -134,7 +147,11 @@ const NavBarInner = ({
                         <div className="flex flex-col space-y-4 pt-8">
                             <h2 className={`${open ? 'ml-6' : 'text-center text-xs'}`}>ADMIN</h2>
                             <ActiveLink href={links.admin}>
-                                <p className={`whitespace-nowrap text-sm ${open && desktop ? 'relative' : 'hidden'}`}>
+                                <p
+                                    className={`ml-2 whitespace-nowrap text-sm ${
+                                        open && desktop ? 'relative' : 'hidden'
+                                    }`}
+                                >
                                     Clients
                                 </p>
                             </ActiveLink>
@@ -142,22 +159,46 @@ const NavBarInner = ({
                     )}
                 </section>
                 {loggedIn && profileFirstName && (
-                    <ActiveLink href="/account">
-                        <div className="-ml-1.5 mb-4 flex flex-row items-center gap-4">
-                            <div className="flex h-9 w-9 items-center justify-center rounded-full border border-gray-200 p-2 text-lg text-gray-800">
-                                {profileFirstName[0]}
-                            </div>
-                            <div className={`flex flex-col whitespace-nowrap text-xs ${sidebarState}`}>
-                                <p className="font-semibold text-gray-800">{profileFirstName}</p>
-                                {featEmail() && (
-                                    <p className="font-light">
-                                        Discover beta!
-                                        <span className="p-1 align-super text-[8px] text-primary-500">Upgrade</span>
-                                    </p>
-                                )}
-                            </div>
+                    <div className="m-4 flex flex-row items-center gap-4">
+                        <div
+                            data-testid="layout-account-menu"
+                            onClick={() => setAccountMenuOpen(!accountMenuOpen)}
+                            ref={accountMenuButtonRef}
+                            className="relative flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-gray-200 p-2 text-base text-gray-800"
+                        >
+                            {profileFirstName[0]}
+                            {accountMenuOpen && (
+                                <div
+                                    className="border-gray absolute bottom-[120%] left-[100%] z-20 flex w-fit origin-top-right flex-col overflow-hidden rounded-md border border-opacity-40 bg-white shadow-lg"
+                                    ref={accountMenuRef}
+                                >
+                                    <Link
+                                        href="/account"
+                                        passHref
+                                        className="whitespace-nowrap px-4 py-2 text-sm hover:bg-gray-100 active:bg-gray-200"
+                                    >
+                                        {t('navbar.account')}
+                                    </Link>
+                                    <Button
+                                        className="px-4 py-2 text-sm hover:bg-gray-100 active:bg-gray-200"
+                                        variant="neutral"
+                                        onClick={logout}
+                                    >
+                                        {t('navbar.logout')}
+                                    </Button>
+                                </div>
+                            )}
                         </div>
-                    </ActiveLink>
+                        <div className={`flex flex-col whitespace-nowrap text-xs ${sidebarState}`}>
+                            <p className="font-semibold text-gray-800">{profileFirstName}</p>
+                            {/*TODO {featEmail() && (
+                                <p className="font-light">
+                                    Discover beta!
+                                    <span className="p-1 align-super text-[8px] text-primary-500">Upgrade</span>
+                                </p>
+                            )} */}
+                        </div>
+                    </div>
                 )}
             </div>
         </>
@@ -165,11 +206,21 @@ const NavBarInner = ({
 };
 
 export const Sidebar = ({
+    accountMenuOpen,
+    accountMenuButtonRef,
+    accountMenuRef,
+    setAccountMenuOpen,
+    logout,
     loggedIn,
     profileFirstName,
     open,
     setOpen,
 }: {
+    accountMenuOpen: boolean;
+    accountMenuButtonRef: MutableRefObject<null>;
+    accountMenuRef: MutableRefObject<null>;
+    setAccountMenuOpen: (menu: boolean) => void;
+    logout: () => void;
     loggedIn: boolean | null;
     profileFirstName?: string;
     open: boolean;
@@ -197,6 +248,11 @@ export const Sidebar = ({
                 } flex h-full transform flex-col border-r border-gray-100 bg-white transition-all`}
             >
                 <NavBarInner
+                    accountMenuOpen={accountMenuOpen}
+                    accountMenuButtonRef={accountMenuButtonRef}
+                    accountMenuRef={accountMenuRef}
+                    setAccountMenuOpen={setAccountMenuOpen}
+                    logout={logout}
                     open={open}
                     profileFirstName={profileFirstName}
                     desktop={desktop}
