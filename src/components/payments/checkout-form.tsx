bf473c092b23a61e68476e9d590ce0eced407312
Ellie Wang <yuetingw@outlook.com>
2023-08-30 13:23:20 +0800

    update the preview url in payment success page with the persistant preview branch url

--- src/components/payments/checkout-form.tsx
+++ src/components/payments/checkout-form.tsx
@@ -55,7 +55,8 @@ export default function CheckoutForm({ selectedPrice }: { selectedPrice: NewRela
                 elements,
                 clientSecret,
                 confirmParams: {
-                    return_url: 'https://relay-supa-club-7u3b027if-relay-club.vercel.app//payments/success', //TODO: remember to update the link when deploy to main
+                    return_url:
+                        'https://relay-supa-club-git-feat-create-trials-withou-2166d6-relay-club.vercel.app/payments/success', //TODO: remember to update the link when deploy to main
                 },
             });
             if (error) {
