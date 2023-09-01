d53bcbc55ecca0866202fd381d317d6b4a63b82f
jacobcoro <xiaokourou@gmail.com>
2023-09-01 12:52:31 +0800

    chinese

--- i18n/en/guide.ts
+++ i18n/en/guide.ts
@@ -46,90 +46,6 @@ const guidePage = {
         },
     },
     modalInfo: {
-        influencerManager: {
-            title: 'Influencer Manager',
-            url: '/influencer-manager',
-            sections: [
-                {
-                    title: 'How to use the Influencer Manager',
-                    description:
-                        'You can view, filter, or search through all the influencers that have responded to your teams outreach emails, and are currently moving through your collab funnel. Pull up the Influencer Profile by clicking on one of the rows, or go to your conversation with them by clicking on their Inbox icon on the right.',
-                },
-                {
-                    title: '',
-                    description:
-                        'Toggle “View Only Mine” to see just the influencers you’re personally responsible for, or keep an eye on the whole team by toggling it off.',
-                },
-            ],
-        },
-        influencerProfile: {
-            title: 'Influencer Profile',
-            url: '/sequence',
-            sections: [
-                {
-                    title: 'How to use the Influencer Profile',
-                    description:
-                        'You can track the Influencer’s status through the collab, helping to keep track of everyone moving through your funnel. Setting and updating the status of your influencers is super important to help you keep on top of things as you start working with more and more influencers. You’ll be able to sort and filter your influencers by their status in the Influencer Manager, and you can set it from right there in the Inbox when you’re responding to their emails!',
-                },
-                {
-                    title: '',
-                    description:
-                        'There are dedicated spots to save all the most important information regarding a collab, from the influencers fee, scheduled post date for the content and even shipping details!',
-                },
-                {
-                    title: '',
-                    description:
-                        'We know there’s going to be things that you need to remember outside of the standard information for a collab. That’s why we added the notes section for you to add longer comments regarding your influencer collab. Once a note is saved, it can be viewed by clicking on the icon beside the Notes title on the profile.',
-                },
-                {
-                    title: '',
-                    description:
-                        'Once your influencer has posted their content, add the URL to the Posts section so we can track the engagements for you to display in the Performance section!',
-                },
-            ],
-        },
-        inbox: {
-            title: 'Inbox',
-            url: '/inbox',
-            sections: [
-                {
-                    title: '',
-                    description:
-                        'The Inbox is where you’ll be able to access all the replies to your outreach emails, and follow-up conversations with influencers. You’ll be able to open that message and reply to the right there on the platform!',
-                },
-                {
-                    title: '',
-                    description:
-                        'With the Inbox connected to the Influencer Profile you’ll have all the context you need, all in one place.',
-                },
-            ],
-        },
-        templates: {
-            title: 'Templates',
-            url: '/sequences',
-            sections: [
-                {
-                    title: 'What are Templates?',
-                    description:
-                        'Templates are pre-built email blueprints that include ‘Variables’ to make sure each email you send is customized for the influencer you’re trying to reach. Each sequence uses 4 different templates, an initial outreach, and 3 follow-up messages that will go out automatically every few days to boost your chances of a reply.',
-                },
-                {
-                    title: '',
-                    description:
-                        'They use information provided by you, as well as information we pull from the influencers report to tailor each emails that goes out, without any of the hassle of copying and pasting yourself',
-                },
-                {
-                    title: 'How to use Templates',
-                    description:
-                        'Before you can start sending out emails in your sequence, you’ll need to set the content that each variable will use, you can do this from the “Update Templates Variables” button in the top right corner of the “Sequence” page.',
-                },
-                {
-                    title: '',
-                    description:
-                        'We’ve pre-built a set of flexible, and highly effective templates for you to use while we work on building the ability to let you make your own templates. These are battle-tested templates we use internally to great success.',
-                },
-            ],
-        },
         sequences: {
             title: 'Sequences',
             url: '/sequences',
@@ -186,6 +102,90 @@ const guidePage = {
                 },
             ],
         },
+        templates: {
+            title: 'Templates',
+            url: '/sequences',
+            sections: [
+                {
+                    title: 'What are Templates?',
+                    description:
+                        'Templates are pre-built email blueprints that include ‘Variables’ to make sure each email you send is customized for the influencer you’re trying to reach. Each sequence uses 4 different templates, an initial outreach, and 3 follow-up messages that will go out automatically every few days to boost your chances of a reply.',
+                },
+                {
+                    title: '',
+                    description:
+                        'They use information provided by you, as well as information we pull from the influencers report to tailor each emails that goes out, without any of the hassle of copying and pasting yourself',
+                },
+                {
+                    title: 'How to use Templates',
+                    description:
+                        'Before you can start sending out emails in your sequence, you’ll need to set the content that each variable will use, you can do this from the “Update Templates Variables” button in the top right corner of the “Sequence” page.',
+                },
+                {
+                    title: '',
+                    description:
+                        'We’ve pre-built a set of flexible, and highly effective templates for you to use while we work on building the ability to let you make your own templates. These are battle-tested templates we use internally to great success.',
+                },
+            ],
+        },
+        inbox: {
+            title: 'Inbox',
+            url: '/inbox',
+            sections: [
+                {
+                    title: '',
+                    description:
+                        'The Inbox is where you’ll be able to access all the replies to your outreach emails, and follow-up conversations with influencers. You’ll be able to open that message and reply to the right there on the platform!',
+                },
+                {
+                    title: '',
+                    description:
+                        'With the Inbox connected to the Influencer Profile you’ll have all the context you need, all in one place.',
+                },
+            ],
+        },
+        influencerProfile: {
+            title: 'Influencer Profile',
+            url: '/influencer-manager',
+            sections: [
+                {
+                    title: 'How to use the Influencer Profile',
+                    description:
+                        'You can track the Influencer’s status through the collab, helping to keep track of everyone moving through your funnel. Setting and updating the status of your influencers is super important to help you keep on top of things as you start working with more and more influencers. You’ll be able to sort and filter your influencers by their status in the Influencer Manager, and you can set it from right there in the Inbox when you’re responding to their emails!',
+                },
+                {
+                    title: '',
+                    description:
+                        'There are dedicated spots to save all the most important information regarding a collab, from the influencers fee, scheduled post date for the content and even shipping details!',
+                },
+                {
+                    title: '',
+                    description:
+                        'We know there’s going to be things that you need to remember outside of the standard information for a collab. That’s why we added the notes section for you to add longer comments regarding your influencer collab. Once a note is saved, it can be viewed by clicking on the icon beside the Notes title on the profile.',
+                },
+                {
+                    title: '',
+                    description:
+                        'Once your influencer has posted their content, add the URL to the Posts section so we can track the engagements for you to display in the Performance section!',
+                },
+            ],
+        },
+        influencerManager: {
+            title: 'Influencer Manager',
+            url: '/influencer-manager',
+            sections: [
+                {
+                    title: 'How to use the Influencer Manager',
+                    description:
+                        'You can view, filter, or search through all the influencers that have responded to your teams outreach emails, and are currently moving through your collab funnel. Pull up the Influencer Profile by clicking on one of the rows, or go to your conversation with them by clicking on their Inbox icon on the right.',
+                },
+                {
+                    title: '',
+                    description:
+                        'Toggle “View Only Mine” to see just the influencers you’re personally responsible for, or keep an eye on the whole team by toggling it off.',
+                },
+            ],
+        },
         discover: {
             title: 'Discover KOL',
             url: '/dashboard',
@@ -219,72 +219,6 @@ const guidePage = {
                     title: 'Contact Info',
                     description:
                         'To find the contact information for an influencer account, we need to scrape their information from their profile. Many influencers choose not to put this information in their profile, and so we aren’t able to collect it. We are working on new ways to collect this information, but for now showing only results with email available will be a much smaller pool of influencers than without.',
-                },
-            ],
-        },
-        outreach: {
-            title: 'Outreach',
-            url: '/sequences',
-            sections: [
-                {
-                    title: 'Sequences',
-                    description: `A Sequence is like a playlist email templates that will be customized, scheduled and automatically sent to an Influencer you add from Discover or BoostBot AI search. So you can think of the Sequence both as the set of scheduled emails, and as the group of influencers you’ve added to it.
-                        
-                        Each sequence consists of 4 email templates, an Outreach message that explains your product and 3 follow-up messages to flag your message to the top of the influencers inbox. Right now all sequences use our internally developed an battle tested templates. You can tailor them to your brand and product by updating the template variables in the Sequence page, and soon you’ll even be able to make your own templates.
-                        
-                        We will track the emails, and once the influencer responds, they’ll be moved to the Influencer Manager, and their response will display in the Inbox.
-                        
-                        We recommend creating a few different sequences, at least one for each genre or niche of influencers you want to collaborate with is ideal. For example if I wanted to promote a smart watch like the Mi Band 8, I would create sequences for ‘Tech Enthusiasts’, ‘Workout Routine Specialists’, and ‘Productivity Hackers’. Then I can adjust my templates and variables for each group so my emails will feel more personalized, and have a better reply rate!
-                        
-                        After your sequence is set up, you’ll be able to set it to either Manual Start, or Auto Start. 
-
-                        When your sequence is set to Manual Start, it will collect all influencers added to it in the ‘Needs Attention’ tab for you, you will need to click the Trigger Sequence button yourself to schedule and send the emails. 
-
-                        If you set your sequence to Auto Start, then any influencer with an email address available that you add to the sequence will automatically have their emails scheduled and sent to them.
-
-                        We recommend using Auto start to make your life easier! It’s better to email lots of potential influencers, and then be more selective about who to work with after they reply.
-
-                        Some influencers choose not to display their contact information in their bio, and so we aren’t able to collect it when we are analyzing their social media profile. On youtube, it is sometimes available in the ‘About’ section of the profile. If you can find the influencers email address, you can add it to their profile in the ‘Needs Attention’ section, then we can schedule and send their sequence emails.                                                 `,
-                },
-                {
-                    title: 'Templates',
-                    description: `Like emails, but magic!
-
-                        Templates are pre-built email blueprints that include ‘Variables’ to make sure each email you send is customized for the influencer you’re trying to reach. Each sequence uses 4 different templates, an initial outreach, and 3 follow-up messages that will go out automatically every few days to boost your chances of a reply. 
-
-                        The templates use information provided by you, as well as information we pull from the influencers report to tailor each emails that goes out, without any of the hassle of copying and pasting yourself
-
-                        Before you can start sending out emails in your sequence, you’ll need to set the content that each variable will use, you can do this from the “Update Templates Variables” button in the top right corner of the “Sequence” page. 
-
-                        We’ve pre-built a set of flexible, and highly effective templates for you to use while we work on building the ability to let you make your own templates. These are battle-tested templates we use internally to great success`,
-                },
-                {
-                    title: 'Inbox',
-                    description: `The Inbox is where you’ll be able to access all the replies to your outreach emails, and follow-up conversations with influencers. You’ll be able to open that message and reply to the right there on the platform!
-
-                        No more switching between your email window, and relay.club. With the Inbox connected to the Influencer Profile you’ll have all the context you need, all in one place.`,
-                },
-                {
-                    title: 'Influencer Profile',
-                    description: `The Influencer Profile is a tool dedicated to organizing and streamlining the collaboration process. 
-
-                        You can track the Influencer’s status through the collab, helping to keep track of everyone moving through your funnel. Setting and updating the status of your influencers is super important to help you keep on top of things as you start working with more and more influencers. You’ll be able to sort and filter your influencers by their status in the Influencer Manager, and you can set it from right there in the Inbox when you’re responding to their emails!
-                        
-                        There are dedicated spots to save all the most important information regarding a collab, from the influencers fee, scheduled post date for the content and even shipping details!
-                        
-                        We know there’s going to be things that you need to remember outside of the standard information for a collab. That’s why we added the notes section for you to add longer comments regarding your influencer collab. Once a note is saved, it can be viewed by clicking on the icon beside the Notes title on the profile. 
-                        
-                        Once your influencer has posted their content, add the URL to the Posts section so we can track the engagements for you to display in the Performance section!`,
-                },
-                {
-                    title: 'Influencer Manager',
-                    description: `The influencer manager is where you can see your entire teams influencers, and get a birds eye view of your in-progress collabs. 
-
-                        You can view, filter, or search through all the influencers that have responded to your teams outreach emails, and are currently moving through your collab funnel. Pull up the Influencer Profile by clicking on one of the rows, or go to your conversation with them by clicking on their Inbox icon on the right.
-
-                        Toggle “View Only Mine” to see just the influencers you’re personally responsible for, or keep an eye on the whole team by toggling it off. 
-
-                        It’s the hub of your influencer marketing efforts, where you can get an overview of your collabs and never let anyone slip through the cracks.`,
                 },
             ],
         },
