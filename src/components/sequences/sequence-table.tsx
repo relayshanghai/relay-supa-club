eb95e44de72d68546538245172be546d36a871f5
TGS963 <ghoshsuvojit2012@gmail.com>
2023-09-08 13:34:39 +0530

    fixed scroll behaviour

--- src/components/sequences/sequence-table.tsx
+++ src/components/sequences/sequence-table.tsx
@@ -59,7 +59,7 @@ const SequenceTable: React.FC<SequenceTableProps> = ({
 
     const columns = sequenceColumns(currentTab);
     return (
-        <div className="max-w-full overflow-visible">
+        <div className="overflow-x-auto">
             <table className="w-full border-collapse border border-gray-300">
                 <thead>
                     <tr className="border-b-2 border-gray-200">
