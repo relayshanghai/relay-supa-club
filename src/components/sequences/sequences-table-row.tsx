08c82c53a2ecf2f6a639ea77406a1eee919c405f
TGS963 <ghoshsuvojit2012@gmail.com>
2023-09-11 16:07:00 +0530

    added view templates button on sequences row

--- src/components/sequences/sequences-table-row.tsx
+++ src/components/sequences/sequences-table-row.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import { useSequenceEmails } from 'src/hooks/use-sequence-emails';
 import { useSequenceInfluencers } from 'src/hooks/use-sequence-influencers';
-import type { Sequence } from 'src/utils/api/db';
+import type { Sequence, SequenceStep } from 'src/utils/api/db';
 import { decimalToPercent } from 'src/utils/formatter';
-import { DeleteOutline } from '../icons';
+import { Brackets, DeleteOutline } from '../icons';
 import { toast } from 'react-hot-toast';
 import { useSequence } from 'src/hooks/use-sequence';
 import { clientLogger } from 'src/utils/logger-client';
@@ -11,12 +11,17 @@ import { useTemplateVariables } from 'src/hooks/use-template_variables';
 import { useTranslation } from 'react-i18next';
 import { DeleteSequenceModal } from '../modal-delete-sequence';
 import { useState } from 'react';
+import { Button } from '../button';
+import { useUser } from 'src/hooks/use-user';
+import { EmailPreviewModal } from './email-preview-modal';
 
 export const SequencesTableRow = ({ sequence }: { sequence: Sequence }) => {
     const { t } = useTranslation();
+    const { sequenceSteps } = useSequence(sequence.id);
+    const { templateVariables } = useTemplateVariables(sequence.id);
+    const { profile } = useUser();
     const { sequenceEmails } = useSequenceEmails(sequence.id);
     const { sequenceInfluencers, refreshSequenceInfluencers } = useSequenceInfluencers([sequence.id]);
-    const { templateVariables } = useTemplateVariables(sequence.id);
     const { deleteSequence } = useSequence();
     const openRate = decimalToPercent(
         (sequenceEmails?.filter(
@@ -35,6 +40,7 @@ export const SequencesTableRow = ({ sequence }: { sequence: Sequence }) => {
         refreshSequenceInfluencers();
     };
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showEmailPreview, setShowEmailPreview] = useState<SequenceStep[] | null>(null);
     return (
         <>
             <DeleteSequenceModal
@@ -42,6 +48,12 @@ export const SequencesTableRow = ({ sequence }: { sequence: Sequence }) => {
                 setShow={setShowDeleteModal}
                 handleDelete={handleDeleteSequence}
                 name={sequence.name}
+            />
+            <EmailPreviewModal
+                visible={!!showEmailPreview}
+                onClose={() => setShowEmailPreview(null)}
+                sequenceSteps={showEmailPreview || []}
+                templateVariables={templateVariables ?? []}
             />
             <tr className="border-b-2 border-gray-200 bg-white">
                 <td className="whitespace-nowrap px-6 py-3 text-primary-600">
@@ -53,7 +65,17 @@ export const SequencesTableRow = ({ sequence }: { sequence: Sequence }) => {
                 <td className="whitespace-nowrap px-6 py-3 text-gray-700">
                     {templateVariables?.find((variable) => variable.key === 'productName')?.value ?? '--'}
                 </td>
-                <td className="whitespace-nowrap px-6 py-3 text-gray-700">
+                <td className="flex items-center gap-2 whitespace-nowrap px-6 py-3 text-gray-700">
+                    <Button
+                        data-testid="show-all-email-previews-button"
+                        className="flex flex-row gap-2"
+                        variant="ghost"
+                        disabled={!profile?.email_engine_account_id || !profile?.sequence_send_email}
+                        onClick={() => setShowEmailPreview(sequenceSteps ?? [])}
+                    >
+                        <Brackets className="h-5 w-5" />
+                        {t('sequences.updateTemplateVariables')}
+                    </Button>
                     <button
                         onClick={() => setShowDeleteModal(true)}
                         className="align-middle"
