08957838df14363484e8079f0675fb1a6e1fcae9
jacobcoro <xiaokourou@gmail.com>
2023-09-20 13:12:05 +0800

    type check tests. fix type errors in tests

--- src/components/influencer/manager/helpers.test.ts
+++ src/components/influencer/manager/helpers.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import type { SequenceInfluencerManagerPage } from '../../../../pages/api/sequence/influencers';
-import type { ProfileDB, Sequence } from '../../../../src/utils/api/db/types.ts';
+import type { Sequence } from '../../../../src/utils/api/db/types';
 import { filterByMe } from './helpers';
+import { mockProfile } from 'src/mocks/test-user';
 describe('filterByMe', () => {
     const mySequenceId = 'my-sequence_id';
     const notMySequenceId = 'not-my-sequence_id';
@@ -28,9 +29,7 @@ describe('filterByMe', () => {
             sequence_id: notMySequenceId,
         },
     ];
-    const mockProfile: Pick<ProfileDB, 'id'> = {
-        id: 'my-profile-id',
-    };
+
     const mockSequences: Pick<Sequence, 'id' | 'manager_id'>[] = [
         {
             manager_id: mockProfile.id,
@@ -42,7 +41,11 @@ describe('filterByMe', () => {
         },
     ];
     it('filters the list of influencers by which ones are managed by the current user', () => {
-        const filteredInfluencers = filterByMe(mockInfluencers, mockProfile, mockSequences);
+        const filteredInfluencers = filterByMe(
+            mockInfluencers as SequenceInfluencerManagerPage[],
+            mockProfile,
+            mockSequences as Sequence[],
+        );
         expect(filteredInfluencers.length).toBe(3);
 
         const filteredInfluencerIds = filteredInfluencers.map((influencer) => influencer.id);
