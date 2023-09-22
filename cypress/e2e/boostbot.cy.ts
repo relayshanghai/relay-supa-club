29bfe4ca522a8b6c2b24c992ccae9dceca0705cb
Sergej Kurbanov <sergejkurbanov@gmail.com>
2023-09-22 18:01:13 +0200

    Filter out unfinished loading progress messages

--- cypress/e2e/boostbot.cy.ts
+++ cypress/e2e/boostbot.cy.ts
@@ -2,6 +2,7 @@ import { deleteDB } from 'idb';
 import { numberFormatter } from 'src/utils/formatter';
 import { boostbotIntercepts, setupIntercepts } from './intercepts';
 import danniCreatorReport from '../../src/mocks/api/creators/report/danni.json';
+import boostbotGetInfluencers from '../../src/mocks/api/boostbot/get-influencers.json';
 
 describe('Boostbot', () => {
     beforeEach(() => {
@@ -31,16 +32,51 @@ describe('Boostbot', () => {
         cy.contains('#facialhair, #bodyhair, #epilator');
     });
 
-    it('can persist chat and influencer result states across reloads', () => {
+    it('can persist chat messages and influencer result states across reloads', () => {
         cy.get('textarea').type('LED beauty mask{enter}');
 
         cy.contains('@DANNIVIVIANI');
 
-        cy.wait(2000); // since IndexedDB is async, wait for it to finish saving state
+        cy.wait(1000); // since IndexedDB is async, wait for it to finish saving state
         cy.reload();
 
         cy.contains('LED beauty mask');
         cy.contains('@DANNIVIVIANI');
+    });
+
+    it('persists finished loading progress messages', () => {
+        cy.get('textarea').type('LED beauty mask{enter}');
+
+        const checkExistingLoadingMessage = () => {
+            cy.contains('Generating topics and niches');
+            cy.contains('Browsing through millions of influencers in our database');
+            cy.contains(`I handpicked the ${boostbotGetInfluencers.length * 3} influencers`);
+        };
+
+        checkExistingLoadingMessage();
+
+        cy.wait(1000); // since IndexedDB is async, wait for it to finish saving state
+        cy.reload();
+
+        checkExistingLoadingMessage();
+    });
+
+    it('does not persist unfinished loading progress messages', () => {
+        cy.intercept('POST', '/api/boostbot/get-influencers', { body: boostbotGetInfluencers, delay: 2000 });
+        cy.intercept('GET', '/api/creators/report*', { body: danniCreatorReport, delay: 2000 });
+
+        cy.get('textarea').type('LED beauty mask{enter}');
+
+        cy.contains('LED beauty mask');
+        cy.contains('Generating topics and niches');
+        cy.contains('Browsing through millions of influencers in our database');
+        cy.contains('Handpicking the best influencers');
+
+        cy.wait(1000); // since IndexedDB is async, wait for it to finish saving state
+        cy.reload();
+
+        cy.contains('LED beauty mask');
+        cy.contains('Generating topics and niches').should('not.exist');
     });
 });
 
