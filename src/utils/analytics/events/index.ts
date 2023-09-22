cb1b5faa07b3bfbf57f7e2d28e24bbcaedce5537
TGS963 <ghoshsuvojit2012@gmail.com>
2023-09-22 21:14:20 +0530

    added play video event

--- src/utils/analytics/events/index.ts
+++ src/utils/analytics/events/index.ts
@@ -156,6 +156,8 @@ import type { GoToInboxPayload } from './outreach/go-to-inbox';
 import { GO_TO_INBOX, GoToInbox } from './outreach/go-to-inbox';
 import type { ToggleViewMinePayload } from './outreach/toggle-view-mine';
 import { TOGGLE_VIEW_MINE, ToggleViewMine } from './outreach/toggle-view-mine';
+import type { PlayTutorialVideoPayload } from './guide/play-tutorial-video';
+import { PLAY_TUTORIAL_VIDEO, PlayTutorialVideo } from './guide/play-tutorial-video';
 
 export {
     Search,
@@ -201,6 +203,7 @@ export {
     AddInfluencerPost,
     AddNoteToInfluencerProfile,
     UpdateInfluencerStatus,
+    PlayTutorialVideo,
 };
 
 export const events = {
@@ -275,6 +278,7 @@ export const events = {
     [FILTER_INFLUENCER_MANAGER]: FilterInfluencerManager,
     [GO_TO_INBOX]: GoToInbox,
     [TOGGLE_VIEW_MINE]: ToggleViewMine,
+    [PLAY_TUTORIAL_VIDEO]: PlayTutorialVideo,
 };
 
 export type payloads = {
@@ -349,6 +353,7 @@ export type payloads = {
     [FILTER_INFLUENCER_MANAGER]: FilterInfluencerManagerPayload;
     [GO_TO_INBOX]: GoToInboxPayload;
     [TOGGLE_VIEW_MINE]: ToggleViewMinePayload;
+    [PLAY_TUTORIAL_VIDEO]: PlayTutorialVideoPayload;
 };
 
 // @note we are using these eventKeys on other zod objects for validation
@@ -425,6 +430,7 @@ export const eventKeys = z.union([
     z.literal(FILTER_INFLUENCER_MANAGER),
     z.literal(GO_TO_INBOX),
     z.literal(TOGGLE_VIEW_MINE),
+    z.literal(PLAY_TUTORIAL_VIDEO),
 ]);
 
 export const isTrackedEvent = (event: (...args: any) => any): event is TrackedEvent => {
