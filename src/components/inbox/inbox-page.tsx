1a192f9adec6e6f15f38bd0d4e9989b742c724b0
TGS963 <ghoshsuvojit2012@gmail.com>
2023-09-22 10:45:47 +0530

    resolved merge conflicts

--- src/components/inbox/inbox-page.tsx
+++ src/components/inbox/inbox-page.tsx
@@ -7,8 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { useMessages } from 'src/hooks/use-message';
 import { useRudderstackTrack } from 'src/hooks/use-rudderstack';
 import { useUser } from 'src/hooks/use-user';
-import { OpenEmailThread, OpenInboxPage } from 'src/utils/analytics/events';
-import { OpenInboxPage, OpenInfluencerProfile } from 'src/utils/analytics/events';
+import { OpenEmailThread, OpenInboxPage, OpenInfluencerProfile } from 'src/utils/analytics/events';
 import { getSequenceInfluencer as baseGetSequenceInfluencer } from 'src/utils/api/db/calls/get-sequence-influencers';
 import { getSequenceInfluencerByEmailAndCompanyCall } from 'src/utils/api/db/calls/sequence-influencers';
 import {
