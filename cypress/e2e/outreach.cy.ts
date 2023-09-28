e6d4f53b6d27c3cfb192d788831982c0b7a10ddd
jacobcoro <xiaokourou@gmail.com>
2023-09-28 13:08:31 +0800

    wrong ticket link

--- cypress/e2e/outreach.cy.ts
+++ cypress/e2e/outreach.cy.ts
@@ -38,7 +38,7 @@ describe('outreach', () => {
         cy.contains('General collaboration', { timeout: 10000 }).click();
 
         // Sequence title row
-        // cy.contains('Auto-start', { timeout: 10000 });  // TODO: reenable when limits are set https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/817
+        // cy.contains('Auto-start', { timeout: 10000 });  // TODO: reenable when reenabling auto-start https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/974
         cy.contains('button', 'View sequence templates');
 
         // stats
@@ -156,7 +156,7 @@ describe('outreach', () => {
         cy.getByTestId('send-email-button-bob.brown@example.com').trigger('mouseout');
         cy.contains('Missing required template variables: **Product Description**').should('not.be.visible');
 
-        // TODO: reenable when limits are set https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/817
+        // TODO: reenable when reenabling auto-start https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/974
         // cy.contains('div', 'Auto-start').within(() => {
         //     cy.get('input[type=checkbox]').trigger('mouseover', { force: true });
         // });
@@ -170,7 +170,7 @@ describe('outreach', () => {
         // cy.contains(
         //     'The values you see here are what will be used to automatically customize the actual email content of your sequence emails!',
         // );
-        // TODO: reenable when limits are set https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/817
+        // TODO: reenable when reenabling auto-start https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/974
         cy.contains('View sequence templates').click(); // and then remove this line
         // can View sequence templates
         cy.get('textarea[id="template-variable-input-productDescription"]').type('test description entry');
