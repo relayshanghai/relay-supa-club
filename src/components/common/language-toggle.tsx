4e85bb34732dd71a17a814bd634cd7fb37ae81fc
James <james@relay.club>
2023-10-05 15:21:11 +0800

    Remove duplicate tracking

--- src/components/common/language-toggle.tsx
+++ src/components/common/language-toggle.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import type { LegacyRef } from 'react';
-import { useRudderstack, useRudderstackTrack } from 'src/hooks/use-rudderstack';
-import { LANGUAGE_TOGGLE } from 'src/utils/rudderstack/event-names';
+import { useRudderstackTrack } from 'src/hooks/use-rudderstack';
 import { LanguageToggleIcon } from '../icons';
 import { useTranslation } from 'react-i18next';
 import { ChangeLanguage } from 'src/utils/analytics/events/change-language';
@@ -21,7 +20,7 @@ export const LanguageToggle = () => {
     };
 
     const languageButtonRef: LegacyRef<HTMLButtonElement> = useRef(null);
-    const { trackEvent } = useRudderstack();
+
     return (
         <div>
             <div className="relative flex flex-col items-center">
@@ -30,12 +29,9 @@ export const LanguageToggle = () => {
                     onClick={() => {
                         if (i18n.language === 'zh-CN') {
                             toggleLanguage('en-US');
-                            trackEvent(LANGUAGE_TOGGLE('switch to en-US'));
                         } else {
                             toggleLanguage('zh-CN');
-                            trackEvent(LANGUAGE_TOGGLE('switch to zh-CN'));
                         }
-                        trackEvent(LANGUAGE_TOGGLE('Clicked'));
                     }}
                     data-testid="language-toggle"
                 >
