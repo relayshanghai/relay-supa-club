eee0cd34dec46b5280b5ec5950a5e2fd6ee08dc1
TGS963 <ghoshsuvojit2012@gmail.com>
2023-10-05 15:14:15 +0530

    changed test order

--- cypress/e2e/outreach.cy.ts
+++ cypress/e2e/outreach.cy.ts
@@ -32,6 +32,41 @@ describe('outreach', () => {
 
         cy.loginTestUser();
     });
+    it('can create new sequences. Can delete sequences', () => {
+        cy.contains('Sequences').click();
+        cy.contains('New sequence', { timeout: 10000 }).click();
+        cy.get('input[placeholder="Enter a name for your sequence"]').type('New Sequence Test');
+        cy.contains('button', 'Create new sequence').click();
+        cy.contains('a', 'New Sequence Test').click({ timeout: 10000 });
+        cy.contains('tr', 'View sequence templates').should('not.exist');
+        cy.contains('button', 'View sequence templates').click({ timeout: 10000 });
+        cy.get('input[id="template-variable-input-productName"]').clear().type('Test Product');
+        cy.contains('button', 'Update variables').click();
+        cy.contains(
+            'The values you see here are what will be used to automatically customize the actual email content of your sequence emails!',
+        ).should('not.exist');
+        cy.contains('Template variables updated');
+        cy.contains('Sequences').click();
+        cy.contains('tr', 'New Sequence Test').contains('Test Product');
+        //  create another dummy sequence to show multi-delete works
+        cy.contains('New sequence').click();
+        cy.get('input[placeholder="Enter a name for your sequence"]').type('New Sequence Test 2');
+        cy.contains('button', 'Create new sequence').click();
+        cy.contains('a', 'New Sequence Test 2');
+        // cleanup and test delete
+        cy.getByTestId('delete-sequences-button').should('not.be.visible');
+        cy.getByTestId('sequences-select-all').should('not.be.checked');
+        cy.getByTestId('sequences-select-all').check();
+        cy.getByTestId('sequence-checkbox').eq(0).should('be.checked');
+        cy.getByTestId('sequence-checkbox').eq(1).should('be.checked');
+        cy.getByTestId('sequence-checkbox').eq(2).should('be.checked');
+        cy.getByTestId('sequence-checkbox').eq(0).uncheck();
+        cy.getByTestId('sequences-select-all').should('not.be.checked');
+        cy.getByTestId('delete-sequences-button').click();
+        cy.contains('button', 'Yes. Delete this sequence').click();
+        cy.contains('tr', 'New Sequence Test').should('not.exist');
+        cy.contains('tr', 'New Sequence Test 2').should('not.exist');
+    });
     it('displays sequence page stats and influencers table', () => {
         cy.contains('Sequences').click();
 
@@ -271,41 +306,6 @@ describe('outreach', () => {
             "One last nudge from me. We'd love to explore the Widget X collab with you. If it's a yes, awesome! If not, no hard feelings.";
         cy.contains(thirdFollowup); // shows all emails not just outreach
         cy.contains('General collaboration').click({ force: true }); // click out of modal
-    });
-    it('can create new sequences. Can delete sequences', () => {
-        cy.contains('Sequences').click();
-        cy.contains('New sequence', { timeout: 10000 }).click();
-        cy.get('input[placeholder="Enter a name for your sequence"]').type('New Sequence Test');
-        cy.contains('button', 'Create new sequence').click();
-        cy.contains('a', 'New Sequence Test').click({ timeout: 10000 });
-        cy.contains('tr', 'View sequence templates').should('not.exist');
-        cy.contains('button', 'View sequence templates').click({ timeout: 10000 });
-        cy.get('input[id="template-variable-input-productName"]').clear().type('Test Product');
-        cy.contains('button', 'Update variables').click();
-        cy.contains(
-            'The values you see here are what will be used to automatically customize the actual email content of your sequence emails!',
-        ).should('not.exist');
-        cy.contains('Template variables updated');
-        cy.contains('Sequences').click();
-        cy.contains('tr', 'New Sequence Test').contains('Test Product');
-        //  create another dummy sequence to show multi-delete works
-        cy.contains('New sequence').click();
-        cy.get('input[placeholder="Enter a name for your sequence"]').type('New Sequence Test 2');
-        cy.contains('button', 'Create new sequence').click();
-        cy.contains('a', 'New Sequence Test 2');
-        // cleanup and test delete
-        cy.getByTestId('delete-sequences-button').should('not.be.visible');
-        cy.getByTestId('sequences-select-all').should('not.be.checked');
-        cy.getByTestId('sequences-select-all').check();
-        cy.getByTestId('sequence-checkbox').eq(0).should('be.checked');
-        cy.getByTestId('sequence-checkbox').eq(1).should('be.checked');
-        cy.getByTestId('sequence-checkbox').eq(2).should('be.checked');
-        cy.getByTestId('sequence-checkbox').eq(0).uncheck();
-        cy.getByTestId('sequences-select-all').should('not.be.checked');
-        cy.getByTestId('delete-sequences-button').click();
-        cy.contains('button', 'Yes. Delete this sequence').click();
-        cy.contains('tr', 'New Sequence Test').should('not.exist');
-        cy.contains('tr', 'New Sequence Test 2').should('not.exist');
     });
 });
 
