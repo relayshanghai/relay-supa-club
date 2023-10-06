f44a510e0f221bea3a3b3f844b404401c0065c5d
TGS963 <ghoshsuvojit2012@gmail.com>
2023-10-06 15:09:38 +0530

    fix error

--- cypress/e2e/expired_user_experience.cy.ts
+++ cypress/e2e/expired_user_experience.cy.ts
@@ -20,11 +20,13 @@ describe('Expired User Experience', () => {
         });
         cy.contains('Your free trial has expired. Please upgrade your account to use this feature.');
         cy.contains('Upgrade subscription');
+    });
     it('r button leads to boostbot', () => {
         cy.loginExpired();
         cy.visit('/account');
         cy.contains('span', 'r').click();
         cy.url().should('include', '/boostbot');
+    });
     it('Can see banners on boostbot and discover', () => {
         cy.loginExpired();
         cy.visit('/boostbot');
