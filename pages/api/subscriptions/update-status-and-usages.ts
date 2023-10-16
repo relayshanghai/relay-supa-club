8c0f92d5bf7eefc783ab125e1fa013d06f1a441a
Ellie Wang <yuetingw@outlook.com>
2023-10-16 16:52:43 +0800

    extract the update subscription status usage logic

--- pages/api/subscriptions/update-status-and-usages.ts
+++ pages/api/subscriptions/update-status-and-usages.ts
@@ -1,10 +1,7 @@
-import { unixEpochToISOString } from 'src/utils/utils';
-import { serverLogger } from 'src/utils/logger-server';
 import { ApiHandler } from 'src/utils/api-handler';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import httpCodes from 'src/constants/httpCodes';
-import { updateCompanySubscriptionStatus, updateCompanyUsageLimits } from 'src/utils/api/db';
-import { stripeClient } from 'src/utils/api/stripe/stripe-client';
+import updateSubscriptionStatusAndUsages from 'src/utils/api/update-subscription-usage-status';
 
 export type UpdateStatusAndUsagesRequestBody = {
     companyId: string;
@@ -15,33 +12,7 @@ export type UpdateStatusAndUsagesRequestBody = {
 const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { companyId, subscriptionId, priceId } = req.body as UpdateStatusAndUsagesRequestBody;
 
-    const subscription = await stripeClient.subscriptions.retrieve(subscriptionId);
-    const price = await stripeClient.prices.retrieve(priceId);
-    const product = await stripeClient.products.retrieve(price.product as string);
-
-    const subscription_start_date = unixEpochToISOString(subscription.start_date);
-    if (!subscription_start_date) throw new Error('Missing subscription start date');
-
-    const { profiles, searches, ai_emails } = product.metadata;
-    if (!profiles || !searches || !ai_emails) {
-        serverLogger('Missing product metadata: ' + JSON.stringify({ product, price }));
-        throw new Error('Missing product metadata');
-    }
-
-    await updateCompanyUsageLimits({
-        profiles_limit: profiles,
-        searches_limit: searches,
-        ai_email_generator_limit: ai_emails,
-        id: companyId,
-    });
-
-    await updateCompanySubscriptionStatus({
-        subscription_status: 'active',
-        subscription_start_date,
-        subscription_current_period_start: unixEpochToISOString(subscription.current_period_start),
-        subscription_current_period_end: unixEpochToISOString(subscription.current_period_end),
-        id: companyId,
-    });
+    updateSubscriptionStatusAndUsages(companyId, subscriptionId, priceId);
 
     return res.status(httpCodes.OK).json('success');
 };
