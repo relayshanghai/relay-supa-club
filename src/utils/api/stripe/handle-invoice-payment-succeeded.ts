7a360570b60d2119ebd27b401567bd745625b161
Ellie Wang <yuetingw@outlook.com>
2023-10-17 17:14:07 +0800

    fix the function name and return res on the new webhook handler

--- src/utils/api/stripe/handle-invoice-payment-succeeded.ts
+++ src/utils/api/stripe/handle-invoice-payment-succeeded.ts
@@ -2,6 +2,7 @@ import type { NextApiResponse } from 'next';
 import type { InvoicePaymentSucceeded } from 'types/stripe/invoice-payment-succeeded-webhook';
 import { getCompanyByCusId } from '../db';
 import updateSubscriptionUsagesAndStatus from '../update-subscription-usage-status';
+import httpCodes from 'src/constants/httpCodes';
 
 export const handleInvoicePaymentSucceeded = async (res: NextApiResponse, invoiceBody: InvoicePaymentSucceeded) => {
     //when receive this webhook, update the company with its companyId, subscriptionId and priceId
@@ -28,4 +29,6 @@ export const handleInvoicePaymentSucceeded = async (res: NextApiResponse, invoic
     }
 
     await updateSubscriptionUsagesAndStatus(companyId, subscriptionId, priceId);
+
+    return res.status(httpCodes.OK).json({ message: 'success' });
 };
