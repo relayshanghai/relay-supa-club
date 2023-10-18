5ba94c848ad9538999153684bdaa19a9dcb0a031
Sergej Kurbanov <sergejkurbanov@gmail.com>
2023-10-18 11:08:57 +0200

    Extract translateGeolocations into helpers and add test

--- src/components/boostbot/message.tsx
+++ src/components/boostbot/message.tsx
@@ -4,6 +4,7 @@ import { VideoPreviewWithModal } from 'src/components/video-preview-with-modal';
 import type { ProgressType } from 'src/components/boostbot/chat-progress';
 import ChatProgress from './chat-progress';
 import { countries } from 'src/utils/api/iqdata/dictionaries/geolocations';
+import { translateGeolocations } from 'src/utils/api/iqdata/dictionaries/helpers';
 import type { AudienceGeo } from 'types/iqdata/influencer-search-request-body';
 
 type TranslationMessage = {
@@ -48,32 +49,18 @@ const Translation = ({
 }: TranslationMessage) => {
     const { t } = useTranslation();
 
-    const translateGeolocations = (geolocations: AudienceGeo[] | undefined) => {
-        // The string type is a fallback for already existing messages in users' indexedDBs from when this feature was not yet implemented.
-        if (typeof geolocations === 'string' || typeof geolocations === 'undefined') return geolocations;
-
-        const and = t('boostbot.chat.and');
-
-        const getTranslatedCountryName = (id: number) => {
-            const countryCode = countries.find((country) => country.id === id)?.country.code;
-            if (!countryCode) return 'Invalid country code';
-            return t(`geolocations.countries.${countryCode}`);
-        };
-        const translatedCountries = geolocations.map((geolocation) => getTranslatedCountryName(geolocation.id));
-
-        if (translatedCountries.length === 2) {
-            return translatedCountries.join(` ${and} `);
-        } else {
-            return translatedCountries.join(', ');
-        }
+    const translatedValues = {
+        ...translationValues,
+        ...(translationValuesToTranslate
+            ? {
+                  geolocations: translateGeolocations({
+                      t,
+                      countries,
+                      geolocations: translationValuesToTranslate.geolocations,
+                  }),
+              }
+            : {}),
     };
-
-    const translatedValues = translationValues
-        ? {
-              ...translationValues,
-              geolocations: translateGeolocations(translationValuesToTranslate?.geolocations),
-          }
-        : undefined;
 
     return (
         <Trans
