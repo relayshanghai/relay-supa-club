434f0d8c42baf96e715cc07baf785fbc0dea31d6
Ellie Wang <yuetingw@outlook.com>
2023-10-18 18:01:05 +0800

    remove new pricing feat flag on pricing page

--- src/components/pricing/pricing-page.tsx
+++ src/components/pricing/pricing-page.tsx
@@ -5,7 +5,6 @@ import { usePrices } from 'src/hooks/use-prices';
 import { useSubscription } from 'src/hooks/use-subscription';
 import type { SubscriptionConfirmModalData } from '../account/subscription-confirm-modal';
 import { SubscriptionConfirmModal } from '../account/subscription-confirm-modal';
-import { Switch } from '../library';
 import { PriceCard } from './price-card';
 import { Button } from '../button';
 import { useRouter } from 'next/router';
@@ -13,7 +12,6 @@ import { screenshots } from 'public/assets/imgs/screenshots';
 import Image from 'next/image';
 import { useRudderstack } from 'src/hooks/use-rudderstack';
 import { LANDING_PAGE } from 'src/utils/rudderstack/event-names';
-import { featNewPricing } from 'src/constants/feature-flags';
 
 const ImageBackground = () => {
     return (
@@ -34,7 +32,7 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
     const { t } = useTranslation();
     const router = useRouter();
     const { trackEvent } = useRudderstack();
-    const [period, setPeriod] = useState<ActiveSubscriptionPeriod>('monthly');
+    const [period] = useState<ActiveSubscriptionPeriod>('monthly');
     const [confirmModalData, setConfirmModalData] = useState<SubscriptionConfirmModalData | null>(null);
     const { createSubscription } = useSubscription();
 
@@ -43,9 +41,8 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
     const openConfirmModal = (priceTier: ActiveSubscriptionTier, period: ActiveSubscriptionPeriod, priceId: string) => {
         setConfirmModalData({ priceTier, period, priceId, price: prices[period][priceTier] });
     };
-    const options: ActiveSubscriptionTier[] = landingPage ? ['free', 'diyMax', 'diy'] : ['diyMax', 'diy'];
 
-    const newOptions: ActiveSubscriptionTier[] = ['discovery', 'outreach'];
+    const options: ActiveSubscriptionTier[] = ['discovery', 'outreach'];
 
     const handleStartFreeTrialClicked = () => {
         trackEvent(LANDING_PAGE('clicked on start free trial'));
@@ -72,32 +69,12 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
                         </h4>
                     </div>
 
-                    <div className="relative">
-                        {!featNewPricing() && (
-                            <>
-                                <Switch
-                                    size={4}
-                                    wrapperClassName="mb-2"
-                                    checked={period === 'quarterly'}
-                                    onChange={(e) => {
-                                        setPeriod(e.target.checked ? 'quarterly' : 'monthly');
-                                        trackEvent(LANDING_PAGE('clicked on switch'), { selectedPeriod: period });
-                                    }}
-                                    beforeLabel={t('pricing.monthly') || 'Monthly'}
-                                    afterLabel={t('pricing.quarterly') || 'Quarterly'}
-                                />
-                                <p className="absolute -right-24 -top-2 mr-2 text-sm font-semibold text-pink-500">
-                                    {t('pricing.saveUpTo33Percent')}
-                                </p>
-                            </>
-                        )}
-                    </div>
                     <div
                         className={`container m-auto flex ${
                             landingPage ? 'min-h-[30rem]' : 'min-h-[32rem]'
                         } w-full max-w-screen-xl flex-wrap justify-center`}
                     >
-                        {(featNewPricing() ? newOptions : options).map((option) => (
+                        {options.map((option) => (
                             <PriceCard
                                 key={option}
                                 period={period}
