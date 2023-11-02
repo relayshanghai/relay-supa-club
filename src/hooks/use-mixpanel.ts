5d4d65e6ae94f2db90b63b94f6770bc4953be821
James <james@relay.club>
2023-11-02 14:02:16 +0800

    Fix logout stuck waiting for mixpanel

--- src/hooks/use-mixpanel.ts
+++ src/hooks/use-mixpanel.ts
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react';
+import { useRudder } from './use-rudderstack';
 
 export const useMixpanel = () => {
+    const rudder = useRudder();
     const [mixpanel, setMixpanel] = useState(() => (typeof window !== 'undefined' ? window.mixpanel : null));
 
     useEffect(() => {
-        if (window.mixpanel) {
-            setMixpanel((s: any) => (s !== window.mixpanel ? window.mixpanel : s));
-        }
-    }, []);
+        const loader = () => {
+            if (window.mixpanel) {
+                setMixpanel((s: any) => (s !== window.mixpanel ? window.mixpanel : s));
+            }
+        };
+        window.addEventListener('load', loader);
+        return () => window.removeEventListener('load', loader);
+    }, [rudder]);
 
     return mixpanel;
 };
