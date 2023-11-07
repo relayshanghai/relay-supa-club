9d9187bfa6815b4e37fbed7527612151c41a9d1c
jacobcoro <xiaokourou@gmail.com>
2023-11-07 15:38:22 +0800

    more icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -92,3 +92,4 @@ export { default as EmailCheck } from './EmailCheck';
 export { default as Drag } from './Drag';
 export { default as ReturnArrowX } from './ReturnArrowX';
 export { default as ChatBubbleText } from './ChatBubbleText';
+export { default as SendX } from './SendX';
