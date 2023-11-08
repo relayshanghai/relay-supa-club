2fcd07451269a156fc692c9c4b2b8e0b11dc0d2e
TGS963 <ghoshsuvojit2012@gmail.com>
2023-11-08 11:42:10 +0530

    remove commented code

--- src/components/boostbot/chat-content.tsx
+++ src/components/boostbot/chat-content.tsx
@@ -33,18 +33,6 @@ export const ChatContent: React.FC<ChatContentProps> = ({ messages, isSearchLoad
             {messages.map((message, index) => (
                 <Message key={index} message={message} />
             ))}
-            {/* {shouldShowButtons && (
-                <div className="z-10 flex flex-wrap gap-2">
-                    <Button
-                        data-testid="boostbot-button-outreach"
-                        onClick={handleSelectedInfluencersToOutreach}
-                        disabled={isOutreachLoading || areChatActionsDisabled || isOutreachButtonDisabled}
-                        className="text-xs"
-                    >
-                        {t('boostbot.chat.outreachSelected')}
-                    </Button>
-                </div>
-            )} */}
             {/* Temporarily disable the stop button until we design a better flow for the chat actions, related ticket: https://toil.kitemaker.co/0JhYl8-relayclub/8sxeDu-v2_project/items/1007 */}
             {isSearchLoading && !temporarilyDisableStopButton && (
                 <div className="flex-grow-1 mt-2 flex flex-1 items-end justify-center">
