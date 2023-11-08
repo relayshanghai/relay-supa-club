dcc92ac2f2218bdc4466d7a103db08d63c7368fc
Ellie Wang <yuetingw@outlook.com>
2023-11-08 21:14:17 +0800

    add prmo code input and submit ui

--- src/components/payments/add-payments-section.tsx
+++ src/components/payments/add-payments-section.tsx
@@ -10,6 +10,8 @@ import Image from 'next/legacy/image';
 import { Alipay, Payment } from '../icons';
 import { useRudderstack } from 'src/hooks/use-rudderstack';
 import { randomNumber } from 'src/utils/utils';
+import { Input } from '../input';
+import { Button } from '../button';
 
 const STRIPE_PUBLIC_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 const stripePromise = loadStripe(STRIPE_PUBLIC_KEY || '');
@@ -47,70 +49,78 @@ export const AddPaymentsSection = ({ priceTier }: { priceTier: newActiveSubscrip
     };
 
     return (
-        <div className="w-80 rounded shadow lg:w-[28rem]">
-            <div className="flex w-full space-x-3 px-6 pt-6 text-xs text-gray-500">
-                <div
-                    className={`${
-                        selectedPaymentMethod === 'card'
-                            ? 'border-2 border-primary-500 fill-primary-500 text-primary-500'
-                            : ''
-                    } group basis-1/2 cursor-pointer rounded-md px-6 py-2 shadow transition hover:border-primary-400 focus:border-primary-400`}
-                    onClick={() => {
-                        setSelectedPaymentMethod('card');
-                        // @note previous name: Payment Page, click on card option
-                        trackEvent('Select Payment Type', { payment_type: 'card' });
-                    }}
-                >
-                    <Payment
-                        className={`${
-                            selectedPaymentMethod === 'card' ? 'fill-primary-500' : 'fill-gray-300'
-                        } h-4 w-4 group-hover:fill-primary-500`}
-                    />
-                    <div className="group-hover:text-primary-500">{t('account.card')}</div>
-                </div>
-                <div
-                    className={`${
-                        selectedPaymentMethod === 'alipay' ? 'border-2 border-primary-500 text-primary-500' : ''
-                    } group basis-1/2 cursor-pointer rounded-md px-6 py-2 shadow transition hover:border-primary-400 focus:border-primary-400`}
-                    onClick={() => {
-                        setSelectedPaymentMethod('alipay');
-                        // @note previous name: Payment Page, click on alipay option
-                        trackEvent('Select Payment Type', { payment_type: 'alipay' });
-                    }}
-                >
-                    <Alipay
-                        className={`${
-                            selectedPaymentMethod === 'alipay' ? 'fill-blue-600' : 'fill-gray-300'
-                        } h-4 w-4 group-hover:fill-blue-600`}
-                    />
-                    <div className="group-hover:text-primary-500">{t('account.alipay')}</div>
-                </div>
+        <div className="w-80 lg:w-[28rem]">
+            <div className="flex items-start space-x-3">
+                <Input label="" type="text" placeholder="Enter a promo code" />
+                <Button variant="ghost" className="m-2 text-xs">
+                    APPLY
+                </Button>
             </div>
-            {selectedPaymentMethod ? (
-                <>
-                    {selectedPaymentMethod === 'card' && (
-                        <StripeElementsProvider stripe={stripePromise} options={cardOptions}>
-                            <CheckoutForm selectedPrice={selectedPrice} batchId={batchId} />
-                        </StripeElementsProvider>
-                    )}
+            <div className="rounded shadow ">
+                <div className="flex w-full space-x-3 px-6 pt-6 text-xs text-gray-500">
+                    <div
+                        className={`${
+                            selectedPaymentMethod === 'card'
+                                ? 'border-2 border-primary-500 fill-primary-500 text-primary-500'
+                                : ''
+                        } group basis-1/2 cursor-pointer rounded-md px-6 py-2 shadow transition hover:border-primary-400 focus:border-primary-400`}
+                        onClick={() => {
+                            setSelectedPaymentMethod('card');
+                            // @note previous name: Payment Page, click on card option
+                            trackEvent('Select Payment Type', { payment_type: 'card' });
+                        }}
+                    >
+                        <Payment
+                            className={`${
+                                selectedPaymentMethod === 'card' ? 'fill-primary-500' : 'fill-gray-300'
+                            } h-4 w-4 group-hover:fill-primary-500`}
+                        />
+                        <div className="group-hover:text-primary-500">{t('account.card')}</div>
+                    </div>
+                    <div
+                        className={`${
+                            selectedPaymentMethod === 'alipay' ? 'border-2 border-primary-500 text-primary-500' : ''
+                        } group basis-1/2 cursor-pointer rounded-md px-6 py-2 shadow transition hover:border-primary-400 focus:border-primary-400`}
+                        onClick={() => {
+                            setSelectedPaymentMethod('alipay');
+                            // @note previous name: Payment Page, click on alipay option
+                            trackEvent('Select Payment Type', { payment_type: 'alipay' });
+                        }}
+                    >
+                        <Alipay
+                            className={`${
+                                selectedPaymentMethod === 'alipay' ? 'fill-blue-600' : 'fill-gray-300'
+                            } h-4 w-4 group-hover:fill-blue-600`}
+                        />
+                        <div className="group-hover:text-primary-500">{t('account.alipay')}</div>
+                    </div>
+                </div>
+                {selectedPaymentMethod ? (
+                    <>
+                        {selectedPaymentMethod === 'card' && (
+                            <StripeElementsProvider stripe={stripePromise} options={cardOptions}>
+                                <CheckoutForm selectedPrice={selectedPrice} batchId={batchId} />
+                            </StripeElementsProvider>
+                        )}
 
-                    {selectedPaymentMethod === 'alipay' && (
-                        <div className="mb-2 p-6">
-                            <p className="p-6 text-xs text-gray-500">{t('account.contactUs')}</p>
+                        {selectedPaymentMethod === 'alipay' && (
+                            <div className="mb-2 p-6">
+                                <p className="p-6 text-xs text-gray-500">{t('account.contactUs')}</p>
 
-                            <Image
-                                src="/assets/imgs/qrcodes/relayclub-clubby.png"
-                                alt="qr code to contact customer service"
-                                layout="responsive"
-                                width={1000}
-                                height={1000}
-                            />
-                        </div>
-                    )}
-                </>
-            ) : (
-                <p className="p-6 text-xs text-gray-500">{t('account.choosePaymentMethod')}</p>
-            )}
+                                <Image
+                                    src="/assets/imgs/qrcodes/relayclub-clubby.png"
+                                    alt="qr code to contact customer service"
+                                    layout="responsive"
+                                    width={1000}
+                                    height={1000}
+                                />
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <p className="p-6 text-xs text-gray-500">{t('account.choosePaymentMethod')}</p>
+                )}
+            </div>
         </div>
     );
 };
