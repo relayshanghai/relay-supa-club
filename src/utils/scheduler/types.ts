f5a6936a581e2e90cad0885309dca213abad9765
James <james@relay.club>
2023-11-09 16:09:24 +0800

    Change SCHEDULER_TOKEN_KEY value

--- src/utils/scheduler/types.ts
+++ src/utils/scheduler/types.ts
@@ -1,7 +1,7 @@
 import type { JobNames } from './jobs';
 
 export const SCHEDULER_TOKEN_HEADER = 'x-scheduler-token';
-export const SCHEDULER_TOKEN_KEY = 'SCHEDULER_WORKER_KEY';
+export const SCHEDULER_TOKEN_KEY = 'SCHEDULER_WORKER_TOKEN';
 
 export enum JOB_QUEUE {
     default = 'default',
