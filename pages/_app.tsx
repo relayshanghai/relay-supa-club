0f0240ee0a99f2311880a5cf7e0f641f6ea98110
jacobcoro <xiaokourou@gmail.com>
2023-11-09 15:32:20 +0800

    set localstorage too

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,8 +45,10 @@ function MyApp({
             if (typeof setLang === 'string') {
                 if (setLang.includes('en')) {
                     i18n.changeLanguage('en-US');
+                    localStorage.setItem('language', 'en-US');
                 } else if (setLang.includes('zh')) {
                     i18n.changeLanguage('zh-CN');
+                    localStorage.setItem('language', 'zh-CN');
                 }
             }
         }
