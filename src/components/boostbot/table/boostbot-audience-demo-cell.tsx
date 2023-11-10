086c5abb36518b2143dcdde58ea8a722d7c23738
TGS963 <ghoshsuvojit2012@gmail.com>
2023-11-10 09:59:39 +0530

    alter table to fit design

--- src/components/boostbot/table/boostbot-audience-demo-cell.tsx
+++ src/components/boostbot/table/boostbot-audience-demo-cell.tsx
@@ -20,7 +20,7 @@ export const BoostbotAudienceDemoCell = ({ row }: BoostbotAudienceDemoCellProps)
                 }}
             >
                 <CartesianGrid vertical={false} horizontal={false} />
-                <XAxis dataKey="category" axisLine={false} tickLine={false} tick={{ fontSize: 8 }} />
+                <XAxis dataKey="category" axisLine={false} tickLine={false} tick={{ fontSize: 8, fill: '#4b5563' }} />
                 <YAxis width={0} tick={false} axisLine={false} tickLine={false} />
                 <Bar dataKey="female" fill="#fcceee" radius={2} />
                 <Bar dataKey="male" fill="#b2ccff" radius={2} />
