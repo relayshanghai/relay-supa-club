5c469fa59b91125ed3cbb6a7e83fa4b4f56c2574
James <james@relay.club>
2023-11-10 12:17:18 +0800

    Use runJob in POST /api/jobs/run endpoint

--- pages/api/jobs/run.ts
+++ pages/api/jobs/run.ts
@@ -1,34 +1,19 @@
+import httpCodes from 'src/constants/httpCodes';
 import type { ActionHandler } from 'src/utils/api-handler';
 import { ApiHandler } from 'src/utils/api-handler';
-import type { Jobs } from 'src/utils/api/db';
 import { checkToken } from 'src/utils/check-token';
-import { serverLogger } from 'src/utils/logger-server';
-import { finishJob, getJobs } from 'src/utils/scheduler/db';
+import { getJobs } from 'src/utils/scheduler/db';
 import { runJob } from 'src/utils/scheduler/jobs/index';
 import type { RunJobRequest } from 'src/utils/scheduler/types';
-import { SCHEDULER_TOKEN_HEADER, SCHEDULER_TOKEN_KEY } from 'src/utils/scheduler/types';
-import { JOB_QUEUE } from 'src/utils/scheduler/types';
-import { JOB_STATUS } from 'src/utils/scheduler/types';
+import { JOB_QUEUE, JOB_STATUS, SCHEDULER_TOKEN_HEADER, SCHEDULER_TOKEN_KEY } from 'src/utils/scheduler/types';
 import { db } from 'src/utils/supabase-client';
-
-const handler = async (job: Jobs['Row']) => {
-    try {
-        const result = await runJob(job.name, job.payload);
-        db(finishJob)(job, JOB_STATUS.success, result);
-        return { job: job.id, result: true };
-    } catch (e) {
-        serverLogger(e);
-        db(finishJob)(job, JOB_STATUS.failed, { error: new Error(String(e)).message });
-        return { job: job.id, result: false };
-    }
-};
 
 const postHandler: ActionHandler = async (req, res) => {
     const query = req.query as RunJobRequest['query'];
     const token = String(req.headers[SCHEDULER_TOKEN_HEADER] ?? '');
 
     if (!checkToken(token, SCHEDULER_TOKEN_KEY)) {
-        return res.status(401).end();
+        return res.status(httpCodes.UNAUTHORIZED).end();
     }
 
     const { queue, status, limit } = query ?? {};
@@ -39,13 +24,13 @@ const postHandler: ActionHandler = async (req, res) => {
         limit: limit ?? 1,
     });
 
-    const runningJobs = jobs.map((job) => handler(job));
+    const runningJobs = jobs.map((job) => runJob(job));
     const finishedJobs = await Promise.allSettled(runningJobs);
     const results: { job: string; result: boolean }[] = finishedJobs.map((result) =>
         result.status === 'fulfilled' ? result.value : result.reason,
     );
 
-    return res.status(200).json(results);
+    return res.status(httpCodes.OK).json(results);
 };
 
 export default ApiHandler({
