688baa6f2d33fe81aa4f2e507f02e41806de65b2
Ellie Wang <yuetingw@outlook.com>
2023-11-14 18:05:52 +0800

    clean up activeSubscriptionTier type

--- src/components/payments/add-payments-section.tsx
+++ src/components/payments/add-payments-section.tsx
@@ -2,9 +2,8 @@ import { loadStripe } from '@stripe/stripe-js';
 import type { StripeElementsOptions } from '@stripe/stripe-js';
 import { Elements as StripeElementsProvider } from '@stripe/react-stripe-js';
 import CheckoutForm from './checkout-form';
-import { usePrices } from 'src/hooks/use-prices';
+import { type ActiveSubscriptionTier, usePrices } from 'src/hooks/use-prices';
 import { useTranslation } from 'react-i18next';
-import type { newActiveSubscriptionTier } from 'types';
 import { useMemo, useState } from 'react';
 import Image from 'next/legacy/image';
 import { Alipay, Payment } from '../icons';
@@ -23,7 +22,7 @@ export type CreatePaymentIntentResponse = {
     clientSecret?: string;
 };
 
-export const AddPaymentsSection = ({ priceTier }: { priceTier: newActiveSubscriptionTier }) => {
+export const AddPaymentsSection = ({ priceTier }: { priceTier: ActiveSubscriptionTier }) => {
     const { i18n, t } = useTranslation();
     const prices = usePrices();
     const { trackEvent } = useRudderstack();
