2ab5ae4bfef5b222ec7c307eb457e4c2ec105115
Ellie Wang <yuetingw@outlook.com>
2023-11-14 16:48:25 +0800

    get rid of unused modal code

--- src/components/pricing/pricing-page.tsx
+++ src/components/pricing/pricing-page.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { ActiveSubscriptionPeriod, ActiveSubscriptionTier } from 'src/hooks/use-prices';
-import { usePrices } from 'src/hooks/use-prices';
-import { useSubscription } from 'src/hooks/use-subscription';
-import type { SubscriptionConfirmModalData } from '../account/subscription-confirm-modal';
-import { SubscriptionConfirmModal } from '../account/subscription-confirm-modal';
 import { PriceCard } from './price-card';
 import { Button } from '../button';
 import { useRouter } from 'next/router';
@@ -34,14 +30,6 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
     const router = useRouter();
     const { trackEvent } = useRudderstack();
     const [period] = useState<ActiveSubscriptionPeriod>('monthly');
-    const [confirmModalData, setConfirmModalData] = useState<SubscriptionConfirmModalData | null>(null);
-    const { createSubscription } = useSubscription();
-
-    const prices = usePrices();
-
-    const openConfirmModal = (priceTier: ActiveSubscriptionTier, period: ActiveSubscriptionPeriod, priceId: string) => {
-        setConfirmModalData({ priceTier, period, priceId, price: prices[period][priceTier] });
-    };
 
     const options: ActiveSubscriptionTier[] = ['discovery', 'outreach'];
 
@@ -59,12 +47,6 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
                 </Link>
             )}
             <main className={`flex flex-grow flex-col`}>
-                <SubscriptionConfirmModal
-                    confirmModalData={confirmModalData}
-                    setConfirmModalData={setConfirmModalData}
-                    createSubscription={createSubscription}
-                />
-
                 <div className="container mx-auto flex flex-col items-center">
                     <div className="mx-auto mb-8 max-w-3xl text-center">
                         <h2 className="font-heading mb-6 text-3xl font-semibold text-gray-800 md:text-4xl">
@@ -81,13 +63,7 @@ export const PricingPage = ({ page = 'upgrade' }: { page?: 'upgrade' | 'landing'
                         } w-full max-w-screen-xl flex-wrap justify-center`}
                     >
                         {options.map((option) => (
-                            <PriceCard
-                                key={option}
-                                period={period}
-                                priceTier={option}
-                                openConfirmModal={openConfirmModal}
-                                landingPage={landingPage}
-                            />
+                            <PriceCard key={option} period={period} priceTier={option} landingPage={landingPage} />
                         ))}
                     </div>
 
