29d3347ddc9f8de9fba6e609f6a394556bb6e92c
TGS963 <ghoshsuvojit2012@gmail.com>
2023-11-15 15:36:38 +0530

    fix styling

--- src/components/library/table-inline-input.tsx
+++ src/components/library/table-inline-input.tsx
@@ -31,17 +31,19 @@ const TableInlineInputWithRef = (
     };
     return (
         <button
-            className="relative flex w-full cursor-pointer justify-between rounded border border-gray-300 px-4 py-2 text-xs text-gray-900 hover:text-primary-500"
+            className={`relative flex w-full cursor-pointer items-center justify-between rounded ${
+                editing ? 'border-none ' : 'border border-gray-300 px-4 py-2'
+            }   text-xs text-gray-900 hover:text-primary-500`}
             onClick={() => setEditing(true)}
         >
             {editing || submitting ? (
                 <div
                     ref={ref}
                     onClick={(e) => e.stopPropagation()}
-                    className="group absolute -left-4 top-1/2 z-[10] h-14 w-fit min-w-[200px] max-w-[360px] -translate-y-1/2 p-2 will-change-transform"
+                    className="group z-[10] flex h-full w-full flex-row items-center"
                 >
                     <form
-                        className="flex h-full min-h-0 items-center"
+                        className="flex h-full w-full items-center"
                         onSubmit={(e) => {
                             e.preventDefault();
                             setEditing(false);
@@ -54,13 +56,13 @@ const TableInlineInputWithRef = (
                             type={type}
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
-                            className="mr-2 h-full w-full resize-none rounded-md border border-gray-200 p-2 text-xs text-gray-600 outline-none"
+                            className="rounded border border-gray-300 py-[7px] pl-4 text-xs text-gray-600 outline-none"
                         />
-                        <div className="flex items-center justify-end">
+                        <div className="flex justify-end">
                             <button
                                 disabled={submitting}
                                 type="submit"
-                                className="column-center mr-2 h-8 w-8 cursor-pointer rounded-md bg-primary-500 duration-300 hover:bg-primary-700"
+                                className="column-center h-8 w-8 cursor-pointer rounded-md bg-primary-500 duration-300 hover:bg-primary-700"
                             >
                                 <Confirm className="h-4 w-4 rounded-md fill-current text-white" />
                             </button>
@@ -85,7 +87,7 @@ const TableInlineInputWithRef = (
                     )}
                 </div>
             )}
-            <Edit className="ml-2 h-4 w-4 text-gray-300" />
+            {!editing && <Edit className="ml-2 h-4 w-4 text-gray-300" />}
         </button>
     );
 };
