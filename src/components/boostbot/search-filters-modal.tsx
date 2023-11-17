e5bd5f82635b88dce30436d7e8f6090d6713ab49
France Tolentino <franiche17@gmail.com>
2023-11-17 14:00:18 +0800

    changes in condition for the input

--- src/components/boostbot/search-filters-modal.tsx
+++ src/components/boostbot/search-filters-modal.tsx
@@ -320,6 +320,7 @@ export const SearchFiltersModal = ({ isOpen, setIsOpen, filters, setFilters }: S
                         </div>
 
                         <div className="flex w-full flex-row rounded-md border bg-white px-3.5 py-2 text-xs ring-2 ring-gray-200 hover:ring-primary-300 focus:outline-none focus:ring-2 focus:ring-gray-400">
+                            {/*                             
                             <div className="flex flex-row justify-center gap-1">
                                 {localFilters['audience_geo'].map((geo) => (
                                     <button
@@ -333,12 +334,32 @@ export const SearchFiltersModal = ({ isOpen, setIsOpen, filters, setFilters }: S
                                         <XMarkIcon className="h-3 w-3" />
                                     </button>
                                 ))}
+                            </div> */}
+                            {/* {shouldShowGeoInput && ( */}
+                            <div className="h-5 w-full" data-testid="boostbot-geo-container">
+                                <InputWithSuggestions
+                                    suggestions={geoSuggestions}
+                                    onSelect={addNewGeo}
+                                    disabled={shouldShowGeoInput}
+                                    content={
+                                        <div className="flex flex-row justify-center gap-1">
+                                            {localFilters['audience_geo'].map((geo) => (
+                                                <button
+                                                    key={geo.id}
+                                                    data-testid={`boostbot-filter-geo-${geo.id}`}
+                                                    className="bg-white-50 flex items-center gap-1 rounded-md border border-tertiary-300 px-2 text-tertiary-500 shadow-md transition-all hover:border-primary-300 hover:text-primary-300"
+                                                    onClick={() => removeGeo(geo.id)}
+                                                >
+                                                    <p>{getFlagCountry(geo.id)[0].emoji}</p>
+                                                    {getTranslatedCountryName(geo.id)}
+                                                    <XMarkIcon className="h-3 w-3" />
+                                                </button>
+                                            ))}
+                                        </div>
+                                    }
+                                />
                             </div>
-                            {shouldShowGeoInput && (
-                                <div className="h-full grow" data-testid="boostbot-geo-container">
-                                    <InputWithSuggestions suggestions={geoSuggestions} onSelect={addNewGeo} />
-                                </div>
-                            )}
+                            {/* )} */}
                         </div>
                         <p className="font-regular mt-1 text-xs text-tertiary-400">
                             {' '}
--- src/components/library/input-with-suggestions.tsx
+++ src/components/library/input-with-suggestions.tsx
@@ -9,9 +9,11 @@ type Suggestion = {
 type Props = {
     suggestions: Suggestion[];
     onSelect: (value: string) => void;
+    content?: any;
+    disabled?: boolean;
 };
 
-export const InputWithSuggestions = ({ suggestions, onSelect }: Props) => {
+export const InputWithSuggestions = ({ suggestions, onSelect, content, disabled }: Props) => {
     const inputWithSuggestionsRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
     const [value, setValue] = useState('');
@@ -29,15 +31,18 @@ export const InputWithSuggestions = ({ suggestions, onSelect }: Props) => {
 
     return (
         <div className="relative flex w-full" ref={inputWithSuggestionsRef}>
-            <input
-                className="w-full rounded-md text-xs focus:outline-none"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                onFocus={() => setIsOpen(true)}
-            />
+            {content}
+            {disabled && (
+                <input
+                    className="w-full rounded-md text-xs focus:outline-none"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    onFocus={() => setIsOpen(true)}
+                />
+            )}
 
             {isOpen && (
-                <ul className="absolute z-20 mt-2 max-h-32 w-full overflow-y-auto rounded-lg border border-gray-200 bg-white text-gray-700 shadow-md">
+                <ul className="absolute z-20 mt-2 mt-4 max-h-32 w-full overflow-y-auto rounded-lg border border-gray-200 bg-white text-gray-700 shadow-md">
                     {filteredSuggestions.map((suggestion) => (
                         <li key={suggestion.value}>
                             <button
