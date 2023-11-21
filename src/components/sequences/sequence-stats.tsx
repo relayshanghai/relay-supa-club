2ec7f9631eadddfee92e9ff7775c096b85a44928
France Tolentino <franiche17@gmail.com>
2023-11-22 02:16:17 +0800

    adjust icones

--- src/components/sequences/sequence-stats.tsx
+++ src/components/sequences/sequence-stats.tsx
@@ -23,14 +23,14 @@ export interface SequenceStatsProps {
 const getStatusDiv = (status: string): ReactElement => {
     if (status === 'alert') {
         return (
-            <div className="flex h-4 w-5 items-center justify-center bg-red-50 p-1 text-red-500">
-                <AlertCircleOutline className="h-3 w-3" />
+            <div className="flex h-4 w-5 items-center justify-center rounded bg-red-50 p-1 text-red-500">
+                <AlertCircleOutline className="h-3 w-3" strokeWidth="1.5" />
             </div>
         );
     } else if (status === 'check') {
         return (
-            <div className="flex h-4 w-5 items-center justify-center bg-green-50 p-1 text-green-500">
-                <CheckCircleOutline className="h-3 w-3" />
+            <div className="flex h-4 w-5 items-center justify-center rounded bg-green-50 p-1 text-green-500">
+                <CheckCircleOutline className="h-3 w-3" strokeWidth="1.5" />
             </div>
         );
     }
