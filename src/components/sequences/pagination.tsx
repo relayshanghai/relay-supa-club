5fc99c5e0b42a031dc396cf2620c361b3ca0f85a
France Tolentino <franiche17@gmail.com>
2023-11-23 15:43:41 +0800

    removed tracking and translations

--- src/components/sequences/pagination.tsx
+++ src/components/sequences/pagination.tsx
@@ -1,34 +1,31 @@
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
-import type { Table } from '@tanstack/react-table';
-import { useTranslation } from 'react-i18next';
-import { Button } from 'src/components/button';
-import { useRudderstackTrack } from 'src/hooks/use-rudderstack';
-import { ChangePage } from 'src/utils/analytics/events';
-import { CurrentPageEvent } from 'src/utils/analytics/events/current-pages';
+// import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
+// import { useTranslation } from 'react-i18next';
+// import { Button } from 'src/components/button';
+// import { useRudderstackTrack } from 'src/hooks/use-rudderstack';
+// import { ChangePage } from 'src/utils/analytics/events';
+// import { CurrentPageEvent } from 'src/utils/analytics/events/current-pages';
 import PageLink from './pagelink';
 import { getPaginationItems } from '../boostbot/table/helper';
 
-
-interface DataTablePaginationProps<TData> {
+interface DataTablePaginationProps {
     // table: Table<TData>;
-    pages:number;
-    currentPage:number;
-    setPageIndex:any;
-
+    pages: number;
+    currentPage: number;
+    setPageIndex: any;
 }
 
-export function DataTablePagination<TData>({ pages,currentPage,setPageIndex }: DataTablePaginationProps<TData>) {
-    const { t } = useTranslation();
-    const { track } = useRudderstackTrack();
+export function DataTablePagination({ pages, currentPage, setPageIndex }: DataTablePaginationProps) {
+    // const { t } = useTranslation();
+    // const { track } = useRudderstackTrack();
 
     //adjust to control the number of links in the pagination section
-    const noOfLinks = 11;
-    const pageNums = getPaginationItems(currentPage+ 1, pages, noOfLinks);
+    const noOfLinks = pages;
+    const pageNums = getPaginationItems(currentPage + 1, pages, noOfLinks);
 
     return (
         <div className="flex w-full justify-between px-0">
-            {/* <div>
-                <Button
+            {/* {/* <div> */}
+            {/* <Button
                     variant="neutral"
                     className="flex h-8 w-32 items-center justify-center !py-0 px-2 text-sm font-medium text-primary-500 disabled:text-gray-400"
                     onClick={() => {
@@ -48,6 +45,7 @@ export function DataTablePagination<TData>({ pages,currentPage,setPageIndex }: D
                     {t('manager.previous')}
                 </Button>
             </div> */}
+
             <div className="flex w-[300px] flex-row ">
                 {pageNums.map((pageNum, idx) => (
                     <PageLink
@@ -55,7 +53,7 @@ export function DataTablePagination<TData>({ pages,currentPage,setPageIndex }: D
                         key={idx}
                         active={currentPage === pageNum}
                         disabled={isNaN(pageNum)}
-                        onClick={() =>setPageIndex(pageNum)}
+                        onClick={() => setPageIndex(pageNum)}
                     >
                         {!isNaN(pageNum) ? pageNum : '...'}
                     </PageLink>
--- src/components/sequences/sequence-table.tsx
+++ src/components/sequences/sequence-table.tsx
@@ -5,7 +5,7 @@ import { sequenceColumns } from './constants';
 import { type SetStateAction, useCallback, useState } from 'react';
 import type { SequenceSendPostResponse } from 'pages/api/sequence/send';
 import type { SequenceInfluencerManagerPage } from 'pages/api/sequence/influencers';
-import {DataTablePagination as Pagination} from './pagination';
+import { DataTablePagination as Pagination } from './pagination';
 
 interface SequenceTableProps {
     sequence?: Sequence;
@@ -66,25 +66,38 @@ const SequenceTable: React.FC<SequenceTableProps> = ({
 }) => {
     const sortedInfluencers = sortInfluencers(currentTab, sequenceInfluencers, sequenceEmails);
     const { t } = useTranslation();
-    const [currentPage,setCurrentPage]=useState(1);
-    const totalNumberOfPages= ()=>{
-        if (sortedInfluencers?.length){
-            console.log(sortInfluencers.length)
-            return (sortedInfluencers.length/numberOfInfluencersPerPage); // change to 25 later
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const totalNumberOfPages = () => {
+        //gets the number of pages
+        if (sortedInfluencers?.length) {
+            return Math.floor(sortedInfluencers.length / numberOfInfluencersPerPage);
         }
         return 1;
     };
-    const numberOfInfluencersPerPage=6;
-    const getRangePerPage=()=>{}
+    const numberOfInfluencersPerPage = 5; // change to 25 later or number of influencers needed per page
+
+    const getRangePerPage = () => {
+        //calculates the range splice in the results
+        const lastPage = totalNumberOfPages();
+        const startRange = (currentPage - 1) * numberOfInfluencersPerPage;
+        let endRange: any = currentPage * numberOfInfluencersPerPage;
+        const totalNoOfInfluencers: any = sortedInfluencers?.length;
+
+        if (totalNoOfInfluencers > endRange && currentPage == lastPage) {
+            endRange = sortedInfluencers?.length;
+        }
+        return { end: endRange, start: startRange };
+    };
 
     const handleCheckboxChange = useCallback(
         function (id: string) {
-        if (selection.includes(id)) {
-            setSelection(selection.filter((selectedId) => selectedId !== id));
-            return;
-        }
-        setSelection([...selection, id]);
-    },
+            if (selection.includes(id)) {
+                setSelection(selection.filter((selectedId) => selectedId !== id));
+                return;
+            }
+            setSelection([...selection, id]);
+        },
         [selection, setSelection],
     );
 
@@ -99,63 +112,63 @@ const SequenceTable: React.FC<SequenceTableProps> = ({
     const columns = sequenceColumns(currentTab);
     return (
         <div>
-        <table className="w-full border-collapse border border-gray-300">
-            <thead>
-                <tr className="border-b-2 border-gray-200">
-                    <th className="bg-white px-4">
-                        <input
-                            data-testid="sequence-influencers-select-all"
-                            className="display-none appearance-none rounded border-gray-300 checked:text-primary-500 focus:ring-2 focus:ring-primary-500"
-                            type="checkbox"
-                            checked={sequenceInfluencers.length === selection.length && selection.length > 0}
-                            onChange={handleCheckAll}
-                        />
-                    </th>
-                    {columns.map((column) => (
-                        <th
-                            key={column}
-                            className="whitespace-nowrap bg-white px-6 py-3 text-left text-xs font-normal tracking-wider text-gray-500"
-                        >
-                            {t(`sequences.columns.${column}`)}
+            <table className="w-full border-collapse border border-gray-300">
+                <thead>
+                    <tr className="border-b-2 border-gray-200">
+                        <th className="bg-white px-4">
+                            <input
+                                data-testid="sequence-influencers-select-all"
+                                className="display-none appearance-none rounded border-gray-300 checked:text-primary-500 focus:ring-2 focus:ring-primary-500"
+                                type="checkbox"
+                                checked={sequenceInfluencers.length === selection.length && selection.length > 0}
+                                onChange={handleCheckAll}
+                            />
                         </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {sortedInfluencers?.slice((currentPage-1)*numberOfInfluencersPerPage, currentPage*numberOfInfluencersPerPage)?.map((influencer) => {
-                    const influencerEmails = sequenceEmails?.filter(
-                        (email) => email.sequence_influencer_id === influencer.id,
-                    );
-                    const lastStep = sequenceSteps.find((step) => step.step_number === influencer.sequence_step);
-                    const nextStep = sequenceSteps.find((step) => step.step_number === influencer.sequence_step + 1);
-                    const lastEmail = influencerEmails?.find((email) => email.sequence_step_id === lastStep?.id);
-                    return (
-                        <SequenceRow
-                            key={influencer.id}
-                            sequence={sequence}
-                            sequenceInfluencer={influencer}
-                            loadingEmails={loadingEmails}
-                            lastEmail={lastEmail}
-                            lastStep={lastStep}
-                            nextStep={nextStep}
-                            sequenceSteps={sequenceSteps}
-                            currentTab={currentTab}
-                            isMissingVariables={isMissingVariables}
-                            missingVariables={missingVariables}
-                            setShowUpdateTemplateVariables={setShowUpdateTemplateVariables}
-                            templateVariables={templateVariables}
-                            handleStartSequence={handleStartSequence}
-                            checked={selection.includes(influencer.id)}
-                            onCheckboxChange={handleCheckboxChange}
-                        />
-                    );
-                })}
-            </tbody>
-        </table>
-        <Pagination setPageIndex={setCurrentPage} currentPage={currentPage} pages={totalNumberOfPages()}/>
+                        {columns.map((column) => (
+                            <th
+                                key={column}
+                                className="whitespace-nowrap bg-white px-6 py-3 text-left text-xs font-normal tracking-wider text-gray-500"
+                            >
+                                {t(`sequences.columns.${column}`)}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {sortedInfluencers?.slice(getRangePerPage().start, getRangePerPage().end)?.map((influencer) => {
+                        const influencerEmails = sequenceEmails?.filter(
+                            (email) => email.sequence_influencer_id === influencer.id,
+                        );
+                        const lastStep = sequenceSteps.find((step) => step.step_number === influencer.sequence_step);
+                        const nextStep = sequenceSteps.find(
+                            (step) => step.step_number === influencer.sequence_step + 1,
+                        );
+                        const lastEmail = influencerEmails?.find((email) => email.sequence_step_id === lastStep?.id);
+                        return (
+                            <SequenceRow
+                                key={influencer.id}
+                                sequence={sequence}
+                                sequenceInfluencer={influencer}
+                                loadingEmails={loadingEmails}
+                                lastEmail={lastEmail}
+                                lastStep={lastStep}
+                                nextStep={nextStep}
+                                sequenceSteps={sequenceSteps}
+                                currentTab={currentTab}
+                                isMissingVariables={isMissingVariables}
+                                missingVariables={missingVariables}
+                                setShowUpdateTemplateVariables={setShowUpdateTemplateVariables}
+                                templateVariables={templateVariables}
+                                handleStartSequence={handleStartSequence}
+                                checked={selection.includes(influencer.id)}
+                                onCheckboxChange={handleCheckboxChange}
+                            />
+                        );
+                    })}
+                </tbody>
+            </table>
+            <Pagination setPageIndex={setCurrentPage} currentPage={currentPage} pages={totalNumberOfPages()} />
         </div>
-        //   * use state total number of pages,currentpage,if can nextpage, sa setter of the current page  */
-        
     );
 };
 
