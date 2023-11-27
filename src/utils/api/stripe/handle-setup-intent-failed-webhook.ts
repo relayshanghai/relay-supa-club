93fbaa50b91a21c2fe82601470f824a0923c8996
Ellie Wang <yuetingw@outlook.com>
2023-11-27 18:21:17 +0800

    comment for setupIntent failed webhook

--- src/utils/api/stripe/handle-setup-intent-failed-webhook.ts
+++ src/utils/api/stripe/handle-setup-intent-failed-webhook.ts
@@ -1,24 +1,13 @@
 import type { NextApiResponse } from 'next';
-import router from 'next/router';
 import httpCodes from 'src/constants/httpCodes';
 import type { SetupIntentFailed } from 'types/stripe/setup-intent-failed-webhook';
 
 export const handleSetupIntentFailed = async (res: NextApiResponse, setupIntentBody: SetupIntentFailed) => {
-    const { customer, last_setup_error } = setupIntentBody.data.object;
+    const { customer } = setupIntentBody.data.object;
     if (!customer) {
         throw new Error('Missing customer ID in invoice body');
     }
+    //currently we handle the fail setup in the client side on the /confirm-alipay page, this is a placeholder for future uses
 
-    const selectedPlan = localStorage.getItem('selectedPlan');
-    if (!selectedPlan) {
-        router.push('/upgrade');
-        // throw error cannot find selected plan from local storage
-    }
-
-    if (last_setup_error && last_setup_error.code === 'setup_intent_authentication_failure') {
-        router.push(`/payments?plan=${selectedPlan}`);
-        // return the error to the client
-    }
-
-    return res.status(httpCodes.OK).json({});
+    return res.status(httpCodes.OK).json({ message: 'OK' });
 };
