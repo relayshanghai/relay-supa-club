c57e9be625c764e2b9bfeb27f3f275556ee2e66c
James <james@relay.club>
2023-11-28 15:37:25 +0800

    Add more logging for debugging jobs on prod

--- src/utils/scheduler/queues/default.ts
+++ src/utils/scheduler/queues/default.ts
@@ -15,28 +15,32 @@ export const Default: JobQueue<typeof QUEUE_NAME> = {
     run: async (payload) => {
         const queueName = payload?.queue ?? QUEUE_NAME;
 
+        crumb({ message: `Start queue: ${queueName}` });
         const jobs = await db(fetchJobs)({
             queue: queueName,
             status: payload?.status ?? JOB_STATUS.pending,
             limit: payload?.limit ?? 1,
         });
 
+        crumb({ message: `Fetched Jobs: ${jobs.length} @ ${queueName}` });
         const runningJobs = jobs.map(async (job) => {
-            crumb({ message: `Start Job: ${job.id}` });
+            crumb({ message: `Start Job: ${job.id} @ ${queueName}` });
             const jobResult = await runJob(job);
 
-            crumb({ message: `Finish Job: ${job.id}` });
+            crumb({ message: `Finish Job: ${job.id} @ ${queueName}` });
             await finishJob(job, jobResult.status, jobResult.result);
 
-            crumb({ message: `Done Job: ${job.id}` });
+            crumb({ message: `Done Job: ${job.id} @ ${queueName}` });
             return { job: job.id, result: jobResult.status === JOB_STATUS.success };
         });
 
+        crumb({ message: `Settling Jobs: ${jobs.length} @ ${queueName}` });
         const finishedJobs = await Promise.allSettled(runningJobs);
         const results: { job: string; result: boolean }[] = finishedJobs.map((result) =>
             result.status === 'fulfilled' ? result.value : result.reason,
         );
 
+        crumb({ message: `Done Queue: ${jobs.length} @ ${queueName}` });
         return results;
     },
 };
