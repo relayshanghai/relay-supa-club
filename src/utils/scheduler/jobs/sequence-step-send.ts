1f494521a4174e7b03f7a6ac0d0ac78ae786ed4a
jacobcoro <xiaokourou@gmail.com>
2023-11-28 12:01:47 +0800

    reset only outreach email

--- src/utils/scheduler/jobs/sequence-step-send.ts
+++ src/utils/scheduler/jobs/sequence-step-send.ts
@@ -207,6 +207,10 @@ const handleResult = async (result: SendResult, influencer: SequenceInfluencerMa
 
 /** Revert the optimistic update and set the influencer to 'To Contact', delete the sequence_emails, and cancel outgoing emails in the outbox */
 const handleSendFailed = async (sequenceInfluencer: SequenceInfluencerManagerPage, outbox: OutboxGetMessage[]) => {
+    if (sequenceInfluencer.sequence_step !== 0) {
+        // only reset the sequence influencer if it fails on the first step
+        return;
+    }
     try {
         await db<typeof updateSequenceInfluencerCall>(updateSequenceInfluencerCall)({
             id: sequenceInfluencer.id,
