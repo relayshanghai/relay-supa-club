f9dbaaa1594cbd868b68064726f9b30027a081ff
jacobcoro <xiaokourou@gmail.com>
2023-11-30 14:38:34 +0800

    scope store to session

--- src/utils/indexeddb-cache-provider.tsx
+++ src/utils/indexeddb-cache-provider.tsx
@@ -2,15 +2,13 @@ import { useCacheProvider } from 'src/utils/cache-provider';
 import { SWRConfig } from 'swr';
 import { appCacheDBKey, appCacheStoreKey } from 'src/constants';
 import type { FC, PropsWithChildren } from 'react';
-import { useEffect } from 'react';
+import { useSessionContext } from '@supabase/auth-helpers-react';
 
 export const CacheProvider: FC<PropsWithChildren> = ({ children }) => {
-    useEffect(() => {
-        localStorage.removeItem(appCacheDBKey); // get rid of our previous localStorage cache. We can remove this line after a few weeks
-    }, []);
+    const { session } = useSessionContext();
 
     const cacheProvider = useCacheProvider({
-        dbName: appCacheDBKey,
+        dbName: appCacheDBKey(session?.user.id ?? ''),
         storeName: appCacheStoreKey,
     });
     if (!cacheProvider) {
