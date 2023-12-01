a201bddf23d28a837648488b2d8e11b0983e9c40
jacobcoro <xiaokourou@gmail.com>
2023-12-01 11:21:16 +0800

    add note on what noOfLinks does

--- src/components/boostbot/table/helper.ts
+++ src/components/boostbot/table/helper.ts
@@ -1,4 +1,21 @@
-export const getPaginationItems = (currentPage: number, lastPage: number, noOfLinks: number) => {
+export const getPaginationItems = (
+    currentPage: number,
+    lastPage: number,
+    /** 
+    * This refers to the number of page links you want to display in your pagination component. For example, if noOfLinks is set to 10, your pagination UI will consistently show 10 page links at a time. (It acts as a limiter if you the number of links is greater than no OFlinks the disabled links(...) will be used. 
+
+    * noOfLinks 10 would give you "1,2,3 ... 7,8... 11, 12, 13". 
+    
+    * if noOfLinks 11, the first 5 and last 5 are active, with one link disabled in the middle. You can visualize it like this:
+
+    * Active Links: [1] [2] [3] [4] [5] [...] [7] [8] [9] [10] [11]
+
+    * [1] to [5] are the first 5 active links.
+    * [...] represents the disabled link in the middle.
+    * [7] to [11] are the last 5 active links. 
+    */
+    noOfLinks: number,
+) => {
     const res = [];
     const firstPage = 1;
     const divisionOfLinks = 3; // represents first page  , .... , lastpage that will always be visible
