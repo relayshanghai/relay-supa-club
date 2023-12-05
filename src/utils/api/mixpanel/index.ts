4c906802d106e7bd331b7089c153ca78623ba50e
TGS963 <ghoshsuvojit2012@gmail.com>
2023-12-05 09:15:13 +0530

    remove throw

--- src/utils/api/mixpanel/index.ts
+++ src/utils/api/mixpanel/index.ts
@@ -2,10 +2,6 @@ export const MIXPANEL_PROJECT_TOKEN = process.env.MIXPANEL_PROJECT_TOKEN;
 
 import mixpanel from 'mixpanel';
 
-if (!MIXPANEL_PROJECT_TOKEN) {
-    throw new Error('Missing MIXPANEL_PROJECT_TOKEN, tracking will be disabled');
-}
-
 // Initialize mixpanel client
 
-export const mixpanelClient = mixpanel.init(MIXPANEL_PROJECT_TOKEN);
+export const mixpanelClient = mixpanel.init(MIXPANEL_PROJECT_TOKEN as string);
