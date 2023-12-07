50f6a7e7c58ab9d38154eb5d111645f7375613b5
TGS963 <ghoshsuvojit2012@gmail.com>
2023-12-07 09:53:16 +0530

    change login

--- cypress/e2e/caching.cy.ts
+++ cypress/e2e/caching.cy.ts
@@ -7,7 +7,7 @@ describe('Caches SWR requests', () => {
         deleteAppCacheDatabases();
     });
     it('caches reports from `use-report`', () => {
-        cy.loginTeammate();
+        cy.loginAdmin();
         cy.visit('/dashboard');
         cy.contains('Search by Topics', { timeout: 10000 });
         cy.getByTestId(`open-influencer-modal/${cocomelonId}`, { timeout: 60000 });
