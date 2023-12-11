fce18e45be7ec70222596b1505db182689522f2c
jacobcoro <xiaokourou@gmail.com>
2023-12-11 11:20:07 +0800

    fix svgs

--- src/components/signup/step-three.tsx
+++ src/components/signup/step-three.tsx
@@ -33,7 +33,7 @@ const TermsModal = ({ setShowModal }: { setShowModal: (show: boolean) => void })
                         >
                             <path
                                 strokeLinecap="round"
-                                stroke-linejoin="round"
+                                strokeLinejoin="round"
                                 d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
                         </svg>
