0b90d21c78b2c3423111bc61cfd2f6392b03fea6
TGS963 <ghoshsuvojit2012@gmail.com>
2023-12-11 19:56:53 +0530

    restore redirect utl

--- pages/api/profiles/change-email-link.ts
+++ pages/api/profiles/change-email-link.ts
@@ -10,13 +10,13 @@ export type ChangeEmailLinkBody = {
 };
 
 const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { oldMail, newMail } = req.body as ChangeEmailLinkBody;
+    const { oldMail, newMail, redirectUrl } = req.body as ChangeEmailLinkBody;
     const { data, error } = await supabase.auth.admin.generateLink({
         type: 'email_change_new',
         email: oldMail,
         newEmail: newMail,
         options: {
-            redirectTo: 'www.google.com',
+            redirectTo: `${redirectUrl}/login?${new URLSearchParams({ newMail })}`,
         },
     });
     if (error) return res.status(httpCodes.BAD_REQUEST).json({ error: error.message });
