a912459fef9c2bc86505defa2001d7fa56a19924
jacobcoro <xiaokourou@gmail.com>
2023-12-13 15:50:47 +0800

    more to get local user more realistic

--- src/mocks/test-user.ts
+++ src/mocks/test-user.ts
@@ -12,6 +12,7 @@ export const mockCompany: CompanyDB = {
     website: 'https://blue-moonlight-stream.com',
     avatar_url: null,
     updated_at: null,
+    /** a real (fake) customer record in the Stripe test mode */
     cus_id: 'cus_PB6Gc1XChOWv5t',
     searches_limit: '100000000',
     profiles_limit: '100000000',
