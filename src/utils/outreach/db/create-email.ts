aa8b5befbfc8db23ce993579b4db3f5f0696ad6c
James <james@relay.club>
2023-12-20 11:01:44 +0800

    Syncing emails will now ignore conflicts via email_engine_id

--- src/utils/outreach/db/create-email.ts
+++ src/utils/outreach/db/create-email.ts
@@ -1,11 +1,12 @@
 import { emails } from 'drizzle/schema';
 import type { DBQuery } from '../../database';
 import { db } from '../../database';
+import { eq } from 'drizzle-orm';
 
 type CreateEmailFn = (params: typeof emails.$inferInsert) => Promise<typeof emails.$inferSelect>;
 
 export const createEmail: DBQuery<CreateEmailFn> = (i) => async (params) => {
-    const result = await db(i)
+    let result = await db(i)
         .insert(emails)
         .values({
             data: params.data,
@@ -16,7 +17,12 @@ export const createEmail: DBQuery<CreateEmailFn> = (i) => async (params) => {
             emailEngineId: params.emailEngineId,
             emailEngineAccountId: params.emailEngineAccountId,
         })
+        .onConflictDoNothing({ target: emails.emailEngineId })
         .returning();
+
+    if (result.length <= 0) {
+        result = await db(i).select().from(emails).where(eq(emails.emailEngineId, params.emailEngineId)).limit(1);
+    }
 
     if (result.length !== 1) throw new Error('Error in inserting row');
 
