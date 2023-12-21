d4b22c3d016e35a274008c9c773f21d547f948cd
TGS963 <ghoshsuvojit2012@gmail.com>
2023-12-21 10:44:41 +0530

    add account to send quick emails

--- src/constants/employeeContacts.ts
+++ src/constants/employeeContacts.ts
@@ -41,4 +41,5 @@ export const EMPLOYEE_EMAILS = [
 export const QUICK_SEND_EMAIL_ACCOUNTS = [
     'egtljwhuz89pfkmj', // jacob@boostbot.ai
     '6nitzaf4gajlnt5c', // kirsten@boostbot.ai
+    '3tsfh97079jmgnni', // anne_ulike@beauty.boostbot.ai
 ];
