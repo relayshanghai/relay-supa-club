9cdc7365f7d5679d18fcf82465c28b758e3cd661
jacobcoro <xiaokourou@gmail.com>
2023-12-26 12:22:06 +0800

    turn off video in tests

--- cypress/e2e/guide_page.cy.ts
+++ cypress/e2e/guide_page.cy.ts
@@ -15,7 +15,7 @@ describe('checks restricted to guide page', () => {
         setupIntercepts();
         cy.loginTestUser();
         Object.keys(guidePage.modalInfo).forEach((section) => {
-            cy.visit('/guide');
+            cy.visit('/guide?show_video=false');
             const sectionData = guidePage.modalInfo[section as keyof typeof guidePage.modalInfo];
             cy.get('[data-testid="guide-modal-' + section + '"]').click();
             cy.contains(sectionData.sections[0].description, { timeout: 10000 });
@@ -27,7 +27,7 @@ describe('checks restricted to guide page', () => {
     it('check modal for every separate guide but go back', () => {
         setupIntercepts();
         cy.loginTestUser();
-        cy.visit('/guide');
+        cy.visit('/guide?show_video=false');
         Object.keys(guidePage.modalInfo).forEach((section) => {
             const sectionData = guidePage.modalInfo[section as keyof typeof guidePage.modalInfo];
             cy.get('[data-testid="guide-modal-' + section + '"]').click();
