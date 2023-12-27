a838adddc0eb142f50a03170bc0e057ca1bc5b93
jacobcoro <xiaokourou@gmail.com>
2023-12-27 11:41:35 +0800

    add enabled check

--- cypress/e2e/guide_page.cy.ts
+++ cypress/e2e/guide_page.cy.ts
@@ -30,7 +30,9 @@ describe('checks restricted to guide page', () => {
         cy.visit('/guide?show_video=false');
         Object.keys(guidePage.modalInfo).forEach((section) => {
             const sectionData = guidePage.modalInfo[section as keyof typeof guidePage.modalInfo];
-            cy.get('[data-testid="guide-modal-' + section + '"]').click();
+            cy.get('[data-testid="guide-modal-' + section + '"]')
+                .should('be.enabled')
+                .click();
             cy.contains(sectionData.sections[0].description, { timeout: 10000 });
             cy.contains(guidePage.goBack).should('be.enabled').click();
             cy.url().should('include', '/guide');
