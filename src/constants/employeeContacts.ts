15f92b20114bb7ab5dd5613e25700f887892113b
James <james@relay.club>
2024-01-09 12:17:11 +0800

    Fix use emails not EE emails

--- src/constants/employeeContacts.ts
+++ src/constants/employeeContacts.ts
@@ -44,4 +44,4 @@ export const QUICK_SEND_EMAIL_ACCOUNTS = [
     '3tsfh97079jmgnni', // anne_ulike@beauty.boostbot.ai
 ];
 
-export const QUICK_SEND_EMAIL_IDS = ['jacob@boostbot.ai', 'kirsten@boostbot.ai', 'anne_ulike@beauty.boostbot.ai'];
+export const QUICK_SEND_EMAIL_IDS = ['jacob@relay.club', 'adriennesamson27@gmail.com', 'anne_ulike@beauty.boostbot.ai'];
