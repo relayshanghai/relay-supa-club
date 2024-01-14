544cabd8046bb1ae1d592e1234ced1f5c3ff0488
echaoeoen <andyepso@gmail.com>
2024-01-14 22:03:26 +0700

    fix context

--- pages/api/profiles/payment-methods.ts
+++ pages/api/profiles/payment-methods.ts
@@ -1,25 +1,14 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import { getProfileByIdCall } from 'src/utils/api/db/calls/profiles';
+import { ApiHandlerWithContext } from 'src/utils/api-handler';
 import { getCustomerPaymentMethods } from 'src/utils/api/stripe/payment-method';
-import { getSession } from 'src/utils/auth';
-import { db } from 'src/utils/supabase-client';
+import { RequestContext } from 'src/utils/request-context/request-context';
 
 export const getHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const session = await getSession(req, res);
-    if (!session.data.session?.user.id) {
-        throw new Error('Not logged in');
-    }
-    const { data: manager, error: getManagerError } = await db(getProfileByIdCall)(session.data.session.user.id);
-    if (!manager?.company?.cus_id) {
-        throw getManagerError;
-    }
-    const paymentMethods = await getCustomerPaymentMethods(manager.company?.cus_id);
+    const customerId = RequestContext.getContext().customerId as string;
+    const paymentMethods = await getCustomerPaymentMethods(customerId);
     res.send(paymentMethods);
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        return getHandler(req, res);
-    }
-    return res.status(404).send('Not found');
-}
+export default ApiHandlerWithContext({
+    getHandler,
+});
