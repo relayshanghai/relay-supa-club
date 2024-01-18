f632c7d7f800ca6980d9739308a6c892c02e777e
TGS963 <ghoshsuvojit2012@gmail.com>
2024-01-18 11:54:39 +0530

    fix pagination

--- pages/component-previews/inbox.tsx
+++ pages/component-previews/inbox.tsx
@@ -531,7 +531,7 @@ const InboxPreview = () => {
     const loadMoreThreads = useCallback(() => {
         const totalThreads = totals.replied + totals.unopened + totals.unreplied;
         if (threads && threads.length > 0 && threads.length < totalThreads && !isThreadsLoading) {
-            setPage(Math.floor(threads.length / totalThreads) + 1);
+            setPage(Math.round(threads.length / totalThreads) + 1);
         }
     }, [setPage, threads, totals, isThreadsLoading]);
 
