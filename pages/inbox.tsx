f8bf2183112e95c47f0d446b9466092fcb057440
echaoeoen <andyepso@gmail.com>
2024-01-30 13:20:52 +0700

    enable new inbox in staging

--- pages/inbox.tsx
+++ pages/inbox.tsx
@@ -1,11 +1,5 @@
-import { InboxPageDummy } from 'src/components/inbox/inbox-page-dummy';
-import { useUser } from 'src/hooks/use-user';
-import { InboxPage } from 'src/components/inbox/inbox-page';
+import InboxPreview from './component-previews/inbox';
 
 export default function Page() {
-    const { profile } = useUser();
-    if (!profile?.email_engine_account_id) {
-        return <InboxPageDummy />;
-    }
-    return <InboxPage />;
+    return <InboxPreview />;
 }
