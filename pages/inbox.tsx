e5c491158d1312c66ef4c258d0904ea85cfda8b0
echaoeoen <andyepso@gmail.com>
2024-01-31 09:18:36 +0700

    fix inbox

--- pages/inbox.tsx
+++ pages/inbox.tsx
@@ -1,5 +1,11 @@
+import { InboxPageDummy } from 'src/components/inbox/inbox-page-dummy';
+import { useUser } from 'src/hooks/use-user';
 import InboxPreview from './component-previews/inbox';
 
 export default function Page() {
+    const { profile } = useUser();
+    if (!profile?.email_engine_account_id) {
+        return <InboxPageDummy />;
+    }
     return <InboxPreview />;
 }
