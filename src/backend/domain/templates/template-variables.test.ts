cb4796b36e3a08f93ea240863af47d0bb0d2a162
jacobcoro <xiaokourou@gmail.com>
2024-02-08 14:38:02 +0800

    refactor to fit convention

--- src/backend/domain/templates/template-variables.test.ts
+++ src/backend/domain/templates/template-variables.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, vi, beforeEach } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import * as dbCalls from '../../../backend/database/outreach-template-variables';
 import TemplateVariablesService from './template-variables';
 import type {
@@ -17,43 +17,48 @@ describe('src/backend/domain/templates/template-variables.test.ts', () => {
             companyId,
         });
     });
-    test('method: getTemplateVariablesByCompanyId calls correct db helper with correct parameters to get template variables by company id', () => {
-        const getOutreachTemplateVariablesByCompanyIdCallMock = vi.fn();
-        vi.spyOn(dbCalls, 'getOutreachTemplateVariablesByCompanyIdCall').mockImplementation(
-            () => getOutreachTemplateVariablesByCompanyIdCallMock,
-        );
-        const templateVariablesService = new TemplateVariablesService();
-        templateVariablesService.getTemplateVariablesByCompanyId();
-        expect(getOutreachTemplateVariablesByCompanyIdCallMock).toHaveBeenCalledWith(companyId);
+    describe('.getTemplateVariablesByCompanyId()', () => {
+        it('calls correct db helper with correct parameters to get template variables by company id', () => {
+            const getOutreachTemplateVariablesByCompanyIdCallMock = vi.fn();
+            vi.spyOn(dbCalls, 'getOutreachTemplateVariablesByCompanyIdCall').mockImplementation(
+                () => getOutreachTemplateVariablesByCompanyIdCallMock,
+            );
+            const templateVariablesService = new TemplateVariablesService();
+            templateVariablesService.getTemplateVariablesByCompanyId();
+            expect(getOutreachTemplateVariablesByCompanyIdCallMock).toHaveBeenCalledWith(companyId);
+        });
+    });
+    describe('.updateTemplateVariable()', () => {
+        it('calls correct db helper with correct parameters to update template variable', () => {
+            const updateOutreachTemplateVariableCallMock = vi.fn();
+            vi.spyOn(dbCalls, 'updateOutreachTemplateVariableCall').mockImplementation(
+                () => updateOutreachTemplateVariableCallMock,
+            );
+            const update: OutreachTemplateVariablesUpdate = {
+                company_id: 'companyId',
+                id: 'variableId',
+                name: 'variableName',
+            };
+            const templateVariablesService = new TemplateVariablesService();
+            templateVariablesService.updateTemplateVariable(update);
+            expect(updateOutreachTemplateVariableCallMock).toHaveBeenCalledWith(update);
+        });
     });
 
-    test('method: updateTemplateVariable calls correct db helper with correct parameters to update template variable', () => {
-        const updateOutreachTemplateVariableCallMock = vi.fn();
-        vi.spyOn(dbCalls, 'updateOutreachTemplateVariableCall').mockImplementation(
-            () => updateOutreachTemplateVariableCallMock,
-        );
-        const update: OutreachTemplateVariablesUpdate = {
-            company_id: 'companyId',
-            id: 'variableId',
-            name: 'variableName',
-        };
-        const templateVariablesService = new TemplateVariablesService();
-        templateVariablesService.updateTemplateVariable(update);
-        expect(updateOutreachTemplateVariableCallMock).toHaveBeenCalledWith(update);
-    });
-
-    test('method: insertTemplateVariable calls correct db helper with correct parameters to insert template variable', () => {
-        const insertOutreachTemplateVariableCallMock = vi.fn();
-        vi.spyOn(dbCalls, 'insertOutreachTemplateVariableCall').mockImplementation(
-            () => insertOutreachTemplateVariableCallMock,
-        );
-        const insert: OutreachTemplateVariablesInsert = {
-            company_id: 'companyId',
-            name: 'variableName',
-            value: 'variableValue',
-        };
-        const templateVariablesService = new TemplateVariablesService();
-        templateVariablesService.insertTemplateVariable(insert);
-        expect(insertOutreachTemplateVariableCallMock).toHaveBeenCalledWith(insert);
+    describe('.insertTemplateVariable()', () => {
+        it('calls correct db helper with correct parameters to insert template variable', () => {
+            const insertOutreachTemplateVariableCallMock = vi.fn();
+            vi.spyOn(dbCalls, 'insertOutreachTemplateVariableCall').mockImplementation(
+                () => insertOutreachTemplateVariableCallMock,
+            );
+            const insert: OutreachTemplateVariablesInsert = {
+                company_id: 'companyId',
+                name: 'variableName',
+                value: 'variableValue',
+            };
+            const templateVariablesService = new TemplateVariablesService();
+            templateVariablesService.insertTemplateVariable(insert);
+            expect(insertOutreachTemplateVariableCallMock).toHaveBeenCalledWith(insert);
+        });
     });
 });
