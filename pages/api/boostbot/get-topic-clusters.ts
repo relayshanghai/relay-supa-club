667a1dff600b1d67975030433054ff3a2821083e
jacobcoro <xiaokourou@gmail.com>
2024-02-20 16:43:15 +0800

    handle openai returns clusters as an object with a clusters

--- pages/api/boostbot/get-topic-clusters.ts
+++ pages/api/boostbot/get-topic-clusters.ts
@@ -20,7 +20,13 @@ const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const { productDescription, topics } = result.data;
-    const topicClusters: GetTopicClustersResponse = await getTopicClusters(productDescription, topics);
+    const topicClusters: GetTopicClustersResponse | { clusters: string[][] } = await getTopicClusters(
+        productDescription,
+        topics,
+    );
+    if (typeof topicClusters === 'object' && 'clusters' in topicClusters) {
+        return res.status(httpCodes.OK).json(topicClusters.clusters);
+    }
 
     return res.status(httpCodes.OK).json(topicClusters);
 };
