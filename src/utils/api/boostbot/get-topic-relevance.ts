bb2d9a0275c1b5fb6f837aa57cceff9cdda060f4
jacobcoro <xiaokourou@gmail.com>
2024-02-21 15:22:41 +0800

    add more validation to open ai and force return object.

--- src/utils/api/boostbot/get-topic-relevance.ts
+++ src/utils/api/boostbot/get-topic-relevance.ts
@@ -21,28 +21,30 @@ export type TopicsAndRelevance = {
 export const getTopicsAndRelevance = async (topics: RelevantTopic[]): Promise<TopicsAndRelevance[]> => {
     const openai = new OpenAIApi(configuration);
 
-    const systemPrompt = `Based on the given influencer topics, please generate 7 diverse niche groups and its relevance to the given topics. The groups should have good relevance to the topics. Do not use more than two words for the labels.
+    const systemPrompt = `Based on the given influencer topics, please generate an array of 7 diverse niche groups and its relevance to the given topics. The groups should have good relevance to the topics. Do not use more than two words for the labels.
 
-    Influencer topics: [
+    example Influencer topics: [
         { tag: 'makeupartist', distance: 0.75 },
         { tag: 'howtobeauty', distance: 0.15 },
         { tag: 'vancouvermua', distance: 0.45 },
         { tag: 'beautyenthusiast', distance: 0.25 }
       ]
 
-Example response:
-[
-    { "topic_en": "Locations", "topic_zh": "位置", "relevance": 0.48 },
-    { "topic_en": "Retail", "topic_zh": "零售", "relevance": 0.36 },
-    { "topic_en": "Makeup Skills", "topic_zh": "化妆技巧", "relevance": 0.38 },
-    { "topic_en": "Beauty", "topic_zh": "美容", "relevance": 0.28 },
-    { "topic_en": "Fashion", "topic_zh": "时尚", "relevance": 0.24 },
-    { "topic_en": "Makeup Artists", "topic_zh": "化妆师", "relevance": 0.32 },
-    { "topic_en": "Ethnic Themes", "topic_zh": "民族主题", "relevance": 0.34 }
-]
+    Example response:
+    {
+        "topics": [
+            { "topic_en": "Locations", "topic_zh": "位置", "relevance": 0.48 },
+            { "topic_en": "Retail", "topic_zh": "零售", "relevance": 0.36 },
+            { "topic_en": "Makeup Skills", "topic_zh": "化妆技巧", "relevance": 0.38 },
+            { "topic_en": "Beauty", "topic_zh": "美容", "relevance": 0.28 },
+            { "topic_en": "Fashion", "topic_zh": "时尚", "relevance": 0.24 },
+            { "topic_en": "Makeup Artists", "topic_zh": "化妆师", "relevance": 0.32 },
+            { "topic_en": "Ethnic Themes", "topic_zh": "民族主题", "relevance": 0.34 }
+        ]
+    }
 
-Only respond in JSON format with the 7 objects as an array. Do not respond with any other text.
-`;
+    Make sure to respond in JSON format. Respond with an array of 7 topic objects in the above format. Do not respond with any other text. Make sure the array is exactly seven items long before responding.
+    `;
 
     const userPrompt = `Influencer topics: "${JSON.stringify(topics)}"`;
 
@@ -65,7 +67,29 @@ Only respond in JSON format with the 7 objects as an array. Do not respond with 
             .replace(/,\s*}/g, '}') // remove trailing commas
             .replace(/```json/g, ''); // replace ```json
 
-        const topicsAndRelevance = JSON.parse(fixedString);
+        const parsed = JSON.parse(fixedString);
+
+        if (typeof parsed !== 'object') {
+            throw new Error('Unable to parse topic clusters response from OpenAI');
+        }
+        const topicsAndRelevance = 'topics' in parsed ? parsed.topics : parsed;
+
+        if (!Array.isArray(topicsAndRelevance)) {
+            ('Unable to parse topic clusters array response from OpenAI');
+        }
+        // check each topic has the required fields
+        for (const topic of topicsAndRelevance) {
+            if (
+                !('topic_en' in topic) ||
+                typeof topic.topic_en !== 'string' ||
+                !('topic_zh' in topic) ||
+                typeof topic.topic_zh !== 'string' ||
+                !('relevance' in topic) ||
+                typeof topic.relevance !== 'number'
+            ) {
+                throw new Error('Invalid topic clusters response from OpenAI');
+            }
+        }
 
         return topicsAndRelevance;
     } catch (error) {
