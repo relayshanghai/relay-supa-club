5fa1abd8e1379d0b9464b6bb2b850ba45862b445
TGS963 <ghoshsuvojit2012@gmail.com>
2024-02-23 14:33:52 +0530

    refactor: sort email templates before returning

--- src/hooks/use-email-templates.ts
+++ src/hooks/use-email-templates.ts
@@ -1,15 +1,29 @@
 import useSWR from 'swr';
 import { nextFetch } from 'src/utils/fetcher';
 import type { TemplatesPostResponseBody } from 'pages/api/email-engine/templates';
+import { type TemplatesTemplateGetResponse } from 'types/email-engine/templates-template-get';
 
 export const useEmailTemplates = (templateIds: string[]) => {
     const { data: emailTemplates, mutate: refreshEmailTemplates } = useSWR(
         templateIds.length > 0 ? [templateIds, 'email_templates'] : null,
-        ([ids]) =>
-            nextFetch<TemplatesPostResponseBody>('email-engine/templates', {
+        async ([ids]) => {
+            const res = await nextFetch<TemplatesPostResponseBody>('email-engine/templates', {
                 method: 'POST',
                 body: { templateIds: ids },
-            }),
+            });
+            const emailTemplateSortFunction = (a: TemplatesTemplateGetResponse, b: TemplatesTemplateGetResponse) => {
+                if (a.name === 'Outreach') {
+                    return -1; // "Outreach" comes first
+                } else if (b.name === 'Outreach') {
+                    return 1; // "Outreach" comes later
+                } else {
+                    // Sort alphabetically by name for all other elements
+                    return a.name.localeCompare(b.name);
+                }
+            };
+
+            return res.sort(emailTemplateSortFunction);
+        },
         { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false },
     );
 
