06aeb4ca2554fc655f51d38089e8042656f8ee6f
TGS963 <ghoshsuvojit2012@gmail.com>
2024-02-23 14:36:23 +0530

    refactor: move function definition outside swr

--- src/hooks/use-email-templates.ts
+++ src/hooks/use-email-templates.ts
@@ -3,6 +3,17 @@ import { nextFetch } from 'src/utils/fetcher';
 import type { TemplatesPostResponseBody } from 'pages/api/email-engine/templates';
 import { type TemplatesTemplateGetResponse } from 'types/email-engine/templates-template-get';
 
+const emailTemplateSortFunction = (a: TemplatesTemplateGetResponse, b: TemplatesTemplateGetResponse) => {
+    if (a.name === 'Outreach') {
+        return -1; // "Outreach" comes first
+    } else if (b.name === 'Outreach') {
+        return 1; // "Outreach" comes later
+    } else {
+        // Sort alphabetically by name for all other elements
+        return a.name.localeCompare(b.name);
+    }
+};
+
 export const useEmailTemplates = (templateIds: string[]) => {
     const { data: emailTemplates, mutate: refreshEmailTemplates } = useSWR(
         templateIds.length > 0 ? [templateIds, 'email_templates'] : null,
@@ -11,16 +22,6 @@ export const useEmailTemplates = (templateIds: string[]) => {
                 method: 'POST',
                 body: { templateIds: ids },
             });
-            const emailTemplateSortFunction = (a: TemplatesTemplateGetResponse, b: TemplatesTemplateGetResponse) => {
-                if (a.name === 'Outreach') {
-                    return -1; // "Outreach" comes first
-                } else if (b.name === 'Outreach') {
-                    return 1; // "Outreach" comes later
-                } else {
-                    // Sort alphabetically by name for all other elements
-                    return a.name.localeCompare(b.name);
-                }
-            };
 
             return res.sort(emailTemplateSortFunction);
         },
