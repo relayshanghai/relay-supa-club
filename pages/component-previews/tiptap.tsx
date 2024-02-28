93c8897b3a491eedd3ffcbc3a30b427a34fd0d04
TGS963 <ghoshsuvojit2012@gmail.com>
2024-02-28 14:01:44 +0530

    refactor: add html preview to preview page

--- pages/component-previews/tiptap.tsx
+++ pages/component-previews/tiptap.tsx
@@ -1,13 +1,14 @@
+import { useState } from 'react';
 import { Tiptap } from 'src/components/inbox/wip/tiptap';
 
 const PreviewTiptap = () => {
+    const [description, setDescription] = useState('');
     return (
-        <div className="m-6 border-2 border-black p-6">
+        <div className="m-6 flex flex-col border-2 border-black p-6">
             <Tiptap
                 description={''}
                 onChange={(text: string) => {
-                    // eslint-disable-next-line no-console
-                    console.log(text);
+                    setDescription(text);
                 }}
                 placeholder={''}
                 onSubmit={() => {
@@ -21,6 +22,10 @@ const PreviewTiptap = () => {
                     //
                 }}
             />
+            <div>
+                <p>Preview</p>
+                <section className="bg-gray-400 p-4" dangerouslySetInnerHTML={{ __html: description }} />
+            </div>
         </div>
     );
 };
