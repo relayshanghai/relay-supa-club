ba86c278488080b89c1c21125440b7cb2bebe925
echaoeoen <andyepso@gmail.com>
2024-03-05 11:00:08 +0700

    fix(BB-434): add function limiter to solve ratelimited iqdata endpoint

---
+++ src/backend/integration/distributed-queue/distributed-queue.ts
@@ -0,0 +1,34 @@
+import { kv } from '@vercel/kv';
+
+export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const UseDistributedQueue =
+    (concurrencyPerSecond: number, delayCheck = 10): MethodDecorator =>
+    (target, method, description: PropertyDescriptor) => {
+        const originalMethod = description.value;
+        description.value = async function (...args: any[]) {
+            const key = `${target.constructor.name}-${method.toString()}`;
+            const queueKey = `${key}-queue`;
+            // check if the queue full, if full, wait for a while
+            let queue = [];
+
+            while (true) {
+                queue = (await kv.get<number[]>(queueKey)) || [];
+                if (queue.length < concurrencyPerSecond) {
+                    break;
+                }
+                if (queue[queue.length - 1] < Date.now() - 1000) {
+                    queue.pop();
+                    await kv.set(queueKey, queue);
+                    break;
+                }
+                await delay(delayCheck);
+            }
+            const now = Date.now();
+            queue = [now, ...queue];
+            await kv.set(queueKey, queue);
+
+            const result = await originalMethod.apply(this, args);
+            return result;
+        };
+    };
--- src/utils/api/iqdata/index.ts
+++ src/utils/api/iqdata/index.ts
@@ -10,6 +10,7 @@ import type { FetchCreatorsFilteredParams } from './transforms';
 import { prepareFetchCreatorsFiltered } from './transforms';
 import { logIqdataLimits } from '../forensicTrack';
 import { serverLogger } from 'src/utils/logger-server';
+import { UseDistributedQueue } from 'src/backend/integration/distributed-queue/distributed-queue';
 
 export const IQDATA_URL = 'https://socapi.icu/v2.0/api/';
 
@@ -34,41 +35,46 @@ export const withServerContext = <T extends (...args: any[]) => any>(fetchFuncti
     };
 };
 
-/**
- *
- * @param path will be prefixed by `https://socapi.icu/v2.0/api/`
- * @returns fetch .json() data
- * @description fetcher for IQData API. API docs: https://iqdata.social/docs/api
- */
-export const iqDataFetch = async <T = any>(path: string, options: RequestInit & { context?: ServerContext } = {}) => {
-    const { context, ...strippedOptions } = options;
+export class IQDataApiFetch {
+    static service = new IQDataApiFetch();
 
-    const res = await fetch(IQDATA_URL + path, {
-        ...strippedOptions,
-        headers: {
-            ...headers,
-            ...strippedOptions.headers,
-        },
-    });
+    /**
+     * @deprecated
+     * @param path will be prefixed by `https://socapi.icu/v2.0/api/`
+     * @returns fetch .json() data
+     * @description fetcher for IQData API. API docs: https://iqdata.social/docs/api
+     */
+    @UseDistributedQueue(10)
+    async request<T = any>(path: string, options: RequestInit & { context?: ServerContext } = {}) {
+        const { context, ...strippedOptions } = options;
 
-    await logIqdataLimits(res, path, context);
-    await handleResError(res);
-    const json = await res.json();
+        const res = await fetch(IQDATA_URL + path, {
+            ...strippedOptions,
+            headers: {
+                ...headers,
+                ...strippedOptions.headers,
+            },
+        });
 
-    if (context) {
-        await rudderstack.identify(context);
-        const identity = rudderstack.getIdentity();
-        try {
-            await rudderstack.send(json);
-        } catch (error: unknown) {
-            serverLogger(error, (scope) => {
-                return scope.setContext('Rudderstack Identity', identity);
-            });
+        await logIqdataLimits(res, path, context);
+        await handleResError(res);
+        const json = await res.json();
+
+        if (context) {
+            await rudderstack.identify(context);
+            const identity = rudderstack.getIdentity();
+            try {
+                await rudderstack.send(json);
+            } catch (error: unknown) {
+                serverLogger(error, (scope) => {
+                    return scope.setContext('Rudderstack Identity', identity);
+                });
+            }
         }
-    }
 
-    return json as T;
-};
+        return json as T;
+    }
+}
 
 //** Search for influencers */
 // We are currently using iqdata lookalike api to search for influencers, there is no api available for searching influencers by name.
@@ -80,24 +86,30 @@ export const fetchIqDataLookalikeByAudience = async (
     term: string,
     platform: CreatorPlatform,
     context?: ServerContext,
-) => await iqDataFetch(`dict/users/?q=${term}&type=lookalike&platform=${platform}&limit=5`, { context }); //Audience lookalike
+) =>
+    await IQDataApiFetch.service.request(`dict/users/?q=${term}&type=lookalike&platform=${platform}&limit=5`, {
+        context,
+    }); //Audience lookalike
 
 export const fetchIqDataLookalikeByInfluencer = async (
     term: string,
     platform: CreatorPlatform,
     context?: ServerContext,
-) => await iqDataFetch(`dict/users/?q=${term}&type=topic-tags&platform=${platform}&limit=5`, { context }); //Influencer lookalike
+) =>
+    await IQDataApiFetch.service.request(`dict/users/?q=${term}&type=topic-tags&platform=${platform}&limit=5`, {
+        context,
+    }); //Influencer lookalike
 
 export const fetchIqDataTopics = async (term: string, platform: CreatorPlatform, context?: ServerContext, limit = 10) =>
-    await iqDataFetch(`dict/topic-tags/?q=${term}&platform=${platform}&limit=${limit}`, { context });
+    await IQDataApiFetch.service.request(`dict/topic-tags/?q=${term}&platform=${platform}&limit=${limit}`, { context });
 
 export const fetchIqDataGeos = async (term: string, context?: ServerContext) =>
-    await iqDataFetch(`geos/?q=${term}&types=country&limit=5`, { context });
+    await IQDataApiFetch.service.request(`geos/?q=${term}&types=country&limit=5`, { context });
 
 export const fetchCreatorsFiltered = async (params: FetchCreatorsFilteredParams, context?: ServerContext) => {
     const { platform, body } = prepareFetchCreatorsFiltered(params);
 
-    return await iqDataFetch<CreatorSearchResult>(
+    return await IQDataApiFetch.service.request<CreatorSearchResult>(
         `search/newv1?${new URLSearchParams({ platform, auto_unhide: '1' })}`,
         {
             method: 'post',
@@ -114,25 +126,34 @@ export const requestNewReport = async (
     subscribe = false,
     dry_run = false,
 ) =>
-    await iqDataFetch<CreatorReport>(
+    await IQDataApiFetch.service.request<CreatorReport>(
         `reports/new?platform=${platform}&url=${id}&subscribe=${subscribe ? 1 : 0}&dry_run=${dry_run}`,
         { context },
     );
 
 export const fetchReport = async (reportId: string, context?: ServerContext) =>
-    await iqDataFetch<CreatorReport>(`reports/${reportId}`, { context });
+    await IQDataApiFetch.service.request<CreatorReport>(`reports/${reportId}`, { context });
 
 //** omit id to get all previously generated reports of that platform */
 export const fetchReportsMetadata = async (platform: CreatorPlatform, creator_id?: string, context?: ServerContext) =>
-    await iqDataFetch<CreatorReportsMetadata>(`reports?platform=${platform}${creator_id ? `&url=${creator_id}` : ''}`, {
-        context,
-    });
+    await IQDataApiFetch.service.request<CreatorReportsMetadata>(
+        `reports?platform=${platform}${creator_id ? `&url=${creator_id}` : ''}`,
+        {
+            context,
+        },
+    );
 
 export const fetchYoutubeVideoInfo = async (videoUrl: string, context?: ServerContext) =>
-    await iqDataFetch<YoutubeVideoDataRaw>(`raw/yt/video?${new URLSearchParams({ url: videoUrl })}`, { context });
+    await IQDataApiFetch.service.request<YoutubeVideoDataRaw>(
+        `raw/yt/video?${new URLSearchParams({ url: videoUrl })}`,
+        { context },
+    );
 
 export const fetchTiktokVideoInfo = async (videoUrl: string, context?: ServerContext) =>
-    await iqDataFetch<TikTokVideoDataRaw>(`raw/tt/user/media?${new URLSearchParams({ url: videoUrl })}`, { context });
+    await IQDataApiFetch.service.request<TikTokVideoDataRaw>(
+        `raw/tt/user/media?${new URLSearchParams({ url: videoUrl })}`,
+        { context },
+    );
 
 // export const fetchIqDataLookalikeByAudienceWithContext = withServerContextIqdata(fetchIqDataLookalikeByAudience);
 // export const fetchIqDataLookalikeByInfluencerWithContext = withServerContextIqdata(fetchIqDataLookalikeByInfluencer);
