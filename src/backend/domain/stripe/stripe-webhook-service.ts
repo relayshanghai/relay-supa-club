02226ba19d464fece2ae686ff473384173b27cf3
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-03-20 11:04:03 +0700

    need email content

--- src/backend/domain/stripe/stripe-webhook-service.ts
+++ src/backend/domain/stripe/stripe-webhook-service.ts
@@ -2,6 +2,7 @@ import { type StripeWebhookRequest } from 'pages/api/v2/stripe-webhook/request';
 import BillingEventRepository from 'src/backend/database/billing-event/billing-event-repository';
 import CompanyRepository from 'src/backend/database/company/company-repository';
 import SubscriptionRepository from 'src/backend/database/subcription/subscription-repository';
+import StripeService from 'src/backend/integration/stripe/stripe-service';
 
 export class StripeWebhookService {
     public static readonly service: StripeWebhookService = new StripeWebhookService();
@@ -10,7 +11,6 @@ export class StripeWebhookService {
     }
 
     async handler(request: StripeWebhookRequest) {
-        console.log('stripe webhook:', JSON.stringify(request));
         const company = await CompanyRepository.getRepository().findOne({
             where: {
                 cusId: request.data?.object.customer as string,
@@ -33,19 +33,73 @@ export class StripeWebhookService {
                 providerLastEvent: new Date(request.created * 1000).toString(),
             },
         );
+        await this.handlingWebhookTypes(request);
         return request;
     }
 
     private async handlingWebhookTypes(request: StripeWebhookRequest) {
         switch (request.type) {
             case 'charge.succeeded':
-                return this.handlingChargeSucceeded(request.data);
+                return this.chargeSucceededHandler(request.data);
             case 'charge.failed':
-                break;
+                return this.chargeFailedHandler(request.data);
             case 'charge.expired':
-                break;
+                return this.chargeExpiredHandler(request.data);
         }
     }
 
-    private async handlingChargeSucceeded(data: StripeWebhookRequest['data']) {}
+    private async chargeSucceededHandler(data: StripeWebhookRequest['data']) {
+        const subscription = await SubscriptionRepository.getRepository().findOne({
+            where: {
+                company: {
+                    cusId: data?.object.customer as string,
+                },
+            },
+        });
+        if (!subscription) {
+            throw new Error('Subscription not found');
+        }
+        const stripeSubscription = await StripeService.getService().retrieveSubscription(
+            subscription.providerSubscriptionId,
+        );
+        if (!stripeSubscription) {
+            throw new Error('Stripe subscription not found');
+        }
+        subscription.pausedAt = new Date(stripeSubscription.current_period_end * 1000);
+        subscription.cancelledAt = null;
+        await SubscriptionRepository.getRepository().save(subscription);
+    }
+
+    private async chargeFailedHandler(data: StripeWebhookRequest['data']) {
+        const subscription = await SubscriptionRepository.getRepository().findOne({
+            where: {
+                company: {
+                    cusId: data?.object.customer as string,
+                },
+            },
+        });
+        if (!subscription) {
+            throw new Error('Subscription not found');
+        }
+        subscription.cancelledAt = new Date();
+        await SubscriptionRepository.getRepository().save(subscription);
+    }
+
+    private async chargeExpiredHandler(data: StripeWebhookRequest['data']) {
+        const subscription = await SubscriptionRepository.getRepository().findOne({
+            where: {
+                company: {
+                    cusId: data?.object.customer as string,
+                },
+            },
+        });
+        if (!subscription) {
+            throw new Error('Subscription not found');
+        }
+        if (!subscription.cancelledAt) {
+            subscription.cancelledAt = new Date();
+        }
+        await SubscriptionRepository.getRepository().save(subscription);
+        await StripeService.getService().cancelSubscription(subscription.providerSubscriptionId);
+    }
 }
