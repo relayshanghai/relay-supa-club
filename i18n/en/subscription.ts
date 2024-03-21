b32bfba4484684c9d63bc5dd4a84def27d8be81e
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-03-21 20:26:40 +0700

    fix: payment callback page

--- i18n/en/subscription.ts
+++ i18n/en/subscription.ts
@@ -1,5 +1,11 @@
 const subscription = {
     redirection: 'We Will Redirect You To Your Account Page',
     failedToConfirm: 'Failed to confirm your subscription. Please try again later.',
+    paymentCallback: {
+        title: 'Payment Successful',
+        thankYou: 'Thank you for choosing BoostBot!',
+        redirection: 'We’ll redirect you back to the account page now.',
+        clickHere: 'Click here if not redirected',
+    },
 };
 export default subscription;
