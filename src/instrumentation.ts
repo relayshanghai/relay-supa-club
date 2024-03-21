c4802d0185cbf21561bf631a7c71460a39cb4c1d
echaoeoen <andyepso@gmail.com>
2024-03-21 08:49:43 +0700

    fix instrumentation only on nodejs

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,7 @@
 import { registerOTel } from '@vercel/otel';
-import './utils/apm';
-export function register() {
+export async function register() {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+        await import('./utils/apm');
+    }
     registerOTel('next-app');
 }
