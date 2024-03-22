5f52b87a1be78cb6dfb52ca8203d49c67fcfa92a
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-03-22 10:08:35 +0700

    verify the zh translation

--- i18n/en/subscription.ts
+++ i18n/en/subscription.ts
@@ -1,5 +1,4 @@
 const subscription = {
-    redirection: 'We Will Redirect You To Your Account Page',
     failedToConfirm: 'Failed to confirm your subscription. Please try again later.',
     paymentCallback: {
         title: 'Payment Successful',
--- i18n/zh/subscription.ts
+++ i18n/zh/subscription.ts
@@ -1,11 +1,10 @@
 const subscription = {
-    redirection: 'We Will Redirect You To Your Account Page',
-    failedToConfirm: 'Failed to confirm your subscription. Please try again later.',
+    failedToConfirm: '暂未能确认您的订阅。请稍后再试。',
     paymentCallback: {
-        title: 'Payment Successful',
-        thankYou: 'Thank you for choosing BoostBot!',
-        redirection: 'We’ll redirect you back to the account page now.',
-        clickHere: 'Click here if not redirected',
+        title: '付款成功',
+        thankYou: '感谢您选择雷宝！',
+        redirection: '现在将跳转回账号页面。',
+        clickHere: '如页面未能跳转，请单击此处',
     },
 };
 export default subscription;
