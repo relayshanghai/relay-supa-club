6356da004e055dfbd5dc5a7d9edd7d5215e8f148
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-05-21 05:51:01 +0700

    fix: zustand

--- src/utils/zustand.ts
+++ src/utils/zustand.ts
@@ -3,4 +3,4 @@ import { devtools } from 'zustand/middleware';
 
 export const create = <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
     initializer: StateCreator<T, [], Mos>,
-) => c(devtools(initializer));
+) => c(devtools(initializer, { enabled: process.env.NODE_ENV !== 'production' }));
