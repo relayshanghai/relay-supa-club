f0c07a343d9f35ace6470d4ae43b0b95760621d3
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-07-18 13:17:37 +0700

    fix: accept invitation

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,2 +1,2 @@
 export const hasCustomError = (error: any, customErrors: { [key: string]: string }) =>
-    error?.message && Object.values(customErrors).includes(error.message);
+    error?.message && Object.values(customErrors).includes(error.message.split(' - ERR:')[0]);
