6af3a271bdbd584536f445003fff9636d42b93d0
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-07-26 09:48:43 +0700

    fix: translation

--- i18n/en/maintenance.ts
+++ i18n/en/maintenance.ts
@@ -1,7 +1,7 @@
 const maintenance = {
     dashboardPage:
-        'We are excited to announce that we are working on improving our Classic Search feature to provide you with a better and more efficient experience. As a result, the classic search feature will be temporarily unavailable due to maintenance. We apologize for any inconvenience this may cause and appreciate your understanding and patience as we work to enhance our services.',
+        'We are excited to announce that we are working on improving our Classic Search feature to provide you with a better and more efficient experience. As a result, the Classic Search feature will be temporarily unavailable due to maintenance. We apologize for any inconvenience this may cause and appreciate your understanding and patience as we work to enhance our services.',
     boostbotPage:
-        'We are excited to announce that we are working on improving our Boostbot AI Search feature to provide you with a better and more efficient experience. As a result, the classic search feature will be temporarily unavailable due to maintenance. We apologize for any inconvenience this may cause and appreciate your understanding and patience as we work to enhance our services.',
+        'We are excited to announce that we are working on improving our Boostbot AI Search feature to provide you with a better and more efficient experience. As a result, the Boostbot AI Search feature will be temporarily unavailable due to maintenance. We apologize for any inconvenience this may cause and appreciate your understanding and patience as we work to enhance our services.',
 };
 export default maintenance;
