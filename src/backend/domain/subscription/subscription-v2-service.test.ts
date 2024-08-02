7731fd4a3b3116257d639d22bcdcbf11ad98b55d
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-08-02 07:57:21 +0700

    fix: test

--- src/backend/domain/subscription/subscription-v2-service.test.ts
+++ src/backend/domain/subscription/subscription-v2-service.test.ts
@@ -299,8 +299,8 @@ describe(`src/backend/domain/subscription/subscription-v2-service.test.ts`, asyn
                     },
                     {
                         subscriptionStatus: 'active',
-                        searchesLimit: '100000000',
-                        profilesLimit: '100000000',
+                        profilesLimit: '1000',
+                        searchesLimit: '2500',
                         subscriptionPlan: 'Outreach',
                         trialProfilesLimit: '100000000',
                         trialSearchesLimit: '100000000',
