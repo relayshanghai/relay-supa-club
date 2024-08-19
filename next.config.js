4668ef39e9df77909fd42856d211f96769cae64d
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-08-19 13:47:51 +0700

    fix: cyclic depencency

--- next.config.js
+++ next.config.js
@@ -4,63 +4,78 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require('@sentry/nextjs');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    typescript: {
-        tsconfigPath: './tsconfig.build.json',
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'yt3.*.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'imgp.*.icu',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: '*.tiktokcdn.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'image-cache.brainchild-tech.cn',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'image-cache.relay.club',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'quwgcjfxxojrvkenqgmi.supabase.co',
-                pathname: '**',
-            },
-        ],
-    },
-    async rewrites() {
-        return [
-          {
-            // proxy to supabase storage
-            source: '/storage/v1/:path*',
-            destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/:path*` // Proxy to Backend
-          }
-        ]
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    tsconfigPath: './tsconfig.build.json',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'yt3.*.com',
+        pathname: '**',
       },
-      experimental: {
-        instrumentationHook: true
-      }
+      {
+        protocol: 'https',
+        hostname: 'imgp.*.icu',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.tiktokcdn.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cache.brainchild-tech.cn',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cache.relay.club',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'quwgcjfxxojrvkenqgmi.supabase.co',
+        pathname: '**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        // proxy to supabase storage
+        source: '/storage/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/:path*`, // Proxy to Backend
+      },
+    ];
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.optimization.minimize = false;
+    config.plugins.push(
+      new CircularDependencyPlugin({
+        exclude: /node_modules|src\/backend\/infrastructure\/database/,
+        failOnError: false,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      })
+    );
+    return config;
+  },
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 module.exports = nextConfig;
 
-module.exports = withSentryConfig(module.exports, { silent: true }, {});
-
+module.exports = withSentryConfig(module.exports, { silent: true });
