3ccae2cd1118abdf35ca0f84a7c8e7f9288720d5
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-08-20 11:30:11 +0700

    apply chatwoot

---
+++ app/components/chatwoot/chatwoot-provider.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useEffect, useState, useContext, type PropsWithChildren } from 'react';
+import type { Chatwoot, ChatwootSettings, ChatwootSDKParams } from 'src/utils/chatwoot/types';
+import { clientLogger } from 'src/utils/logger-client';
+import { enUS, zhCN } from 'src/constants';
+
+const disableChatwoot = process.env.NEXT_PUBLIC_DISABLE_CHATWOOT === 'true';
+
+const ChatWootContext = createContext<Chatwoot | null>(null);
+
+export const useChatwoot = () => useContext(ChatWootContext);
+
+type ChatwootProviderProps = PropsWithChildren<ChatwootSettings & ChatwootSDKParams>;
+
+export const mapLangCode = (lang: string) => {
+    const codes: { [k: string]: string } = {
+        [zhCN]: 'zh_CN',
+        [enUS]: 'en',
+    };
+
+    if (lang in codes) {
+        return codes[lang];
+    }
+
+    return lang;
+};
+
+export default function ChatwootProviderV2({ children, ...chatwootOptions }: ChatwootProviderProps) {
+    const { baseUrl, websiteToken, ...settings } = chatwootOptions;
+    const [chatwoot, setChatwoot] = useState<Chatwoot | null>(null);
+
+    useEffect(() => {
+        if (disableChatwoot) return;
+        if (chatwoot !== null) return;
+        window.chatwootSettings = settings || {};
+
+        const _baseUrl = baseUrl || 'https://app.chatwoot.com';
+        const element = document.createElement('script');
+        element.src = _baseUrl + '/packs/js/sdk.js';
+        element.async = true;
+        element.onload = () => {
+            if (window.$chatwoot) return;
+
+            window.addEventListener('chatwoot:ready', function () {
+                setChatwoot(window.$chatwoot ?? null);
+            });
+
+            window.addEventListener('chatwoot:error', function (error) {
+                clientLogger(error, 'error', true);
+            });
+
+            window.chatwootSDK.run({
+                websiteToken: websiteToken,
+                baseUrl: _baseUrl,
+            });
+        };
+
+        document.body.appendChild(element);
+
+        return () => {
+            document.body.removeChild(element);
+        };
+    }, [chatwoot, baseUrl, websiteToken, settings]);
+
+    return <ChatWootContext.Provider value={chatwoot}>{children}</ChatWootContext.Provider>;
+}
--- app/components/language-toggle/language-toggle.tsx
+++ app/components/language-toggle/language-toggle.tsx
@@ -2,7 +2,7 @@
 import { type FC, useEffect, useState } from 'react';
 import { ChevronDown, LanguageToggleIcon } from '../../../src/components/icons';
 import { useTranslation } from 'react-i18next';
-import { enUS } from '../../../src/constants';
+import { enUS, zhCN, LOCAL_STORAGE_LANGUAGE_KEY } from '../../../src/constants';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -10,6 +10,10 @@ import {
     DropdownMenuTrigger,
 } from 'shadcn/components/ui/dropdown-menu';
 import { Languages } from 'src/hooks/use-localization';
+import { setBirdEatsBugLanguage } from '../analytics/bird-eats-bugs';
+import i18n from 'i18n'; // importing this initializes i18n using i19n.init()
+import { useCookies } from 'react-cookie';
+import { mapLangCode } from '../chatwoot/chatwoot-provider';
 
 export interface Language {
     lang: string;
@@ -19,6 +23,32 @@ export interface Language {
 type LanguageToggleV2Props = {
     language: string;
     setLanguage?: (language: string) => void;
+};
+
+export const useLocalization = () => {
+    const [l, setCookie] = useCookies(['language']);
+
+    useEffect(() => {
+        i18n.on('languageChanged', (l) => {
+            localStorage.setItem(LOCAL_STORAGE_LANGUAGE_KEY, l);
+            setBirdEatsBugLanguage(l);
+            window.$chatwoot?.setLocale(mapLangCode(l));
+            setCookie('language', l);
+        });
+        const urlParams = new URLSearchParams(window.location.search);
+        const setLang = urlParams.get('set_lang');
+        // if language is specified in the URL, use that, otherwise use the localStorage stored language
+        if (typeof setLang === 'string') {
+            if (setLang.includes('en')) {
+                i18n.changeLanguage(enUS);
+            } else if (setLang.includes('zh')) {
+                i18n.changeLanguage(zhCN);
+            }
+        } else {
+            i18n.changeLanguage(l.language || enUS);
+        }
+        return () => i18n.on('languageChanged', () => null);
+    }, []);
 };
 
 export const Default: FC<LanguageToggleV2Props> = ({ language, setLanguage }) => {
