764c7271ff2dd756ab7e1f21a2f2cfc9ac767437
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-10-04 08:53:38 +0700

    fix: prevent clickjacking

--- next.config.js
+++ next.config.js
@@ -73,6 +73,41 @@ const nextConfig = {
   experimental: {
     instrumentationHook: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Feature-Policy',
+            value: "geolocation 'self'; microphone 'self'; camera 'self'",
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net",
+            },
+        ],
+      },
+    ];
+}
+
 };
 
 module.exports = nextConfig;
