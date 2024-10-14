c149f462fd18d4ef1de9c4653b45ccefeb9499c1
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2024-10-14 18:20:11 +0700

    fix: logic

--- src/components/search/search-creators.tsx
+++ src/components/search/search-creators.tsx
@@ -42,6 +42,7 @@ export const SearchCreators = () => {
                         platform: searchPlatform,
                     }),
                 );
+                return;
             }
             setIsLoading(false);
 
