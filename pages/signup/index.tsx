d6c205231fece0fb4fd14cca8a6daee1c73ec209
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2025-01-12 23:24:21 +0700

    fix: build

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react';
 import SignUpPage from 'src/components/signup/signup-page';
 import { LoginSignupLayout } from 'src/components/SignupLayout';
-import { STRIPE_PRICE_MONTHLY_DIY } from 'src/utils/api/stripe/constants';
 import { ScreenshotsCarousel } from 'src/components/signup/screenshots-carousel';
 
 export default function Register() {
-    const [selectedPriceId] = useState(STRIPE_PRICE_MONTHLY_DIY);
-
     return (
-        <LoginSignupLayout
-            leftBgColor="bg-carouselbackground"
-            left={<ScreenshotsCarousel />}
-            right={<SignUpPage selectedPriceId={selectedPriceId} />}
-        />
+        <LoginSignupLayout leftBgColor="bg-carouselbackground" left={<ScreenshotsCarousel />} right={<SignUpPage />} />
     );
 }
